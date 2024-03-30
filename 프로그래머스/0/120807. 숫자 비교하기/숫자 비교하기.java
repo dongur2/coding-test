40aaed7e001f527c4ad62b8da853927c88aa7bf5
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-03-31 04:03:58 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,6 +1,9 @@
 class Solution {
     public int solution(int num1, int num2) {
-        if(num1 == num2) return 1;
-        else return -1;
+        // if(num1 == num2) return 1;
+        // else return -1;
+        
+        // * 삼항연산자 사용법
+        return (num1 == num2)? 1:-1;
     }
 }
