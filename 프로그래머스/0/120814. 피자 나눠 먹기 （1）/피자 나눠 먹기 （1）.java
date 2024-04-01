0f32cbb7381d7f690ff018c1d4c67bacdc0236a5
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-01 19:33:05 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 67.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        if(n <= 7) {
+            return 1;
+        }
+        
+        int answer = (n%7 == 0)? n/7 : n/7+1;
+        return answer;
+    }
+}
