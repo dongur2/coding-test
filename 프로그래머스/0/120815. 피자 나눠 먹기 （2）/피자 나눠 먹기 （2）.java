16671fc5538f3bedb09291242f7fc1ba3df902e1
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-01 20:17:19 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        // 6조각과 사람수의 최소공배수 / 6 = 피자개수
+        // 6조각과 사람수의 최대공약수의 배수 중 최소공배수 존재
+        
+        int[] factorsOfSix = {1, 2, 3, 6};
+        
+        int maxFactor = 1;
+        for(int factor : factorsOfSix) {
+            if(n % factor == 0) {
+                maxFactor = factor;
+            }
+        }
+        
+        int i = 1;
+        while(true) {
+            int minMultiple = maxFactor * i;
+            if(minMultiple % 6 == 0 && minMultiple % n == 0) {
+                return minMultiple / 6;
+            } else {
+                i++;
+            }
+        }
+        
+    }
+}
