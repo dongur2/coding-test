f2ee1adca39ae6825d54971a7dc562b791cf5d38
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-02 20:41:56 +0900

    [level 0] Title: 배열의 유사도, Time: 0.03 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -3,7 +3,10 @@ class Solution {
         int answer = 0;
         for(String word1 : s1) {
             for(String word2 : s2) {
-                if(word1.equals(word2)) answer++;
+                if(word1.equals(word2)) {
+                    answer++; 
+                    break;
+                }
             }
         }
         
