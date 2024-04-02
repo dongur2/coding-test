70c85ac39ba0396aabf19eed4e3613db72a419b7
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-02 18:56:42 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/README.md
+++ 프로그래머스/0/120822. 문자열 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 뒤집기 - 120822 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120822?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120822) 
 
 ### 성능 요약
 
-메모리: 81.5 MB, 시간: 9.36 ms
+메모리: 75.8 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 02일 18:53:26
+2024년 04월 02일 18:56:40
 
 ### 문제 설명
 
--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,12 +1,21 @@
 class Solution {
-    public String solution(String my_string) {
+    private String myCodeWithString(String my_string) {
         char[] chars = my_string.toCharArray();
         
         String answer = "";
         for(int i = chars.length - 1; i >= 0; i--) {
             answer += chars[i];
         }
-        
         return answer;
+    }
+    
+    private String useStringBuilder(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(my_string);
+        return sb.reverse().toString();
+    }
+    
+    public String solution(String my_string) {
+        return useStringBuilder(my_string);
     }
 }
