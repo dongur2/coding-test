a22ef02d01057c73ac623ef29144bdbe22b97c62
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-02 18:59:56 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,10 +1,8 @@
 class Solution {
     private String myCodeWithString(String my_string) {
-        char[] chars = my_string.toCharArray();
-        
         String answer = "";
-        for(int i = chars.length - 1; i >= 0; i--) {
-            answer += chars[i];
+        for(int i = my_string.length() - 1; i >= 0; i--) {
+            answer += my_string.charAt(i);
         }
         return answer;
     }
@@ -13,6 +11,8 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         sb.append(my_string);
         return sb.reverse().toString();
+        
+        // return new StringBuilder(my_string).reverse().toString();
     }
     
     public String solution(String my_string) {
