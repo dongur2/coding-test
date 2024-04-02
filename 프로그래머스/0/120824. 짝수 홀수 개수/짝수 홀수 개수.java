4c5b489412b70b8ee07c154734d4a3a03761ea8d
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-02 19:23:01 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        for(int number : num_list) {
+            if(number % 2 == 0) {
+                answer[0]++;
+            } else {
+                answer[1]++;
+            }
+        }
+        return answer;
+    }
+}
