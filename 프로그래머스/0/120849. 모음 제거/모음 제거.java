b62fca454afffdadd28b9d6b70180c09d20ad6cf
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-02 21:26:48 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,14 +1,19 @@
 class Solution {
-    public String solution(String my_string) {
+    private String myCode(String my_string) { // 76.5MB, 0.11ms
         String[] vowels = {"a", "e", "i", "o", "u"};
         
-        StringBuffer answer = new StringBuffer(my_string);
+        String answer = my_string;
         for(String vowel : vowels) {
-            while(answer.indexOf(vowel) != -1) {
-                answer.deleteCharAt(answer.indexOf(vowel));
-            }
+            answer = answer.replace(vowel, "");
         }
-        
-        return answer.toString();
+        return answer;
+    }
+    
+    private String useRegex(String my_string) {
+        return my_string.replaceAll("[aeiou]", "");
+    }
+    
+    public String solution(String my_string) {
+        return myCode(my_string);
     }
 }
