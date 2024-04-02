2065c53aa59363f5b313990913ff44b57e862ec3
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-02 19:25:13 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,6 +1,5 @@
 class Solution {
-    public int[] solution(int[] num_list) {
-        int[] answer = new int[2];
+    private void myCode(int[] num_list, int[] answer) {
         for(int number : num_list) {
             if(number % 2 == 0) {
                 answer[0]++;
@@ -8,6 +7,17 @@ class Solution {
                 answer[1]++;
             }
         }
+    }
+    
+    private void referTo(int[] num_list, int[] answer) {
+        for(int number : num_list) {
+            answer[number % 2]++;
+        }
+    }
+    
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        referTo(num_list, answer);
         return answer;
     }
 }
