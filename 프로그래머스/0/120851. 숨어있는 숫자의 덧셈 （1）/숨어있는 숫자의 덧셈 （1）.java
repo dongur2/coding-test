6f48b9f7329551c2bc560fb817d4c5a3f5ad2261
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-02 22:17:00 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.12 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int solution(String my_string) {
+    private int myCode(String my_string) {
         int answer = 0;
         
         my_string = my_string.replaceAll("[A-Za-z]", "");
@@ -7,6 +7,21 @@ class Solution {
             answer += Integer.parseInt(my_string.charAt(i)+"");
         }
         
+        return answer; 
+    }
+    
+    private int referTo(String my_string) {
+        int answer = 0;
+        
+        my_string = my_string.replaceAll("[^0-9]", "");
+        for(char ch : my_string.toCharArray()) {
+            answer += Character.getNumericValue(ch);
+        }
+        
         return answer;
+    }
+    
+    public int solution(String my_string) {
+        return referTo(my_string);
     }
 }
