9b2c7a7fd65a9183e2a66c6e2f14b2cfb2b8905f
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-02 20:52:08 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -1,7 +1,19 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] array, int height) {
+    private int useStream(int[] array, int height) {
         return (int)Arrays.stream(array).filter(man -> man > height).count();
+    }
+    
+    private int useFor(int[] array, int height) {
+        int answer = 0;
+        for(int man : array) {
+            if(man > height) answer++;
+        }
+        return answer;
+    }
+    
+    public int solution(int[] array, int height) {
+        return useFor(array, height);
     }
 }
