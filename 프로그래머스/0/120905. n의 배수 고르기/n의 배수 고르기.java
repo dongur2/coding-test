6c72e3f79cefdeda987602e08eeef189174cfd82
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-03 18:07:04 +0900

    [level 0] Title: n의 배수 고르기, Time: 3.04 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    private int[] useFor(int n, int[] numlist) {
+        List<Integer> multiples = new ArrayList<>();
+        for(int num : numlist) {
+            if(num % n == 0) multiples.add(num);
+        }
+    
+        int[] answer = new int[multiples.size()];
+        for(int i=0; i<multiples.size(); i++) {
+            answer[i] = multiples.get(i);
+        }
+        
+        return answer;
+    }
+    
+    private int[] forStream(int n, int[] numlist) {
+        return Arrays.stream(numlist).filter(num -> num % n == 0).toArray();
+    }
+    
+    public int[] solution(int n, int[] numlist) {
+        return forStream(n, numlist);
+    }
+}
