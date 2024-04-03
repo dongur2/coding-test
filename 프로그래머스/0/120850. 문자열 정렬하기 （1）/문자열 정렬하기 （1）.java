61c49da54c37f2468ed3255a15a3c15003a4854c
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-04 02:31:33 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 6.51 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String my_string) {
+        String numbers = my_string.replaceAll("[^0-9]", "");
+        
+        int[] answer = new int[numbers.length()];
+        for(int i=0; i<numbers.length(); i++) {
+            answer[i] = Integer.parseInt(numbers.charAt(i)+"");
+        }
+        
+        Arrays.sort(answer);
+        return answer;
+    }
+}
