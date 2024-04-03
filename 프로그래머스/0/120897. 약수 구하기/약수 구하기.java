989d3d630eb93aaebfc130b29dd74007ffb04881
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-04 02:50:11 +0900

    [level 0] Title: 약수 구하기, Time: 0.03 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -1,7 +1,27 @@
 import java.util.stream.*;
+import java.util.*;
 
 class Solution {
-    public int[] solution(int n) {
+    private int[] useStream(int n) {
         return IntStream.rangeClosed(1, n).filter(num -> n % num == 0).toArray();
+    }
+    
+    private int[] useFor(int n) {
+        List<Integer> num = new ArrayList<>();
+        
+        for(int i=1; i<=n; i++) {
+            if(n % i == 0) num.add(i);
+        }
+        
+        int[] answer = new int[num.size()];
+        for(int i=0; i<num.size(); i++) {
+            answer[i] = num.get(i);
+        }
+        
+        return answer;
+    }
+    
+    public int[] solution(int n) {
+        return useFor(n);
     }
 }
