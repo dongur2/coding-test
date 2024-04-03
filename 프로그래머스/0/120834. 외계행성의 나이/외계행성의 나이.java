90828edff757324bb58dabe1d17aae8936426711
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-03 18:44:19 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,16 +1,10 @@
-import java.util.*;
-
 class Solution {
     public String solution(int age) {
-        String[] table = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
-        // 48, 49, 50 ,51 ...
-        
         StringBuilder answer = new StringBuilder();
         
-        String stringAge = age+"";
-        for(int i=0; i<stringAge.length(); i++) {
-            String ch = table[(int)stringAge.charAt(i) - 48];
-            answer.append(ch);
+        while(age > 0) {
+            answer.insert(0, (char)(age % 10 + 97));
+            age /= 10;
         }
         
         return answer.toString();
