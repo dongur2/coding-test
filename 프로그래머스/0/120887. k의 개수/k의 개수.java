29d248e3d8653090d599e08b95cb570fae3d69a0
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-04 04:10:33 +0900

    [level 0] Title: k의 개수, Time: 1.80 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int solution(int i, int j, int k) {
+    private int useChar(int i, int j, int k) {
         int answer = 0;
         
         for(int num=i; num<=j; num++) {
@@ -10,5 +10,23 @@ class Solution {
         }
         
         return answer;
+    }
+    
+    private int useMath(int i, int j, int k) {
+        int answer = 0;
+        
+        for(int num=i; num<=j; num++) {
+            int tempNum = num;
+            while(tempNum > 0) {
+                if(tempNum % 10 == k) answer++;
+                tempNum /= 10;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public int solution(int i, int j, int k) {
+        return useMath(i, j, k);
     }
 }
