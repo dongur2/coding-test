6d6f0ec14f0fbb910858310b2b4704b340399975
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-03 23:56:43 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/README.md
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 구슬을 나누는 경우의 수 - 120840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120840#) 
 
 ### 성능 요약
 
-메모리: 76.2 MB, 시간: 13.62 ms
+메모리: 77.5 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 03일 23:56:06
+2024년 04월 03일 23:56:42
 
 ### 문제 설명
 
--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
@@ -4,14 +4,11 @@ class Solution {
         double answer = 1;
         
         for(int i=balls; i>share; i--) { // n! / m!
-            System.out.println("i= "+i);
             answer *= i;
-            System.out.println("** "+answer);
         }
         
         for(int j=(balls-share); j>0; j--) { // (n-m)!
             answer /= j;
-            System.out.println("// "+answer);
         }
         
         return (int)answer;
