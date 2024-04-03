ff609bb5b39addb5c38170da32207b9d39383c66
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-03 19:20:17 +0900

    [level 0] Title: 가위 바위 보, Time: 0.04 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -3,9 +3,7 @@ class Solution {
         StringBuilder answer = new StringBuilder();
         
         for(int i=0; i<rsp.length(); i++) {
-            char what = rsp.charAt(i);
-            
-            switch(what) {
+            switch(rsp.charAt(i)) {
                 case '2': answer.append("0"); break;
                 case '0': answer.append("5"); break;
                 case '5': answer.append("2"); break;
