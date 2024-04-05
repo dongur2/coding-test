ddb404e5ef184c300ff0614ad9162366585dfab5
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-05 20:42:34 +0900

    [level 0] Title: 가까운 수, Time: 0.38 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array, int n) {
+        Arrays.sort(array);
+        
+        int answer = array[0];
+        for(int num : array) {
+            if(Math.abs(Math.abs(answer) - n) > Math.abs(Math.abs(num) - n)) {
+                answer = num;
+            }
+        }
+        
+        return answer;
+    }
+}
