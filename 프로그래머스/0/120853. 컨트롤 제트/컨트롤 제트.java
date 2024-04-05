6ce3604092c4ccfd208fbb7acd3aae3f0e767605
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-05 20:29:51 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.31 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,6 +1,8 @@
+import java.util.*;
+
 class Solution {
-    public int solution(String s) {
-        String[] arr = s.split(" ");
+    private int myCode(String s) {
+       String[] arr = s.split(" ");
         
         int answer = 0;
         
@@ -14,6 +16,32 @@ class Solution {
             }
         }    
         
+        return answer; 
+    }
+    
+    /*
+    * Stack 활용: Z 직전에 저장한 (가장 최근에 저장한) 숫자를 제거
+    */
+    private int useStack(String s) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for(String piece : s.split(" ")) {
+            if(piece.equals("Z")) {
+                stack.pop();
+            } else {
+                stack.push(Integer.parseInt(piece));
+            }
+        }
+        
+        int answer = 0;
+        for(int num : stack) {
+            answer += num;
+        }
+        
         return answer;
+    }
+    
+    public int solution(String s) {
+        return useStack(s);
     }
 }
