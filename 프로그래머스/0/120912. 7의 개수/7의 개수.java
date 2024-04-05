d98bb9c34dfba6b44e17c790f0ea8dda2b30f03f
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-05 22:22:47 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] array) {
+        int count = 0;
+        for(int num : array) {
+            while(num > 0) {
+                if(num%10 == 7) count++;
+                num /= 10;
+            }
+        }
+        
+        return count;
+    }
+}
