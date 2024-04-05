c2989e917cbaa1f58982c9a21a562a2071f113ae
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-06 03:01:13 +0900

    [level 0] Title: 종이 자르기, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -1,9 +1,11 @@
 class Solution {
     public int solution(int M, int N) {
-        if(M*N == 1) return 0;
-        if(N == 1) return M-1; 
-        if(M == 1) return N-1;
+//         if(M*N == 1) return 0;
+//         if(N == 1) return M-1; 
+//         if(M == 1) return N-1;
         
-        return (M-1) + (M * (N-1));
+//         return (M-1) + (M * (N-1));
+        
+        return M * N - 1;
     }
 }
