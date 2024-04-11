7485ee930bd5d752a4a151a79617c985699e768c
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-12 05:05:59 +0900

    [level 0] Title: ad 제거하기, Time: 0.76 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -1,7 +1,26 @@
 import java.util.*;
 
 class Solution {
-    public String[] solution(String[] strArr) {
+    private String[] useStream(String[] strArr) {
         return Arrays.stream(strArr).filter(s -> !s.contains("ad")).toArray(String[]::new);
+    }
+    
+    private String[] useFor(String[] strArr) {
+        List<String> list = new ArrayList<>();
+        
+        for(String s:strArr) {
+            if(!s.contains("ad")) list.add(s);
+        }
+        
+        String[] answer = new String[list.size()];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+    
+    public String[] solution(String[] strArr) {
+        return useFor(strArr);
     }
 }
