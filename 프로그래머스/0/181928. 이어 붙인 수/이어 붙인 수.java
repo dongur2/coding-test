7ea72964673d3950b731feed66748359b5ee9513
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-13 05:49:07 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int solution(int[] num_list) {
+    private int my(int[] num_list) {
         StringBuilder odd = new StringBuilder();
         StringBuilder even = new StringBuilder();
         
@@ -9,5 +9,26 @@ class Solution {
         }
         
         return Integer.parseInt(odd.toString()) + Integer.parseInt(even.toString());
+    }
+    
+    private int refer(int[] num_list) {
+        int odd = 0;
+        int even = 0;
+        
+        for(int num:num_list) {
+            if(num % 2 == 0) {
+                even *= 10;
+                even += num;
+            } else {
+                odd *= 10;
+                odd += num;
+            }
+        }
+        
+        return odd + even;
+    }
+    
+    public int solution(int[] num_list) {
+        return refer(num_list);
     }
 }
