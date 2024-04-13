fa569d6909cda4a174bbc97c2ca15e40da6eb1e4
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-13 19:26:55 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.01 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length];
+        
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = num_list[(i+n) % answer.length];
+        }
+        
+        return answer;
+    }
+}
