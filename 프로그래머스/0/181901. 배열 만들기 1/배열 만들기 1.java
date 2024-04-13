4b759cba261ac693e5416e4a3e82ed4fc370ed80
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-13 19:50:57 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.02 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,3 +1,5 @@
+import java.util.stream.*;
+
 class Solution {
     private int[] useArr(int n, int k) {
         int[] answer = new int[n/k];
@@ -5,6 +7,10 @@ class Solution {
             answer[i] = k * (i+1);
         }
         return answer;
+    }
+    
+    private int[] useStream(int n, int k) {
+        return IntStream.rangeClosed(1, n).filter(num -> num % k == 0).toArray();
     }
     
     public int[] solution(int n, int k) {
