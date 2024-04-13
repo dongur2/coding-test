a29b8bfcc767e84eda2d1c2f5876d760d2bbec9e
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-13 20:30:47 +0900

    [level 0] Title: l로 만들기, Time: 13.71 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,5 +1,19 @@
 class Solution {
-    public String solution(String myString) {
+    private String useRegex(String myString) {
         return myString.replaceAll("[a-l]", "l");
+    }
+    
+    private String useChar(String myString) {
+        StringBuilder answer = new StringBuilder();
+        
+        for(int i=0; i<myString.length(); i++) {
+            answer.append((myString.charAt(i) > 'l')? myString.charAt(i):"l");
+        }
+        
+        return answer.toString();
+    }
+    
+    public String solution(String myString) {
+        return useChar(myString);
     }
 }
