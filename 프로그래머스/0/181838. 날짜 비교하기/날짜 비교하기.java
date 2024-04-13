8b0536bdec76f6304bcede3ffb3b08c43bef18f3
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-13 22:47:34 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.77 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,9 +1,22 @@
+import java.time.*;
+
 class Solution {
-    public int solution(int[] date1, int[] date2) {
+     private int my(int[] date1, int[] date2) {
         for(int i=0; i<3; i++) {
             if(date1[i] < date2[i]) return 1;
             else if (date1[i] > date2[i]) return 0;
         }
         return 0;
+    }
+    
+    private int useDate(int[] date1, int[] date2) {
+        LocalDate dateOne = LocalDate.of(date1[0], date1[1], date1[2]);
+        LocalDate dateTwo = LocalDate.of(date2[0], date2[1], date2[2]);
+        
+        return dateOne.isBefore(dateTwo)? 1:0;
+    }
+    
+    public int solution(int[] date1, int[] date2) {
+        return useDate(date1, date2);
     }
 }
