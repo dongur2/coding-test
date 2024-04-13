28a3ba1add524e015283e5e1f686905933ba0f28
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-14 05:12:41 +0900

    [level 0] Title: 배열 만들기 4, Time: 3.80 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        Stack<Integer> stack = new Stack();
+        
+        int i = 0; 
+        
+        while (i < arr.length) {
+            if(stack.empty()) stack.push(arr[i++]);
+            if(stack.peek() < arr[i]) stack.push(arr[i++]); 
+            else stack.pop(); 
+            
+        }
+        
+        int[] stk = new int[stack.size()];
+        for(int j=stack.size()-1; j>=0; j--) {
+            stk[j] = stack.pop();
+        }
+        
+        return stk;
+    }
+}
