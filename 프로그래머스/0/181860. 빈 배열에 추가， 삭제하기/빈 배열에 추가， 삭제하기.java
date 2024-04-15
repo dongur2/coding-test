eaec3f76d181743ef0ee573e9ebea4aabd5a5b15
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-16 05:37:46 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 2.28 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.java
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.java
@@ -6,17 +6,13 @@ class Solution {
         
         for(int i=0; i<flag.length; i++) {
             if(flag[i]) { 
-                for(int j=0; j<arr[i]*2; j++) {
-                    stack.push(arr[i]);
-                }
+                for(int j=0; j<arr[i]*2; j++) stack.push(arr[i]);
             }
             else {
-                for(int j=0; j<arr[i]; j++) {
-                    stack.pop();
-                }
+                for(int j=0; j<arr[i]; j++) stack.pop();
             }
         }
         
-        return stack.stream().mapToInt(l->l).toArray();
+        return stack.stream().mapToInt(Integer::intValue).toArray();
     }
 }
