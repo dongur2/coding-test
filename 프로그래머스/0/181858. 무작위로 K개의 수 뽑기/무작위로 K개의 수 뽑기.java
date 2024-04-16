9dc3a82bca8df27eafe2997b127115fcbafe4d55
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-16 22:27:30 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 173.74 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] arr, int k) {
+    private int[] myWithMap(int[] arr, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         
         int idx = 0;
@@ -17,5 +17,31 @@ class Solution {
         }
         
         return answer;
+    }
+    
+    private int[] useArrayList(int[] arr, int k) {
+        List<Integer> list = new ArrayList<>();
+        
+        for(int num : arr) {
+            if(!list.contains(num)) list.add(num);
+            continue;
+        }
+        
+        if(list.size() == k) return list.stream().mapToInt(Integer::intValue).toArray();
+        else if(list.size() > k) {
+            return Arrays.copyOfRange(list.stream().mapToInt(Integer::intValue).toArray(), 0, k);
+        } else {
+            int[] answer = new int[k];
+            for(int i=0; i<k; i++) {
+                if(i<list.size()) answer[i] = list.get(i);
+                else answer[i] = -1;
+            }
+            return answer;
+        }
+        
+    }
+    
+    public int[] solution(int[] arr, int k) {
+        return useArrayList(arr, k);
     }
 }
