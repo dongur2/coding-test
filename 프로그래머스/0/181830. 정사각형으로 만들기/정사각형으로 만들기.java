4424fad588501e43844c4007e95d83b09d683e9c
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-18 12:20:49 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.11 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    public int[][] solution(int[][] arr) {
+    private int[][] my(int[][] arr) {
         int rows = arr.length;
         int cols = arr[0].length;
 
@@ -15,5 +15,31 @@ class Solution {
         }
 
         return answer;
+    }
+    
+    /*
+    * System.arraycopy(Object src, int srcPos, Object dest, int destPos, int length)
+    * - copies an array from the specified source array
+    * , beginning at the specified position, to the specified position of the destination array. 
+    *
+    * src - the source array.
+    * srcPos - starting position in the source array.
+    * dest - the destination array.
+    * destPos - starting position in the destination data.
+    * length - the number of array elements to be copied.
+    */
+    private int[][] useSystemCopy(int[][] arr) {
+        int n = Math.max(arr.length, arr[0].length);    
+        int[][] answer = new int[n][n];
+        
+        for(int i=0; i<arr.length; i++) {
+            System.arraycopy(arr[i], 0, answer[i], 0, arr[i].length);
+        }
+        
+        return answer;
+    }
+    
+    public int[][] solution(int[][] arr) {
+        return useSystemCopy(arr);
     }
 }
