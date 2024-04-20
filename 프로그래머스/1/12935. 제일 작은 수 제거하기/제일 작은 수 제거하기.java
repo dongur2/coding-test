ea87c765768d2c9dc3afa37183256ea3de1679a7
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-21 04:21:02 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 3.60 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -4,12 +4,13 @@ class Solution {
     public int[] solution(int[] arr) {
         if(arr.length == 1) return new int[] {-1};
         
-        int min = Integer.MAX_VALUE;
-        for(int i=0; i<arr.length; i++) {
-            min = (arr[i] < min)? arr[i] : min;
-        }
+        int min = Arrays.stream(arr).min().getAsInt();
+        // int min = Integer.MAX_VALUE;
+        // for(int i=0; i<arr.length; i++) {
+        //     min = (arr[i] < min)? arr[i] : min;
+        // }
+        // final int rmv = min;
         
-        final int rmv = min;
-        return Arrays.stream(arr).filter(a -> a != rmv).toArray();
+        return Arrays.stream(arr).filter(a -> a != min).toArray();
     }
 }
