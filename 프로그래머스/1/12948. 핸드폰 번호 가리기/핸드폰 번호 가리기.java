d334b0101d2ab342f900b0e52030059e686a1288
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-21 03:34:08 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,5 +1,11 @@
 class Solution {
     public String solution(String phone_number) {
-        return "*".repeat(phone_number.length()-4) + phone_number.substring(phone_number.length()-4);
+        // return "*".repeat(phone_number.length()-4) + phone_number.substring(phone_number.length()-4);
+        
+        char[] chars = phone_number.toCharArray();
+        for(int i=0; i<phone_number.length()-4; i++) {
+            chars[i] = '*';
+        }
+        return String.valueOf(chars);
     }
 }
