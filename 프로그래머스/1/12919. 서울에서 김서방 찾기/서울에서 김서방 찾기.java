5d2a363b22140b67d2f3064ab7bca17d873508d7
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-21 02:54:14 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 5.70 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,13 +1,23 @@
+import java.util.*;
+
 class Solution {
-    public String solution(String[] seoul) {
-        int answer = -1;
-        
+    private int useFor(String[] seoul) {
+        int idx = 0;
         for(int i=0; i<seoul.length; i++) {
             if(seoul[i].equals("Kim")) {
-                answer = i;
+                idx = i;
                 break;
             }
         }
+        return idx;
+    }
+    
+    private int useArray(String[] seoul) {
+        return Arrays.asList(seoul).indexOf("Kim");
+    }
+    
+    public String solution(String[] seoul) {
+        int answer = useArray(seoul);
         
         return "김서방은 " + answer +"에 있다";
     }
