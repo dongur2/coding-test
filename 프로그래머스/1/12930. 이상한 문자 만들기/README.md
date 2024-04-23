16535fc374059e40304dac79c30d9bc3d05e5484
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-23 22:55:05 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.05 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/README.md
+++ 프로그래머스/1/12930. 이상한 문자 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 이상한 문자 만들기 - 12930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930#) 
 
 ### 성능 요약
 
-메모리: 71.9 MB, 시간: 0.16 ms
+메모리: 76.8 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 23일 22:48:47
+2024년 04월 23일 22:55:04
 
 ### 문제 설명
 
--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,5 +1,5 @@
 class Solution {
-    public String solution(String s) {
+    private String my(String s) {
         String[] words = s.split(" ");
 
         StringBuilder answer = new StringBuilder();
@@ -15,6 +15,21 @@ class Solution {
         if(answer.length() < s.length()) answer.append(" ".repeat(s.length()-answer.length()));
         
         return (answer.length() > s.length())? 
-            answer.deleteCharAt(answer.length()-1).toString() : answer.toString();
+            answer.deleteCharAt(answer.length()-1).toString() : answer.toString(); 
+    }
+    
+    private String useChar(String s) {
+        char[] chars = s.toCharArray();
+        
+        int idx = 0;
+        for(int i=0; i<chars.length; i++) {
+            idx = (chars[i] == ' ')? 0 : idx+1;
+            chars[i] = (idx % 2 == 0)? Character.toLowerCase(chars[i]) : Character.toUpperCase(chars[i]);
+        }
+        
+        return String.valueOf(chars);
+    }    
+    public String solution(String s) {
+        return useChar(s);
     }
 }
