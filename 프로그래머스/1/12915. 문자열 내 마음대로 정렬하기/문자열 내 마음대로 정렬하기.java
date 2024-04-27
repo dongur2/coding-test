7e0d4720ee228b1c540c4b3efaff2b6e7dbbb634
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-28 03:27:39 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 9.52 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,14 +1,34 @@
 import java.util.*;
 
 class Solution {
-    public String[] solution(String[] strings, int n) {
-        Arrays.sort(strings, new Comparator<String>() {
+    private String[] useComparator(String[] strings, int n) {
+       Arrays.sort(strings, new Comparator<String>() {
             @Override
             public int compare(String s1, String s2) {
                 if(s1.charAt(n) == s2.charAt(n)) return s1.compareTo(s2);
                 else return s1.charAt(n) - s2.charAt(n);
             }
         });
-        return strings;
+        return strings; 
+    }
+    
+    private String[] refer(String[] strings, int n) {
+        String[] arr = new String[strings.length];
+        
+        for(int i=0; i<strings.length; i++) {
+            arr[i] = strings[i].charAt(n) + strings[i];
+        }
+        
+        Arrays.sort(arr);
+        
+        for(int i=0; i<strings.length; i++) {
+            arr[i] = arr[i].substring(1);
+        }
+        
+        return arr;
+    }
+    
+    public String[] solution(String[] strings, int n) {
+        return refer(strings, n);
     }
 }
