7333ac2bd6c274f0f8e9e1b8878f9107b2a03315
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-28 18:59:54 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 1.16 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,22 @@
+class Solution {
+    private int GCD(int n, int m) {
+        if(m == 0) return n;
+        return GCD(m, n % m);
+    }
+    
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        
+        answer[0] = GCD(n,m);
+        
+        for(int i=1; ;i++) {
+            if(answer[0] * i % n == 0 && answer[0] * i % m == 0) {
+                answer[1] = answer[0] * i;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+
+}
