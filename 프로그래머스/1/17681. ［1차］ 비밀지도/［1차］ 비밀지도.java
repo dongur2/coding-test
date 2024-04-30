01a6ecfe7d34b9adbaba09821006614590f05202
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-04-30 21:13:49 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.05 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -5,28 +5,14 @@ class Solution {
         for(int i=0; i<n; i++) {
             StringBuilder res = new StringBuilder();
             
-            StringBuilder t1 = new StringBuilder(convertTo2(arr1[i]));
-            StringBuilder t2 = new StringBuilder(convertTo2(arr2[i]));
-            
-            if(t1.length() < n) t1.insert(0, "0".repeat(n-t1.length()));
-            if(t2.length() < n) t2.insert(0, "0".repeat(n-t2.length()));
+            StringBuilder t = new StringBuilder(Integer.toBinaryString(arr1[i] | arr2[i]));
+            if(t.length() < n) t.insert(0, "0".repeat(n-t.length()));
             
             for(int j=0; j<n; j++) {
-                res.append((t1.charAt(j) > '0' || t2.charAt(j) > '0')? "#":" ");
+                res.append((t.charAt(j) > '0')? "#":" ");
             }
             answer[i] = res.toString();
         }
-        
-        return answer;
-    }
-    
-    private String convertTo2(int origin) {
-            StringBuilder t = new StringBuilder();
-            while(origin > 1) {
-                t.append(origin % 2);
-                origin /= 2;
-            }
-            t.append(origin);
-            return t.reverse().toString();
+        return answer;  
     }
 }
