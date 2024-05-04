51828660b5a35008a26d9699d0b99824c2a5c6de
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-05-04 15:16:56 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,7 +1,24 @@
 import java.time.LocalDate;
 
 class Solution {
-    public String solution(int a, int b) {
+    private String real(int a, int b) {
         return LocalDate.of(2016,a,b).getDayOfWeek().toString().substring(0,3);
+    }
+    
+    private String again(int a, int b) {
+        int[] endOfMonth = {31,29,31,30,31,30,31,31,30,31,30,31};
+        String[] MM = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+        
+        int today = 0;
+        for(int i=0; i<a-1; i++){
+            today += endOfMonth[i];
+        }
+        today += b-1;
+        
+        return MM[today%7];
+    }
+    
+    public String solution(int a, int b) {
+        return again(a,b);
     }
 }
