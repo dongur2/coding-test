e39b3ba35cd58b655c0f24271de1a60e749b3a33
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-05-08 06:02:26 +0900

    [level 1] Title: 소수 찾기, Time: 36.79 ms, Memory: 55.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        int[] cnt = new int[n+1]; // 인덱스의 약수 개수
+        
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n/i; j++) {
+                cnt[i*j]++;
+            }
+        }
+        
+        int answer = 0;
+        for(int c:cnt) {
+            if(c==2) answer++;
+        }
+        return answer;
+    }
+}
