3b10528f62ed3ff9ff04fcdd3f1973a38ea4a91e
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-05-09 16:30:05 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] nums) {
         int cnt = 0;
@@ -18,19 +16,12 @@ class Solution {
     }
     
     private boolean isPrime(int num) {
-        int cnt = 0;
-        
-        if (num % 2 == 0) {
-            return false; // 짝수는 소수가 아님
-        }
+        if (num % 2 == 0) return false; // 짝수는 소수가 아님
         
         // 제곱근까지의 수로만 나눠지는지 확인
         for (int i = 3; i * i <= num; i += 2) {
-            if (num % i == 0) {
-                return false; // 나누어 떨어지면 소수가 아님
-            }
+            if (num % i == 0) return false; // 나누어 떨어지면 소수가 아님
         }
         return true; // 모두 통과하면 소수
     }
-
 }
