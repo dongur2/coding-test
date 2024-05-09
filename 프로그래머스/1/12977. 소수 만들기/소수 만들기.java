d328c875c4b4b49d444b91e4bd772398d6d7becb
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-05-09 16:27:49 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -2,25 +2,35 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        List<Integer> sum = new ArrayList<>();
+        int cnt = 0;
+        
         for(int i=0; i<nums.length; i++) {
             for(int j=i+1; j<nums.length; j++) {
                 for(int l=j+1; l<nums.length; l++) {
-                    sum.add(nums[i] + nums[j] + nums[l]);
+                    int sum = nums[i] + nums[j] + nums[l];
+                    
+                    // 세 수의 합이 소수인지 판별
+                    if(isPrime(sum)) cnt++;
                 }
             }
         }
-        
-        int cnt = 0;
-        for(int s:sum) {
-            int d = 0;
-            for(int i=1; i<=s; i++) {
-                if(s % i == 0) d++;
-                if(d > 2) break;
-            }
-            if(d==2) cnt++;
-        }
-        
         return cnt;
     }
+    
+    private boolean isPrime(int num) {
+        int cnt = 0;
+        
+        if (num % 2 == 0) {
+            return false; // 짝수는 소수가 아님
+        }
+        
+        // 제곱근까지의 수로만 나눠지는지 확인
+        for (int i = 3; i * i <= num; i += 2) {
+            if (num % i == 0) {
+                return false; // 나누어 떨어지면 소수가 아님
+            }
+        }
+        return true; // 모두 통과하면 소수
+    }
+
 }
