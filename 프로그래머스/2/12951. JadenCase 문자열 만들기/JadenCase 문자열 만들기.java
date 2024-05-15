f11f73558b92640bb2335c6e9d5e83121c6413cb
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-05-15 14:46:37 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,12 +1,22 @@
 class Solution {
-    public String solution(String s) {
-        char[] arr = s.toLowerCase().toCharArray();
-        
+    private void useLength(char[] arr) {
         if(arr[0] >= 65) arr[0] = Character.toUpperCase(arr[0]);
         for(int i=0; i<arr.length; i++) {
             if(arr[i] == ' ' && i+1 < arr.length) arr[i+1] = Character.toUpperCase(arr[i+1]);
         }
-        
+    }
+    
+    private void useBoolean(char[] arr) {
+        boolean isBlank = true;
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = (isBlank)? Character.toUpperCase(arr[i]):arr[i];
+            isBlank = (arr[i] == ' ')? true:false;
+        }
+    }
+    
+    public String solution(String s) {
+        char[] arr = s.toLowerCase().toCharArray();
+        useBoolean(arr);        
         return String.valueOf(arr);
     }
 }
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951#) 
 
 ### 성능 요약
 
-메모리: 74.6 MB, 시간: 0.04 ms
+메모리: 73.1 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 15일 14:33:18
+2024년 05월 15일 14:46:36
 
 ### 문제 설명
 
