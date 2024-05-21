892126e5fdd9a08fc77fced278813822f7907fa9
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-05-21 13:16:17 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -1,16 +1,17 @@
 public class Solution {
     public int solution(int n) {
-        int power = 0;
-
+        // int power = 0;
         // 2로 나누다가 나누어 떨어지지 않으면 -1씩 감소 - 0까지
-        while(n>0) {
-            if(n % 2 == 0) n/=2;
-            else {
-                n--;
-                power++;
-            }
-        }
-
-        return power;
+        // while(n>0) {
+        //     if(n % 2 == 0) n/=2;
+        //     else {
+        //         n--;
+        //         power++;
+        //     }
+        // }
+        // return power;
+        
+        // * 2로 나누어지지 않을 때만 카운트하니까 bitCount()로 1인 경우만 카운트하는 것과 같음
+        return Integer.bitCount(n);
     }
 }
