91e5c7cecc6f7666337664d091c9d7f1361ab8f0
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-07-01 21:13:50 +0900

    [level 1] Title: 예산, Time: 0.36 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/README.md
+++ 프로그래머스/1/12982. 예산/README.md
@@ -1,10 +1,10 @@
 # [level 1] 예산 - 12982 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12982) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12982?language=java) 
 
 ### 성능 요약
 
-메모리: 75.9 MB, 시간: 0.45 ms
+메모리: 73.8 MB, 시간: 0.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 23일 23:08:19
+2024년 07월 01일 21:13:48
 
 ### 문제 설명
 
--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,15 +1,21 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[] d, int budget) {
+        // 최대한 많은 부서를 지원했을 때 그 부서 개수 리턴
+        // -> 신청 금액이 적은 순서대로 정렬 후 예산이 그 금액 합에 가까울 때까지 지원
+
+        //1. 신청 금액 배열을 오름차순 정렬
         Arrays.sort(d);
-        
-        int cnt = 0, sum = 0;
-        for(int price : d) {
-            sum += price;
-            if(sum <= budget) cnt++;
+
+        int summary = 0;
+        //2. 인덱스를 하나씩 증가: 지원 부서 하나씩 증가 & 신청 금액 합과 예산 비교
+        for(int i=0; i<d.length; i++) {
+            summary += d[i];
+
+            if(summary > budget) return i; // 신청 금액 합 > 예산: 바로 직전 지원 부서 개수 리턴
+            else if(summary == budget) return i+1; // 신청 금액 합 == 예산: 현재 지원 부서 개수 리턴
         }
-        
-        return cnt;
+        return d.length; //for문 종료까지 빠져나가지 않았다면 신청 부서 모두 지원 가능
     }
 }
