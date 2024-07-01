c1d5106fb27fdf55e9cbe971157d1875a138b5a1
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-07-01 21:47:19 +0900

    [level 2] Title: 전화번호 목록, Time: 368.09 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,10 +3,10 @@ import java.util.Arrays;
 class Solution {
     public boolean solution(String[] phone_book) {
         // 배열의 전화번호 중 다른 번호의 접두어가 있을 경우, false 리턴
-        
+
         //1. 배열을 통째로 비교하게 되면 시간 초과 -> 정렬 후 비교
         Arrays.sort(phone_book);
-        
+
         //2. 인접한 위치를 비교
         for(int i=0; i<phone_book.length-1; i++) {
             if(phone_book[i+1].startsWith(phone_book[i])) return false;
