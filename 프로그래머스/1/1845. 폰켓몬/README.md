27555d33ad77e0799b4cdf2925f1aaa6efe8463b
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-07-02 17:03:51 +0900

    [level 1] Title: 폰켓몬, Time: 6.56 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
 
 ### 성능 요약
 
-메모리: 69 MB, 시간: 4.85 ms
+메모리: 77.5 MB, 시간: 6.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 04일 14:47:28
+2024년 07월 02일 17:03:47
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,13 +2,21 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int pick = nums.length/2;
-  
-        Set<Integer> set = new HashSet<>();
-        for(int n:nums) {
-            set.add(n);
-        } 
-                
-        return (set.size() > pick)? pick:set.size();
+        //N마리 중 N/2마리 & 종류마다 번호로 구분
+        //최대한 많은 종류의 포켓몬을 선택했을 때, 포켓몬 종류의 개수를 리턴
+        
+        //배열 [] 인덱스값+1 => 종류
+        //원소 => 카운트
+        
+        //1. 종류별 카운트할 HashMap 생성
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        //2. 배열 돌면서 Key 비교해서 등록하거나 카운트
+        for(int num:nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        //3. 맵 크기와 N/2 중 작은 개수를 리턴: 고를 수 있는 최대값이 N/2
+        return Math.min(map.size(), nums.length/2);
     }
 }
