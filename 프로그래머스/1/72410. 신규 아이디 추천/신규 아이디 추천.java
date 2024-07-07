a43a2b7531c678854a36fc0c3a11a19194690d84
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-07-08 01:02:54 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.15 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -18,17 +18,16 @@ class Solution {
         
         //5. 빈 문자열이면 a 대입
         recommend = recommend.isEmpty() ? "a" : recommend;
-
-        StringBuilder recommendSb = new StringBuilder(recommend);
         
         //6. 16자 이상이면 첫 15개 문자 이후는 제거
-        if(recommendSb.length() >= 16) {
-            recommendSb.delete(15, recommendSb.length());
+        if(recommend.length() >= 16) {
+            recommend = recommend.substring(0, 15);
             //7. 마지막 글자가 마침표라면 제거
-            if(recommendSb.charAt(recommendSb.length()-1) == '.') recommendSb.deleteCharAt(recommendSb.length()-1);
+            recommend = recommend.replaceAll("[.]$", "");
         }
         
         //8. 2자 이하라면 마지막 문자를 길이가 3이 될 때까지 중복하여 끝에 추가
+        StringBuilder recommendSb = new StringBuilder(recommend);
         while(recommendSb.length() < 3) {
             recommendSb.append(recommendSb.charAt(recommendSb.length()-1));
         }
