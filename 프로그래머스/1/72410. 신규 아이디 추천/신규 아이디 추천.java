8bbb60efcbe57a79f7282ad38a243cc655e2e1ce
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-07-08 00:50:07 +0900

    [level 1] Title: 신규 아이디 추천, Time: 2.18 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,42 @@
+class Solution {
+    /*
+    * 규칙에 맞지 않는 아이디를 입력받았을 경우, 입력된 아이디와 유사하고 규칙을 충족하는 아이디를 추천(리턴)
+    - 처리 과정에 따라 차례차례 진행
+    */
+    public String solution(String new_id) {
+        //1. 소문자로 치환
+        String recommend = new_id.toLowerCase();
+
+        //2. 가능한 글자 제외 모든 글자 제거
+        recommend = recommend.replaceAll("[^a-z0-9-_.]", "");
+        
+        //3. 마침표가 2개 이상 연속 => 1개로 치환
+        StringBuilder recommendSb = new StringBuilder();
+        for(int i=0; i<recommend.length(); i++) {
+            if(i>0 && recommendSb.charAt(recommendSb.length()-1) == '.' && recommend.charAt(i) == '.') continue;
+            else recommendSb.append(recommend.charAt(i));
+        }
+        
+        //4. 마침표가 처음/끝이면 제거
+        if(recommendSb.charAt(0) == '.') recommendSb.deleteCharAt(0);
+        if(recommendSb.length() > 0 && 
+           recommendSb.charAt(recommendSb.length()-1) == '.') recommendSb.deleteCharAt(recommendSb.length()-1);
+        
+        //5. 빈 문자열이면 a 대입
+        if(recommendSb.length() == 0) recommendSb.append("a");
+        
+        //6. 16자 이상이면 첫 15개 문자 이후는 제거
+        if(recommendSb.length() >= 16) {
+            recommendSb.delete(15, recommendSb.length());
+            //7. 마지막 글자가 마침표라면 제거
+            if(recommendSb.charAt(recommendSb.length()-1) == '.') recommendSb.deleteCharAt(recommendSb.length()-1);
+        }
+        
+        //8. 2자 이하라면 마지막 문자를 길이가 3이 될 때까지 중복하여 끝에 추가
+        while(recommendSb.length() < 3) {
+            recommendSb.append(recommendSb.charAt(recommendSb.length()-1));
+        }
+        
+        return recommendSb.toString();
+    }
+}
