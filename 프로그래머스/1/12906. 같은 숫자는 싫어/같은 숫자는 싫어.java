380c1515c70c3f9ed54f20ddf4018060d975a9b3
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-07-10 19:28:12 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 88.48 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,14 +1,26 @@
 import java.util.*;
+import java.util.stream.*;
 
 public class Solution {
+    /*
+    * 배열에서 연속으로 나타나는 숫자는 하나만 남기고 제거한 배열을 리턴
+    */
     public int[] solution(int []arr) {
-        List<Integer> list = new ArrayList<>();
-        list.add(arr[0]);
+        //1. 스택 생성
+        Deque<Integer> stack = new ArrayDeque<>();
         
+        //2. 배열 첫번째 숫자를 스택에 저장: 비교
+        stack.push(arr[0]);
+        
+        //3. 배열에 있는 숫자를 두번째 인덱스부터 순차적으로 순회
         for(int i=1; i<arr.length; i++) {
-            if(arr[i-1]!=arr[i])list.add(arr[i]);
+            //4. 스택에 있는 숫자와 다르면 현재 숫자를 스택에 저장
+            if(stack.peek() != arr[i]) stack.push(arr[i]);            
         }
         
-        return list.stream().mapToInt(Integer::valueOf).toArray();
+        //5. 스택에서 숫자를 꺼내어 배열에 저장해 리턴
+        List<Integer> list = stack.stream().collect(Collectors.toList());
+        Collections.reverse(list);
+        return list.stream().mapToInt(n->n).toArray();
     }
 }
