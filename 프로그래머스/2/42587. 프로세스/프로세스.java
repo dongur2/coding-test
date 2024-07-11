39a14b0964677147ed38770d30644200ac58b2ff
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-07-11 13:39:24 +0900

    [level 2] Title: 프로세스, Time: 0.53 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -6,16 +6,7 @@ class Solution {
     * 1. 대기 큐에서 프로세스를 꺼내는데,
     * 2. 대기 큐에 우선순위가 더 높은 프로세스가 있다면 꺼낸 프로세스를 다시 넣음
     * 3. 없다면 그대로 실행 -> 프로세스 종료
-    => 우선순위가 더 높은 프로세스가 있으면, 그 프로세스 차례가 올 때까지 대기 큐는 앞의 값이 뒤로 가는 회전 반복
-    
-    priorities: 대기 큐에 있는 프로세스의 우선순위가 담긴 배열
-    - 1 <= priorities.length <= 10^2
-    - 1 <= priorities[i] <= 9
-    - priorities의 원소가 우선순위: 클수록 높은 우선순위
-    
-    location: 몇 번째로 실행되는지 알고 싶은 프로세스의 위치
-    - 0 <= location <= (대기 큐에 있는 프로세스 수 - 1)
-    - location이 priorities에 있는 해당 프로세스의 인덱스
+    => 우선순위에 따라 큐에서 프로세스를 실행
     */
 
     public int solution(int[] priorities, int location) {
@@ -30,7 +21,7 @@ class Solution {
         //3. 대기 큐가 빌 때까지 순회: 모든 프로세스 완료
         int cnt = 0;
         while(!q.isEmpty()) {
-            //4. 원래 순서대로 계속해서 순회
+            //4. 원래 순서(우선순위)대로 계속해서 순회
             for(int i=0; i<priorities.length; i++) {
                 if(q.peek() == priorities[i]) {
                     q.poll();
