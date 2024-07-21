2a81a105db3a610e102d26d305f43da0c44d2c04
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-07-22 04:07:35 +0900

    [level 3] Title: 단어 변환, Time: 1.09 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    Map<String, Integer> cntMap;
+    
+    public int solution(String begin, String target, String[] words) {
+        cntMap = new HashMap<>();
+        return countPhase(begin, target, words);
+    }
+    
+    private int countPhase(String begin, String target, String[] words) {
+        //1. 완전 탐색 반복: 현재 문자열을 꺼내 방문
+        Queue<String> q = new ArrayDeque<>();
+        q.offer(begin); cntMap.put(begin, 0);
+        
+        while(!q.isEmpty()) {
+            String cur = q.poll();
+            
+            //2. words배열을 탐색하여 현재 문자열과 문자 1개만 다른 문자열을 찾아 예약, 카운트 저장
+            for(String word:words) {
+                int cnt = 0;
+                for(int i=0; i<word.length(); i++) {
+                    if(cur.charAt(i) != word.charAt(i)) {
+                        cnt++;
+                        if(cnt > 1) break;
+                    }
+                }
+                
+                if(cnt == 1) {
+                    q.offer(word); cntMap.put(word, cntMap.get(cur)+1);
+
+                    //3. 다음 문자열이 target과 같으면 카운트 리턴
+                    if(word.equals(target)) return cntMap.get(word);
+                }
+            }
+        }
+
+        //4. 반복문이 끝날 때까지 target 문자열로 변환하지 못했다면 0 리턴
+        return 0;
+    }
+}
