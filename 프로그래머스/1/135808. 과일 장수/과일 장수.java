aaa9d912b0cba8a0391035d20e32fe1b36bd9817
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-07-29 10:46:01 +0900

    [level 1] Title: 과일 장수, Time: 52.78 ms, Memory: 150 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,15 +1,24 @@
 import java.util.*;
 
 class Solution {
+    /*
+    주어진 사과들(score)로 m개가 들은 상자를 만들어서 판매했을 때, 얻을 수 있는 최대 이익
+    - 각 상자의 가격은 들어있는 사과 중 최소 점수 x m개 
+    
+    --> 높은 점수 사과부터 박스에 패키징하고 남는 걸 버리기
+    */
     public int solution(int k, int m, int[] score) {
-        Arrays.sort(score);
-        int box = score.length / m; // 총 박스 개수
+        //주어진 사과로 만들 수 있는 박스 개수
+        int box = score.length / m;
         
-        int sales = 0;
-        for(int i=1; i<=box; i++) {
-            int lowest = score[score.length - (m*i)]; // 박스당 최저 사과 점수
-            sales += lowest * m;
+        //주어진 사과를 점수 순서대로 정렬
+        Arrays.sort(score);
+        
+        //뒤에서부터 사과를 m개씩 박스에 넣고, 박스에 넣은 마지막 사과가 최소 점수가 되므로, 그 점수 * 사과 개수를 계산한 가격을 합산
+        int profit = 0;
+        for(int i=score.length-m; i>=0; i -= m) {
+            profit += score[i] * m;
         }
-        return sales;
+        return profit;
     }
 }
