c636841c46155a22f127e7621125410233ee2f85
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-07-29 10:30:21 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.91 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,23 +1,9 @@
 class Solution {
-    private String useStringBuilder(String s, String[] words) {
-        StringBuilder res = new StringBuilder(s);
-        for(int i=0; i<words.length; i++) {
-            while(res.indexOf(words[i]) > -1) {
-                res.replace(res.indexOf(words[i]), res.indexOf(words[i])+words[i].length(), i+"");
-            }
-        }
-        return res.toString();
-    }
-    
-    private String useString(String s, String[] words) {
-        for(int i=0; i<words.length; i++) {
-            s = s.replaceAll(words[i], i+"");
-        }
-        return s;
-    }
-    
     public int solution(String s) {
-        String[] words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        return Integer.parseInt(useString(s, words));        
+        String[] table = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i=0; i<table.length; i++) {
+            if(s.contains(table[i])) s = s.replaceAll(table[i], String.valueOf(i));
+        }
+        return Integer.parseInt(s);
     }
 }
