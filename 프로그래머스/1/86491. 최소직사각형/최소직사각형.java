dfb242930c4d3f60ea72513a3ffc491b140975b2
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-07-29 23:55:53 +0900

    [level 1] Title: 최소직사각형, Time: 2.01 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,15 +1,21 @@
+import java.util.*;
+
 class Solution {
+    /*
+    문제: 명함을 다 넣을 수 있는 지갑의 최소 크기를 리턴
+    - 1 <= 명함 개수(배열 길이) <= 10^4
+    - 가로/세로를 바꿀 수 있음 --> 긴쪽을 한쪽으로 고정
+    */
     public int solution(int[][] sizes) {
-        int width = Integer.MIN_VALUE;
-        int height = Integer.MIN_VALUE;
+        int maxW = 0, maxH = 0;
         
-        for(int i=0; i<sizes.length; i++) {
-            int w = Math.max(sizes[i][0], sizes[i][1]);
-            int h = Math.min(sizes[i][0], sizes[i][1]);
-            width = Math.max(width, w);
-            height = Math.max(height, h);
+        for(int[] size:sizes) {
+            int width = Math.max(size[0], size[1]);
+            int height = Math.min(size[0], size[1]);
+            maxW = Math.max(maxW, width);
+            maxH = Math.max(maxH, height);
         }
         
-        return width * height;
+        return maxW * maxH;
     }
 }
