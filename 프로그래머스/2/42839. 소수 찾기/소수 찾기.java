9f06e3e32df9fbc43142fc7339cd0bbb9198a7d0
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-08-04 12:46:42 +0900

    [level 2] Title: 소수 찾기, Time: 17.28 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,58 +1,56 @@
 import java.util.*;
 
 class Solution {
-    Set<Integer> set;
+    /*
+    ** 주어진 문자열에 포함된 숫자들로 만들 수 있는 '소수의 개수'를 반환
+    -- 013, 031.. 중복 불가능 && 순서에 의미 부여 ==> 순열
+    -- 011의 경우 011,011이 같은 글자가 되므로 세트로 저장해서 개수 세기
+    */
+    
     boolean[] visited;
+    Set<Integer> set; //소수를 저장
     
     public int solution(String numbers) {
-        set = new HashSet<>();
         visited = new boolean[numbers.length()];
+        set = new HashSet<>();
         
-        //주어진 문자열을 1글자씩 분할
-        String[] splited = numbers.split("");
-        
-        //1글자부터 주어진 문자열 길이까지 순회:
-        for(int size=1; size<=numbers.length(); size++) {
-            //반복문을 도는 길이의 숫자 조합을 모두 생성하여 소수인지 확인 후 카운트: 매개변수로 새로운 StringBuilder 전달 
-            makeNumberAndFindPrime(splited, size, new StringBuilder());
+        //만들 수 있는 모든 글자수로 숫자 생성
+        for(int len=1; len<=numbers.length(); len++) {
+            backtrack(numbers, len, new StringBuilder());
         }
-
-        //카운트한 소수 개수 리턴
+        
         return set.size();
     }
     
-    private void makeNumberAndFindPrime(String[] splited, int size, StringBuilder number) {
-        //base case: 현재 반복문을 돌고있는 문자열 길이와 만든 숫자 길이가 같다면:
-	    if(number.length() == size) {
-            //소수인지 확인 후, 소수라면 정답 세트에 저장하고(아니라면 아무 작업도 하지 않고) 재귀 중지
+    void backtrack(String numbers, int len, StringBuilder number) {
+        //base case: 만드려는 글자수와 일치하면 소수 확인 후 저장
+        if(number.length() == len) {
             int num = Integer.parseInt(number.toString());
-            if(isPrime(num)) set.add(num);
-            return;
-        }    
+            if(isPrime(num)) set.add(num); return;
+        }
         
-        //recursive call
-        //분할한 문자열 배열을 처음부터 끝까지 순회:
-        for(int i=0; i<splited.length; i++) {
-            if(!visited[i]) {
-                //현재 노드 방문 체크 & 문자열에 추가
-                visited[i] = true;
-                number.append(splited[i]);
-
-                //backtrack(문자열): 다음 노드 탐색에 대한 하청
-                makeNumberAndFindPrime(splited, size, number);
-
-                //현재 노드 방문 체크 해제 & 문자열에 추가했던 현재 노드 삭제
-                visited[i] = false;
-                number.deleteCharAt(number.length()-1);
-            }
+        /* recursive call:전체 배열 순회
+        이미 방문한 원소라면 무시
+        아니라면 방문 체크 & 문자열에 추가
+        추가한 문자열에 더해서 탐색 시작 - 하청
+        하청 후 방문 체크 무효 & 문자열에서 삭제
+        */
+        for(int i=0; i<numbers.length(); i++) {
+            if(visited[i]) continue;
+            
+            visited[i] = true; number.append(numbers.charAt(i)+"");
+            backtrack(numbers, len, number);
+            visited[i] = false; number.deleteCharAt(number.length()-1);
         }
     }
     
-    private boolean isPrime(int num) {
-        if(num == 0 || num == 1) return false;
-        for(int n=2; n <= Math.sqrt(num); n++) {
-            if(num % n == 0) return false;
+    boolean isPrime(int number) {
+        if(number == 0 || number == 1) return false;
+        
+        for(int i=2; i<=Math.sqrt(number); i++) {
+            if(number % i == 0) return false;
         }
+        
         return true;
     }
 }
