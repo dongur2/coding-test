9168b28ba278538cc63b437887248eadfd643fca
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-08-05 17:40:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.03 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,54 +1,58 @@
 import java.util.*;
 
 class Solution {
-    //4방향 이동
+    /*
+    ** 상대 팀 진영에 도착하기 위해 지나야 하는 '최소' 칸의 개수 반환
+    - 상대 팀 진영[n][m]에 도착할 수 없으면 -1 반환
+    - 캐릭터는 [0][0]에서 시작
+    - 상하좌우로 값이 1인 칸으로 이동 가능; 주어진 맵 바깥으로 나갈 수 없음
+    
+    최소 칸의 개수니까 BFS
+    */
+    
+    int rowLen, colLen;
+    boolean[][] visited;
+    
     int[] rowArr = {0, 1, 0, -1};
     int[] colArr = {1, 0, -1, 0};
     
-    int rowLen, colLen;
-    boolean[][] checked;
-    
     public int solution(int[][] maps) {
-        rowLen = maps.length;
-        colLen = maps[0].length;
-        checked = new boolean[rowLen][colLen];
+        rowLen = maps.length; colLen = maps[0].length;
+        visited = new boolean[rowLen][colLen];
         
-        return bfs(maps);
+        return findWays(maps);
     }
     
-    private int bfs(int[][] maps) {
-        //시작 위치(0, 0)를 방문 체크하고 대기열에 카운트와 함께 추가
-        Queue<int[]> waiting = new ArrayDeque<>();
-        checked[0][0] = true;
-        waiting.offer(new int[]{0, 0, 1});
+    int findWays(int[][] maps) {
+        //start
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{0, 0, 1}); visited[0][0] = true;
         
-        //대기열이 소진될 때까지 반복:
-        while(!waiting.isEmpty()) {
-	        //대기열에서 가져온 노드에 방문
-            int[] cur = waiting.poll();
+        //find way
+        while(!q.isEmpty()) {
+            //current node
+            int[] cur = q.poll();
             int curR = cur[0];
             int curC = cur[1];
             int curCnt = cur[2];
             
-            //인접 노드 4방향을 탐색하여 
+            //next node
             for(int i=0; i<4; i++) {
-                int nxtR = curR + rowArr[i];
-                int nxtC = curC + colArr[i];
+                int nextR = curR + rowArr[i];
+                int nextC = curC + colArr[i];
                 
-                //인접 노드 중에 상대 진영(N-1, M-1)이 있다면: 즉시 카운트를 추가하여 리턴
-                if(nxtR == rowLen - 1 && nxtC == colLen - 1) return curCnt + 1;
-                
-                //아직 방문하지 않은 이동 가능한 노드가 있다면: 방문 체크 & 대기열에 카운트를 추가하여 저장
-                if(isValid(maps, nxtR, nxtC)) {
-                    checked[nxtR][nxtC] = true;
-                    waiting.offer(new int[]{nxtR, nxtC, curCnt + 1});
+                if(isValid(maps, nextR, nextC)) {
+                    if(nextR == rowLen - 1 && nextC == colLen - 1) return curCnt + 1;
+                    
+                    q.offer(new int[]{nextR, nextC, curCnt + 1});
+                    visited[nextR][nextC] = true;
                 }
             }
         }
         return -1;
     }
     
-    private boolean isValid(int[][] maps, int row, int col) {
-        return row >= 0 && row < rowLen && col >= 0 && col < colLen && maps[row][col] == 1 && !checked[row][col];
+    boolean isValid(int[][] maps, int row, int col) {
+        return row >= 0 && row < rowLen && col >= 0 && col < colLen && !visited[row][col] && maps[row][col] == 1;
     }
 }
