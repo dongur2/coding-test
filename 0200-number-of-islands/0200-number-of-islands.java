e7028cde69cd5d882e7ef3194465447087556674
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-08-05 13:28:35 +0900

    Time: 6 ms (24.06%), Space: 48.6 MB (96.91%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,70 +1,60 @@
 import java.util.*;
 
 class Solution {
-    //상하좌우로 좌표이동
+    /*
+     ** 주어진 맵(grid)에 있는 섬의 개수를 반환
+     1. 전체 노드를 차례로 순회
+     2. 땅(1)을 만나면 그 노드에서 상하좌우로 연결된 땅을 탐색: 연결된 땅이 없을 때까지
+        - 그 땅이 방문한 적 없는 새로운 땅일 경우에만 탐색을 진행
+     */
+
+    int rowLen, colLen;
+    boolean[][] visited;
+
     int[] rowArr = {0, 1, 0, -1};
     int[] colArr = {1, 0, -1, 0};
 
-    int rowLength;
-    int colLength;
-
-    char[][] map;
-    boolean[][] checked;
-
     public int numIslands(char[][] grid) {
-        rowLength = grid.length;
-        colLength = grid[0].length;
-        map = grid; //편리하게 사용 위해 맵 복사
-        checked = new boolean[rowLength][colLength];
+        rowLen = grid.length; colLen= grid[0].length;
+        visited = new boolean[rowLen][colLen];
 
-        return findIslands();
-    }
-
-    private int findIslands() {
-        int cnt = 0;
-
-        //모든 노드를 순차적으로 순회:
-        for(int row=0; row < rowLength; row++) {
-            for(int col=0; col < colLength; col++) {
-                //방문하지 않았던 육지에 도착:
-                if(map[row][col] == '1' && !checked[row][col]) {
-    	    	    //도착한 "섬"의 면적을 모두 탐색하여 방문 여부 체크 & 섬 개수 카운트
-                    checkIslandArea(row, col); cnt++;
+        int answer = 0;
+        for(int r=0; r<rowLen; r++) {
+            for(int c=0; c<colLen; c++) {
+                if(grid[r][c] == '1' && !visited[r][c]) {
+                    bfs(grid, r, c);
+                    answer++;
                 }
             }
         }
-        return cnt;
+        return answer;
     }
 
-    private void checkIslandArea(int row, int col) {
-        //탐색 시작 위치를 대기열에 추가 & 방문 체크
-        Queue<int[]> waiting = new ArrayDeque<>();
-        waiting.offer(new int[]{row, col});
-        checked[row][col] = true;
+    void bfs(char[][] grid, int r, int c) {
+        //start
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{r, c}); visited[r][c] = true;
 
-        //대기열이 소진될 때까지:
-        while(!waiting.isEmpty()) {
-            //대기열에서 탐색 시작 위치를 획득
-            int[] cur = waiting.poll();
+        while(!q.isEmpty()) {
+            //current node
+            int[] cur = q.poll();
             int curR = cur[0];
             int curC = cur[1];
 
-            //현재 위치의 상하좌우 좌표를 탐색:
+            //next node
             for(int i=0; i<4; i++) {
-                int nxtR = curR + rowArr[i];
-                int nxtC = curC + colArr[i];
+                int nextR = curR + rowArr[i];
+                int nextC = curC + colArr[i];
 
-                //맵 밖으로 나가지 않고, 방문하지 않은 육지가 있을 경우: 
-                if(isValid(nxtR, nxtC)) {
-                    //방문 여부 체크 & 그 육지에서 다시 상하좌우를 재탐색
-                    checked[nxtR][nxtC] = true;
-                    checkIslandArea(nxtR, nxtC);
+                if(isValid(grid, nextR, nextC)) {
+                    q.offer(new int[]{nextR, nextC});
+                    visited[nextR][nextC] = true;
                 }
             }
         }
     }
 
-    private boolean isValid(int row, int col) {
-        return row >= 0 && row < rowLength && col >= 0 && col < colLength && map[row][col] == '1' && !checked[row][col];
+    boolean isValid(char[][] grid, int row, int col) {
+        return row >= 0 && row < rowLen && col >= 0 && col < colLen && !visited[row][col] && grid[row][col] == '1';
     }
 }
