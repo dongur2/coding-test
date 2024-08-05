f0aa1a893288b0741e86035d62033682e2206a1f
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-08-05 22:40:17 +0900

    [level 2] Title: 괄호 회전하기, Time: 35.64 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,36 +1,35 @@
 import java.util.*;
 
 class Solution {
+    /*
+    ** 처음부터 끝까지 회전시켰을 때, 올바른 괄호 문자열이 되는 횟수를 반환
+    */
     public int solution(String s) {
-        //1. 문자열 두 개를 이어붙인 새로운 문자열 생성
-        String line = s + s;
+        int answer = 0;
+        String temp = s + s;
         
-        //2. 배열 크기를 유지한 윈도우를 끝까지 이동하며 순회
-        int cnt = 0;
+        //1.한칸씩 뒤로 윈도우 이동
         for(int i=0; i<s.length(); i++) {
-            //3. 변경한 문자열, 시작인덱스와 끝인덱스를 올바른 괄호 문자열 판별 메서드로 넘김
-            if(isValid(line, i, i+s.length())) cnt++;
+            String t = temp.substring(i, i+s.length());
+            
+            //2.올바른 괄호로만 이루어져있는지 검사
+            if(isValid(t)) answer++;
         }
-        return cnt;
+        return answer;
     }
     
-    private boolean isValid(String line, int start, int end) {
-        Deque<Character> stack = new ArrayDeque<>();
-        
-        String t = "({[";
-        //시작~끝인덱스 사이 순회
-        for(int i=start; i<end; i++) {
-            //열린 괄호는 스택에 저장
-            if(t.contains(line.charAt(i)+"")) stack.push(line.charAt(i));
-            else {
-                //닫힌 괄호가 빈 스택에 들어가면 즉시 false 리턴
-                if(stack.isEmpty()) return false;
-                
-                if(line.charAt(i) == ')' && stack.peek() == '('
-                  || line.charAt(i) == '}' && stack.peek() == '{'
-                  || line.charAt(i) == ']' && stack.peek() == '[') stack.pop();
+    boolean isValid(String word) {
+        Stack<Character> stack = new Stack<>();
+            for(char c : word.toCharArray()) {
+                if(c == '[' || c == '{' || c == '(') stack.push(c);
+                else {
+                    if(stack.isEmpty()) return false;
+                    else if(c == ']' && stack.peek() == '[') stack.pop();
+                    else if(c == '}' && stack.peek() == '{') stack.pop();
+                    else if(c == ')' && stack.peek() == '(') stack.pop();
+                }
             }
-        }
         return stack.isEmpty();
     }
 }
+
