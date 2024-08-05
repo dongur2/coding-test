974935b748723dfcd58a65f18c30add47d5dff84
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-08-05 21:04:35 +0900

    [level 2] Title: 미로 탈출, Time: 6.33 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,81 +1,90 @@
 import java.util.*;
 
 class Solution {
+    /*
+    ** 미로를 빠져나가기 위해 필요한 '최소' 시간 반환
+    - 빠져나갈 수 없으면 -1 반환
+    - 규칙: 레버를 당긴 후에 출구로 탈출할 수 있음
+    - X인 칸은 이동할 수 없음
+    
+    최소 시간을 구해야 하므로 BFS로 접근
+    
+    1. 시작/레버/출구 위치를 찾기
+    2. 시작 위치에서 탐색 -> 레버로 가는 최소 루트 탐색
+    3. 레버에 도착 후 출구로 가는 최소 루트 탐색
+    */
+    
     int[] rowArr = {0, 1, 0, -1};
     int[] colArr = {1, 0, -1, 0};
     
     int rowLen, colLen;
-    int[] start, lever, exit;
-    String[] mapCopied;
-    boolean[][] checked;
+    Map<Character, int[]> checkpoints; //위치 저장
+    boolean[][] visited; // 시작->레버, 레버->출구 방문 체크 별개
     
     public int solution(String[] maps) {
-        mapCopied = maps;
-        rowLen = maps.length; 
+        rowLen = maps.length;
         colLen = maps[0].length();
-        checked = new boolean[rowLen][colLen];
+        checkpoints = new HashMap<>();
+        visited = new boolean[rowLen][colLen];
         
-        return findTheShortestEscape();
+        return countMinimum(maps);
     }
     
-    private int findTheShortestEscape() {
-        //시작점/레버/출구 위치를 찾아서 변수에 저장
-        findPoints();
-        
-        //탈출에 필요한 최단 시간 탐색
-        //BFS: 시작점에서 출발해 레버에 도착 & 레버에서 출발해 출구에 도착하는 최소 거리(시간) 탐색 - 레버에 도착했을 경우에만 진행
-        //-> 시작위치(start, lever), 도착위치(lever, exit) 
-        return findRouteTo();
-    }
-    
-    private void findPoints() { //O(NM)?
-        for(int i=0; i<rowLen; i++) {
-            if(mapCopied[i].contains("S")) start = new int[]{i, mapCopied[i].indexOf("S")};
-            if(mapCopied[i].contains("E")) exit = new int[]{i, mapCopied[i].indexOf("E")};
-            if(mapCopied[i].contains("L")) lever = new int[]{i, mapCopied[i].indexOf("L")};
-        }
-    }
-    
-    private int findRouteTo() {
-        Queue<int[]> waiting = new ArrayDeque<>();
-        checked[start[0]][start[1]] = true;
-        waiting.offer(new int[]{start[0], start[1], 0});
-        
+    int countMinimum(String[] maps) {
         boolean isLeverOn = false;
         
-        while(!waiting.isEmpty()) {
-            int[] cur = waiting.poll(); //현재 방문 노드
+        //시작-레버-출구 위치 찾기
+        findCheckpoints(maps);
+        
+        //시작->레버->출구까지 탐색
+        Queue<int[]> q = new ArrayDeque<>();
+        
+        //start
+        int[] startPoint = checkpoints.get('S');
+        q.offer(new int[]{startPoint[0], startPoint[1], 0}); //[row, col, count]
+        visited[startPoint[0]][startPoint[1]] = true;
+        
+        while(!q.isEmpty()) {
+            //cur
+            int[] cur = q.poll();
             int curR = cur[0];
             int curC = cur[1];
             int curCnt = cur[2];
             
-            for(int i=0; i<4; i++) { //방문한 노드의 상하좌우를 탐색
-                int nxtR = curR + rowArr[i];
-                int nxtC = curC + colArr[i];
+            //next node
+            for(int i=0; i<4; i++) {
+                int nextR = curR + rowArr[i];
+                int nextC = curC + colArr[i];
                 
-                //이동할 수 있는 인접 노드 중에 레버가 존재하면 레버 외 예약 노드 비움
-                if(nxtR == lever[0] && nxtC == lever[1] && !checked[nxtR][nxtC]) {
-                    isLeverOn = true;
-                    
-                    checked = new boolean[rowLen][colLen]; //왔던 길도 갈 수 있도록 방문 체크 초기화
-                    waiting.clear(); //레버에서 시작하도록 나머지 대기열 비움
-                    waiting.offer(new int[]{nxtR, nxtC, curCnt+1});
-                    checked[nxtR][nxtC] = true; 
-                    break;
-                    
-                } else if(nxtR == exit[0] && nxtC == exit[1] && isLeverOn) return curCnt + 1;
+                if(isValid(maps, nextR, nextC)) {
+                    q.offer(new int[]{nextR, nextC, curCnt+1});
+                    visited[nextR][nextC] = true;
                 
-                if(isValid(nxtR, nxtC)) {
-                    waiting.offer(new int[]{nxtR, nxtC, curCnt+1});
-                    checked[nxtR][nxtC] = true;
+                    //레버에 도착하면 대기열, 방문 체크 초기화 & 레버만 체크하고 출구까지 탐색 시작
+                    if(nextR == checkpoints.get('L')[0] && nextC == checkpoints.get('L')[1]) {
+                        q.clear(); visited = new boolean[rowLen][colLen]; //initialize
+                        isLeverOn = true; //lever on
+                        visited[nextR][nextC] = true; q.offer(new int[]{nextR, nextC, curCnt+1}); //add q
+                        break;
+                    }
+                    
+                    //레버를 당긴 후 출구 도착
+                    if(isLeverOn && nextR == checkpoints.get('E')[0] && nextC == checkpoints.get('E')[1]) return curCnt + 1;
                 }
             }
         }
         return -1;
     }
     
-    private boolean isValid(int row, int col) {
-        return row >= 0 && row < rowLen && col >= 0 && col < colLen && mapCopied[row].charAt(col) != 'X' && !checked[row][col];
+    void findCheckpoints(String[] maps) {
+        for(int r=0; r<rowLen; r++){
+            if(maps[r].indexOf('S') > -1) checkpoints.put('S', new int[]{r, maps[r].indexOf('S')});
+            if(maps[r].indexOf('L') > -1) checkpoints.put('L', new int[]{r, maps[r].indexOf('L')});
+            if(maps[r].indexOf('E') > -1) checkpoints.put('E', new int[]{r, maps[r].indexOf('E')});
+        }
     }
     
+    boolean isValid(String[] maps, int row, int col) {
+        return row >= 0 && row < rowLen && col >= 0 && col < colLen && !visited[row][col] && maps[row].charAt(col) != 'X';
+    }
 }
