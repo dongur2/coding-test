0ce83bb791b95fdcb3df6a182e0e1569d961f388
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-08-08 19:46:48 +0900

    [level 2] Title: 괄호 회전하기, Time: 32.26 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,35 +1,29 @@
 import java.util.*;
 
 class Solution {
-    /*
-    ** 처음부터 끝까지 회전시켰을 때, 올바른 괄호 문자열이 되는 횟수를 반환
-    */
     public int solution(String s) {
         int answer = 0;
-        String temp = s + s;
-        
-        //1.한칸씩 뒤로 윈도우 이동
-        for(int i=0; i<s.length(); i++) {
-            String t = temp.substring(i, i+s.length());
+        String t = s + s;
             
-            //2.올바른 괄호로만 이루어져있는지 검사
-            if(isValid(t)) answer++;
+        for(int i=0; i<s.length(); i++) {
+            if(isValid(t.substring(i, i+s.length()))) answer++;
         }
+        
         return answer;
     }
     
     boolean isValid(String word) {
-        Stack<Character> stack = new Stack<>();
-            for(char c : word.toCharArray()) {
-                if(c == '[' || c == '{' || c == '(') stack.push(c);
-                else {
-                    if(stack.isEmpty()) return false;
-                    else if(c == ']' && stack.peek() == '[') stack.pop();
-                    else if(c == '}' && stack.peek() == '{') stack.pop();
-                    else if(c == ')' && stack.peek() == '(') stack.pop();
-                }
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char w:word.toCharArray()) {
+            if(w == '(' || w == '{' || w == '[') stack.push(w);
+            else {
+                if(stack.isEmpty()) return false;
+                else if(stack.peek() == '(' && w == ')' 
+                        || stack.peek() == '{' && w == '}' 
+                        || stack.peek() == '[' && w == ']') stack.pop();
+                else return false;
             }
+        }
         return stack.isEmpty();
     }
 }
-
