daa1b6e560b162f970fa453eab4eebda2256fe2f
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-08-09 10:11:37 +0900

    [level 1] Title: 소수 만들기, Time: 3.27 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/README.md
+++ 프로그래머스/1/12977. 소수 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 소수 만들기 - 12977 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12977?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12977) 
 
 ### 성능 요약
 
-메모리: 73 MB, 시간: 0.02 ms
+메모리: 73.6 MB, 시간: 3.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 01일 21:15:13
+2024년 08월 09일 10:11:35
 
 ### 문제 설명
 
--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,27 +1,33 @@
+import java.util.*;
+
 class Solution {
-    //소수 판별 메서드
-    public boolean isPrime(int num) {
-        for(int i=2; i<num; i++) {
+    //nums 배열의 숫자들 중 3개를 더해서 소수가 되는 경우 개수를 리턴
+    int answer = 0, sum = 0;
+    
+    public int solution(int[] nums) {
+        //세개 고르기: 순서의미없는 중복 불가 '조합'
+        backtrack(nums, 0, 0, 0);
+        return answer;
+    }
+    
+    void backtrack(int[] nums, int start, int cnt, int sum) {
+        //basecase: 세개 고르기 완료
+        if(cnt == 3) {
+            if(isPrime(sum)) answer++;
+            return;
+        }
+        
+        //recursive: 세개 고르자
+        for(int i=start; i<nums.length; i++) {
+            backtrack(nums, i+1, cnt+1, sum + nums[i]);
+        }
+    }
+    
+    boolean isPrime(int num) {
+        if(num == 0 || num == 1) return false;
+        for(int i=2; i<=Math.sqrt(num); i++) {
             if(num % i == 0) return false;
         }
         return true;
-    }
-    
-    public int solution(int[] nums) {
-        // 3개 수를 더했을 때 소수가 되는 경우의 수 리턴: 숫자는 중복 불허
-        int cnt = 0;
-
-        // 1.가능한 3개 조합 모두 순회
-        for(int i=0; i<nums.length; i++) {
-            for(int j=i+1; j<nums.length; j++) {
-                for(int l=j+1; l<nums.length; l++) {
-                    // 2. 소수 판별 후 소수라면 카운트
-                    if(isPrime(nums[i]+nums[j]+nums[l])) {
-                        cnt++;
-                    }
-                }
-            }
-        }
-        return cnt;
     }
 }
