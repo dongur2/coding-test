aa08127d1b992525c1a1de02b9ffbf983d47fa94
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-08-09 09:24:57 +0900

    [level 3] Title: 가장 먼 노드, Time: 71.65 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,78 +1,70 @@
 import java.util.*;
 
+/*
+N개 노드(1~N-1번) 중에 1번 노드에서 가장 멀리 떨어진 노드의 개수 리턴
+- edge[0] == [3,6] 3번 노드와 6번 노드가 연결됨
+
+그래프화: 해시테이블 - {노드:연결노드목록(List)}
+DFS끝까지 순회
+최대 간선 업데이트
+최대 간선 가진 노드 개수 카운트
+*/
 class Solution {
-    /*
-    ** 1번 노드에서 가장 먼 노드의 개수 반환
-    - N개 노드 (1~N번)
-    */
-    
-    int maximum = 0; //가장 먼 간선 개수 저장
-    
-    Map<Integer, List<Integer>> graph; //그래프 {노드 번호 : 연결된 노드 목록}
-    Map<Integer, Integer> nodes; //{노드 번호 : 노드 1로부터 연결된 간선 개수}
+    Map<Integer, List<Integer>> graph;
+    boolean[] visited;
     
     public int solution(int n, int[][] edge) {
         graph = new HashMap<>();
-        nodes = new HashMap<>();
+        visited = new boolean[n+1];
         
-        return countFar(n, edge);
-    }
-    
-    int countFar(int n, int[][] edge) {
-        //1. 그래프화
+        //그래프화
         makeGraph(edge);
         
-        //2. bfs
-        findFarNode(n, edge);
-        
-        //3. count: 가장 많은 간선 개수를 가진 값을 카운트해서 반환
-        int answer = 0;
-        for(int node:nodes.values()) {
-            if(node == maximum) answer++;
-        }
-        return answer;
-        
-        // return (int)(nodes.values().stream().filter(l->l==maximum).count());
+        //bfs
+        return bfs();
     }
     
-    // {노드:연결노드목록} 그래프로 변환
     void makeGraph(int[][] edge) {
-        for(int i = 0; i < edge.length; i++) {
-            addConnection(edge[i][0], edge[i][1]);
-            addConnection(edge[i][1], edge[i][0]);
+        for(int[] connection : edge) {
+            addConnection(connection[0], connection[1]);
+            addConnection(connection[1], connection[0]);
         }
     }
     
-    //노드 간 연결관계 추가
-    void addConnection(int baseNode, int addedNode) {
-        if (!graph.containsKey(baseNode)) graph.put(baseNode, new ArrayList<>());
-        graph.get(baseNode).add(addedNode);
+    void addConnection(int baseNode, int addNode) {
+        List<Integer> connects = graph.getOrDefault(baseNode, new ArrayList<>());
+        connects.add(addNode);
+        graph.put(baseNode, connects);
     }
     
-    //전체 그래프 순회하며 간선 개수 카운트
-    void findFarNode(int n, int[][] edge) {
-        boolean[] visited = new boolean[n+1];
+    int bfs() {
+        int maxFar = 0, answer = 0;
         
         //start
-        Queue<Integer> q = new ArrayDeque<>();
-        q.offer(1); 
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{1, 0});
         visited[1] = true;
-        nodes.put(1, 0); 
         
         while(!q.isEmpty()) {
             //cur
-            int cur = q.poll();
+            int[] cur = q.poll();
+            int curNode = cur[0];
+            int curEdge = cur[1];
+            
+            if(maxFar < curEdge) {
+                maxFar = curEdge;
+                answer = 1;
+            }
+            else if (maxFar == curEdge) answer++;
             
             //near
-            for(int nxt : graph.get(cur)) {
-                if(visited[nxt]) continue;
-                
-                q.offer(nxt); 
-                visited[nxt] = true;
-                nodes.put(nxt, nodes.get(cur)+1); 
-                
-                maximum = Math.max(maximum, nodes.get(cur)+1); //가장 먼 간선 개수 업데이트
+            for(int nxt : graph.get(curNode)) {
+                if(!visited[nxt]) {
+                    q.offer(new int[]{nxt, curEdge+1});
+                    visited[nxt] = true;
+                }
             }
         }
-    }
+        return answer;
+    }            
 }
