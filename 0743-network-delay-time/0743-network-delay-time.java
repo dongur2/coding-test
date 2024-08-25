a1b6ba52797d32faba20629369f1c864b96738f9
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-08-26 00:15:04 +0900

    Time: 24 ms (25.76%), Space: 47.1 MB (89.52%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,67 +1,76 @@
 import java.util.*;
 
 class Solution {
+    /*
+     Q. k번 노드에서 출발하여 모든 n번 노드를 통과할 수 있는 최소 거리를 반환
+     - n번 노드에 갈 수 없으면 -1 반환
+     times[i] = [출발 노드, 타겟 노드, 가중치]
+     */
+
     Map<Integer, List<Edge>> graph;
-    int[] costs;
-    int INF = Integer.MAX_VALUE;
-    
+    final int INF = Integer.MAX_VALUE;
+
     public int networkDelayTime(int[][] times, int n, int k) {
-        //그래프화
         makeGraph(times);
-        
-        //다익스트라
-        return dijkstra(n, k);
+        return countMinimum(n, k);
     }
-    
+
+    class Edge {
+        public int node; public int time;
+        public Edge(int node, int time) {
+            this.node = node; this.time = time;
+        }
+    }
+
     void makeGraph(int[][] times) {
         graph = new HashMap<>();
-        
+
         for(int[] time:times) {
             List<Edge> list = graph.getOrDefault(time[0], new ArrayList<>());
             list.add(new Edge(time[1], time[2]));
-            graph.put(time[0], new ArrayList<>(list));
+            graph.put(time[0], list);
         }
     }
-  
-    //노드 번호, 비용
-    class Edge implements Comparable<Edge> {
-        public int node; public int cost;
-        public Edge(int node, int cost) {
-            this.node = node; this.cost = cost;
-        }
-        
-        @Override
-        public int compareTo(Edge e) {
-            return this.cost - e.cost;
-        }
-    }
-    
-    int dijkstra(int n, int k) {
-        int[] costs = new int[n+1]; //최소비용 저장
-        Arrays.fill(costs, INF); //무한대로 초기화
 
-        Queue<Edge> pq = new PriorityQueue<>(); //heap
-        
-        //시작점 초기화
-        costs[k] = 0; //비용 0
-        pq.add(new Edge(k, 0)); //힙에 시작점 추가
-        
-        //예약
+    class Entry implements Comparable<Entry>{
+        public int to; public int cost;
+        public Entry(int to, int cost) {
+            this.to = to; this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Entry o) {
+            return this.cost - o.cost;
+        }
+    }
+
+    int countMinimum(int n, int k) {
+        //ready: set INF
+        int[] costs = new int[n+1];
+        Arrays.fill(costs, INF);
+
+        //make heap: priority queue - min heap
+        Queue<Entry> pq = new PriorityQueue<>();
+
+        //set start point
+        costs[k] = 0;
+        pq.add(new Entry(k, 0));
+
+        //while loop
         while(!pq.isEmpty()) {
-            Edge cur = pq.poll(); //지금까지의 비용 가장 적은 노드 방문
-            
-            if(costs[cur.node] < cur.cost || graph.get(cur.node) == null) continue; //이미 저장된 현재 노드로 오는 데 필요한 최소 비용보다 현재 노드가 가진 비용이 크다면 무시: 이미 비용 초과
-            
-            for(Edge near : graph.get(cur.node)) { //인접 노드 업데이트
-                int newCost = costs[cur.node] + near.cost;
-                if(newCost < costs[near.node]) {
-                    pq.add(new Edge(near.node, newCost));
-                    costs[near.node] = newCost;
-                }
+            //cur
+            Entry cur = pq.poll();
+            if(costs[cur.to] < cur.cost || graph.get(cur.to) == null) continue; //저장된 타겟노드로의 비용이 지금 들고있는 비용이 크다면 무시(이미 방문) 또는 인접 노드가 없으면 무시
+
+            //next
+            for(Edge nxt:graph.get(cur.to)) {
+                int newCost = costs[cur.to] + nxt.time;
+                if(costs[nxt.node] > newCost) {
+                    costs[nxt.node] = newCost;
+                    pq.add(new Entry(nxt.node, newCost));
+                } //인접노드에 방문한 적 없으면 비용 업데이트 & 예약 - 최소
             }
         }
-        
-        //인덱스 0을 제외하고 INF값이 없으면 모든 노드 순회 완료
-        return Arrays.stream(costs).filter(cnt -> cnt == INF).count() == 1L ? Arrays.stream(costs).skip(1).max().getAsInt() : -1;
+        return Arrays.stream(costs).filter(c -> c == INF).count() == 1L ? Arrays.stream(costs).skip(1).max().getAsInt() : -1;
     }
 }
