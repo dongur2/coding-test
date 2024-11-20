e160f893871fa8e4e871d1058b856054722e6516
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-11-21 02:08:25 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 304.20 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,6 +1,11 @@
 import java.util.*;
-
 class Solution {
+    /*
+    Q. 완주하지 못한 선수의 이름 반환
+    - 참여 선수 배열
+    - 완주 선수 배열
+    -> 참여 배열에는 있으나, 완주 배열에 없는 이름을 반환
+    */
     public String solution(String[] participant, String[] completion) {
         Arrays.sort(participant);
         Arrays.sort(completion);
@@ -8,6 +13,7 @@ class Solution {
         for(int i=0; i<completion.length; i++) {
             if(!participant[i].equals(completion[i])) return participant[i];
         }
+        
         return participant[participant.length-1];
     }
 }
