233cb01f0ec4a68924f5d9eb981d0a43aff001af
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-11-26 00:16:22 +0900

    [level 2] Title: 피로도, Time: 2.84 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,39 +2,37 @@ import java.util.*;
 
 class Solution {
     /*
-    ** 탐험할 수 있는 최대 '던전 수'를 반환
-    -- 탐험하는 던전 순서에 따라 탐험 가능한 던전 수가 달라짐
-    ---> 같은 던전을 중복 탐험 불가능 && 순서에 의미를 부여 ==> 순열
-    -- [최소 필요 피로도, 소모 피로도]: 던전에 방문했을 때, 유저의 피로도가 최소 필요 피로도 이상이어야 탐험 진행
+    Q. 탐험 가능한 최대 던전 개수를 반환
+    - [최소 피로도, 소모 피로도]
+    - k: 현재 피로도
+    
+    - 한번 방문한 던전은 재방문하지 않음 (중복 불가)
+    - 순서 상관 있음 [2,3] [3,2] 둘 다 유효
+      => 순열(permutation)
     */
     
-    boolean[] visited;
-    int answer = 0;
+    int res = -1;
     
     public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
-        
-        backtrack(k, dungeons, 0);
-        
-        return answer;
+        visit(k, dungeons, new boolean[dungeons.length], 0);
+        return res;
     }
     
-    void backtrack(int k, int[][] dungeons, int cnt) {
-        /* recursive call: 전체 던전을 순회
-            현재 방문한 던전이 이전에 방문한 적 있다면(탐험 완료) 무시
-            아니라면, 유저 피로도와 현재 방문한 던전의 최소 필요 피로도를 비교: 
-            - 필요 피로도 이상이라면 방문 체크 & 탐험 카운트 & 그 다음 던전 탐색 (하청)
-            - 하청 후 방문 체크 무효
-            
-            - 전역 변수로 저장한 탐험 횟수와 현재 탐험 횟수를 비교하여 최대값 저장
-        */
+    private void visit(int k, int[][] dungeons, boolean[] visited, int cnt) {
+        //주어진 던전을 순차적으로 순회
         for(int i=0; i<dungeons.length; i++) {
-            if(!visited[i] && k >= dungeons[i][0]) {
-                visited[i] = true;
-                backtrack(k - dungeons[i][1], dungeons, cnt+1);
-                visited[i] = false;
-            }
+            //방문한 던전이거나, 현재 피로도 < 최소 피로도라면 무시
+            if(visited[i] || k < dungeons[i][0]) continue;
+            
+            //던전을 방문: 피로도 감소 & 방문 처리 & 방문 카운트
+            visited[i] = true;
+            visit(k - dungeons[i][1], dungeons, visited, cnt+1);
+            
+            //이후 방문 처리 무효
+            visited[i] = false;
         }
-        answer = Math.max(answer, cnt);
+        
+        //최대값 비교 후 바인딩
+        res = Math.max(res, cnt);
     }
 }
