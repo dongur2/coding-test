408218755283fd20acfd826a7010faad1065af03
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-11-28 21:22:30 +0900

    Time: 1 ms (75.47%), Space: 44.4 MB (39.55%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -2,62 +2,30 @@ import java.util.*;
 
 class Solution {
     /*
-    ** 모든 방에 대한 방문 가능 여부를 반환
-    - 0번 방은 잠기지 않음: 무조건 0번에서 시작(루트 노드)
-    - 방 n개에는 다른 방의 열쇠가 있어,방을 방문하고 나면 그 방에 있는 열쇠에 해당하는 방에 방문할 수 있음
-    --> 방(노드)과 열쇠에 해당하는 방(노드)들이 연결
+    Q. 모든 방을 방문할 수 있는지 여부를 리턴 (방문할 수 있으면 true)
+    - 0번 ~ n-1번 방
+    - 0번 방을 제외하고 잠금
+    - 각 방에는 다른 방의 열쇠가 있으므로, 그 방을 방문한 뒤에는 해당 방을 들어갈 수 있음
     
-    모든 방에 대한 순회가 가능한지 여부를 반환해야하므로 BFS, DFS 모두 가능
+    - 방에 있는 열쇠 -> 노드와 연결된 인접노드로 처리
     */
     
-    boolean answer;
-    boolean[] visited;
+    Map<Integer, Boolean> visited = new HashMap<>();
     
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        answer = false; 
-        visited = new boolean[rooms.size()];
-        
-        // bfs(rooms);
         dfs(rooms, 0);
-        
-        int cnt = 0;
-        for(boolean v:visited) {
-            if(v) cnt++;
-        }
-        
-        return cnt == rooms.size();
+        return visited.keySet().size() == rooms.size();
     }
     
-    //레벨을 차례차례 순회
-    void bfs(List<List<Integer>> rooms) {
-        //1.시작점 설정
-        Queue<Integer> q = new ArrayDeque<>();
-        q.offer(0);
-        visited[0] = true;
+    void dfs(List<List<Integer>> rooms, int node) {
+        //현재 노드 방문
+        visited.put(node, true);
         
-        while(!q.isEmpty()) {
-            //2. 대기열에서 노드 방문
-            int cur = q.poll();
-
-            //3. 방문한 노드의 인접 노드를 탐색하여 예약 
-            for(int nextNode : rooms.get(cur)) {
-                if(visited[nextNode]) continue; //이미 방문했던 노드는 무시
-
-                visited[nextNode] = true;
-                q.offer(nextNode);
+        //인접 노드 방문
+        if(!rooms.get(node).isEmpty()) {
+            for(Integer nxt : rooms.get(node)) {
+                if(!visited.containsKey(nxt)) dfs(rooms, nxt);
             }
-        }
-    }
-    
-    //깊이 끝까지 순회
-    void dfs(List<List<Integer>> rooms, int cur) {
-        //1. 방문한 노드 체크
-        visited[cur] = true;
-        
-        //2. 방문한 노드의 인접 노드를 탐색해 방문
-        for(int nextNode : rooms.get(cur)) {
-            if(visited[nextNode]) continue;
-            dfs(rooms, nextNode);
         }
     }
 }
