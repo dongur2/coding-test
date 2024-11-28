9e1f36230a158ae5138110d37c148dc40eff1667
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-11-29 03:32:14 +0900

    [level 3] Title: 가장 먼 노드, Time: 185.92 ms, Memory: 234 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,70 +1,67 @@
 import java.util.*;
 
-/*
-N개 노드(1~N-1번) 중에 1번 노드에서 가장 멀리 떨어진 노드의 개수 리턴
-- edge[0] == [3,6] 3번 노드와 6번 노드가 연결됨
-
-그래프화: 해시테이블 - {노드:연결노드목록(List)}
-DFS끝까지 순회
-최대 간선 업데이트
-최대 간선 가진 노드 개수 카운트
-*/
 class Solution {
+    /*
+    Q. 1번 노드에서 가장 멀리 떨어진 "노드의 개수"를 리턴
+    - 노드 N개(1번~N번)
+    - 1번에서 N번까지 '최단경로'로 이동했을 때 간선의 개수가 기준
+    */
     Map<Integer, List<Integer>> graph;
     boolean[] visited;
+    int dist = 0;
+    Map<Integer, Integer> distMap;
     
     public int solution(int n, int[][] edge) {
         graph = new HashMap<>();
         visited = new boolean[n+1];
+        distMap = new HashMap<>();
         
-        //그래프화
+        //{노드 : [인접노드목록]}
         makeGraph(edge);
         
-        //bfs
-        return bfs();
+        //dfs: 1번 노드에서 시작
+        dfs(1);
+        
+        //최대 간선을 가지는 노드 개수를 리턴
+        return (int)distMap.values().stream().filter(d -> d == dist).count();
     }
     
     void makeGraph(int[][] edge) {
-        for(int[] connection : edge) {
-            addConnection(connection[0], connection[1]);
-            addConnection(connection[1], connection[0]);
+        for(int[] e : edge) {
+            List<Integer> front = graph.getOrDefault(e[0], new ArrayList<>());
+            front.add(e[1]);
+            graph.put(e[0], new ArrayList<>(front));
+            
+            List<Integer> back = graph.getOrDefault(e[1], new ArrayList<>());
+            back.add(e[0]);
+            graph.put(e[1], new ArrayList<>(back));
         }
     }
     
-    void addConnection(int baseNode, int addNode) {
-        List<Integer> connects = graph.getOrDefault(baseNode, new ArrayList<>());
-        connects.add(addNode);
-        graph.put(baseNode, connects);
-    }
-    
-    int bfs() {
-        int maxFar = 0, answer = 0;
+    void dfs(int node) {
+        Queue<int[]> q = new ArrayDeque<>();
         
         //start
-        Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{1, 0});
-        visited[1] = true;
+        q.offer(new int[]{node, 0});
+        visited[node] = true;
+        distMap.put(node, 0);
         
+        //queue
         while(!q.isEmpty()) {
-            //cur
             int[] cur = q.poll();
             int curNode = cur[0];
-            int curEdge = cur[1];
+            int curCnt = cur[1];
             
-            if(maxFar < curEdge) {
-                maxFar = curEdge;
-                answer = 1;
-            }
-            else if (maxFar == curEdge) answer++;
+            dist = Math.max(dist, curCnt);
+            distMap.put(curNode, curCnt);
             
-            //near
-            for(int nxt : graph.get(curNode)) {
-                if(!visited[nxt]) {
-                    q.offer(new int[]{nxt, curEdge+1});
-                    visited[nxt] = true;
+            for(int nxtNode : graph.get(curNode)) {
+                if(!visited[nxtNode]) {
+                    q.offer(new int[]{nxtNode, curCnt+1});
+                    visited[nxtNode] = true;
                 }
             }
         }
-        return answer;
-    }            
+    }
+    
 }
