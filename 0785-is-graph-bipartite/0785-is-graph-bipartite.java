578ec1ec27f0d1d9fe7fb9ea2f07de6362180d5b
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-12-01 00:38:26 +0900

    Time: 1 ms (67.53%), Space: 45.6 MB (8.14%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -28,17 +28,11 @@ class Solution {
         
         if(graph[node].length > 0) {
             map.put(node, set);
-            System.out.println(node+"의 set:"+set);
             
             for(int nxt : graph[node]) {
                 if(visited[nxt]) {
-                    System.out.println("현재:"+node+"의 세트:"+map.get(node)+", 다음:"+nxt+"의 세트:"+map.get(nxt));
-                    if(map.get(nxt) == map.get(node)) {
-                        System.out.println("ERRRR");
-                        return false;}
+                    if(map.get(nxt) == map.get(node)) return false;
                 } else {
-                    // set = 1 - set;
-                    // System.out.printl/n("다음 세트는 "+set);
                     if (!dfs(graph, nxt, 1-set)) return false;
                 }
             }
