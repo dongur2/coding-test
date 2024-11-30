c6c02f09bc0aee3a4073db9fd3c2612bbe724ba7
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-12-01 00:37:44 +0900

    Time: 62 ms (5.12%), Space: 45.7 MB (8.14%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -2,71 +2,48 @@ import java.util.*;
 
 class Solution {
     /*
-    '그래프의 노드'가 독립적인 2개 세트로 나눠지는지 여부를 반환
-    - 서로 연결된(인접한) 노드끼리는 서로 다른 세트에 포함
-    - 연결되지 않은 노드가 존재 -> 그래프에 포함되지 않음 
+    Q. 주어진 그래프가 2그룹으로 나누어지는지 여부 리턴
+    - 무방향 그래프
+    - n개 노드: 0번 ~ n-1번
     */
     
-    boolean answer = true;
-    Map<Integer, Character> map; //방문했던 노드에 부여한 세트 확인 목적
+    Map<Integer, Integer> map;
+    boolean[] visited;
     
-    //int[][] graph: 인덱스에 해당하는 노드와 연결된 노드 목록
+    boolean answer = true;
     public boolean isBipartite(int[][] graph) {
         map = new HashMap<>();
+        visited = new boolean[graph.length];
         
-        // bfs(graph);
         for(int i=0; i<graph.length; i++) {
-            dfs(graph, i, 'A');
+            if(visited[i]) continue;
+            if(!dfs(graph, i, 0)) return false;
         }
         
         return answer;
     }
-    
-    void bfs(int[][] graph) {
-        for(int i=0; i<graph.length; i++) {
-            //시작점
-            if(map.get(i) != null) continue;
-            
-            Queue<Integer> q = new ArrayDeque<>();
-            q.offer(i);
-            map.put(i, 'A');
-                    
-            //대기열 방문
-            while(!q.isEmpty()) {
-                int cur = q.poll();
-                
-                //다음 노드 탐색
-                for(int nextNode : graph[cur]) {
-                    //다음 노드가 방문했던 적이 있고, 그 노드와 현재 노드의 세트가 다르면 통과, 같으면 이분되지 않으므로 즉시 리턴
-                    if(map.get(nextNode) != null) {
-                        if(map.get(cur) == map.get(nextNode)) {
-                            answer = false; return;
-                        } 
-                        else continue; //방문한 적 있으나 서로 세트가 다르면 무시
-                    }
-                    
-                    //다음 노드가 방문한 적 없으면, 세트 부여 & 대기열에 추가
-                    q.offer(nextNode);
-                    if(map.get(cur) == 'A') map.put(nextNode, 'B');
-                    else map.put(nextNode, 'A');
-                }
-            }
-        }
-    }
-    
-    void dfs(int[][] graph, int cur, char set) {
-        if(map.get(cur) == null) map.put(cur, set);
+
+    boolean dfs(int[][] graph, int node, int set) {
+        visited[node] = true;
         
-        for(int nextNode : graph[cur]) {
-            if(map.get(nextNode) != null) {
-                if(map.get(nextNode) == map.get(cur)) {
-                    answer = false; return;
-                }
-                continue;
-            }
+        if(graph[node].length > 0) {
+            map.put(node, set);
+            System.out.println(node+"의 set:"+set);
             
-            if(set == 'A') dfs(graph, nextNode, 'B');
-            else dfs(graph, nextNode, 'A');
+            for(int nxt : graph[node]) {
+                if(visited[nxt]) {
+                    System.out.println("현재:"+node+"의 세트:"+map.get(node)+", 다음:"+nxt+"의 세트:"+map.get(nxt));
+                    if(map.get(nxt) == map.get(node)) {
+                        System.out.println("ERRRR");
+                        return false;}
+                } else {
+                    // set = 1 - set;
+                    // System.out.printl/n("다음 세트는 "+set);
+                    if (!dfs(graph, nxt, 1-set)) return false;
+                }
+            }
         }
+        
+        return true;
     }
 }
