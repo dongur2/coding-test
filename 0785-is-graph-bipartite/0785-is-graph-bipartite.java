38f88016049bad247ea864f590dadb995299f638
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-12-22 19:54:18 +0900

    Time: 1 ms (66.00%), Space: 45.4 MB (12.42%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,36 +1,40 @@
 import java.util.*;
 
+/*
+Q. 주어진 그래프의 노드가 2그룹으로 나눠지는지 여부 리턴
+- 0 ~ n-1번 노드
+- graph[u]는 u번 노드의 인접 노드 목록
+
+- 규칙: 인접한 노드끼리는 다른 그룹에 소속되어야 한다.
+
+- 모든 노드를 순회하며 확인해야하므로 dfs 활용
+*/
 class Solution {
-    /*
-    Q. 주어진 그래프가 2그룹으로 나누어지는지 여부 리턴
-    - 무방향 그래프
-    - n개 노드: 0번 ~ n-1번
-    */
-    
     Map<Integer, Integer> map;
-    boolean answer = true;
     
     public boolean isBipartite(int[][] graph) {
         map = new HashMap<>();
-        
         for(int i=0; i<graph.length; i++) {
-            dfs(graph, i, 0);
+            if(map.containsKey(i)) continue;
+            
+            if(!setGroupAndCheckIsBipartite(graph, i, 0)) return false;
         }
-        
-        return answer;
+        return true;
     }
-
-    void dfs(int[][] graph, int node, int set) {
-        //이미 방문한 적 있으면 무시
-        if(map.get(node) == null) map.put(node, set);
-        
-        for(int nxt : graph[node]) {
-            if(map.get(nxt) != null) { //방문한 적 있는 인접노드
-                if(map.get(nxt) == map.get(node)) {
-                    answer = false; return;
-                }
-            //방문한 적 없는 인접노드
-            } else dfs(graph, nxt, 1-set);
+    
+    boolean setGroupAndCheckIsBipartite(int[][] graph, int node, int group) {
+        //방문한 적 있는 노드라면 그룹 확인
+        if(map.containsKey(node)) {
+            if(group != map.get(node)) return false;
+            return true;
+        } else {
+            //방문한 적 없는 노드라면 그룹 부여
+            map.put(node, group);
+            for(int nxt : graph[node]) {
+                if(!setGroupAndCheckIsBipartite(graph, nxt, 1-group)) return false;
+            }       
         }
+        return true;
     }
+        
 }
