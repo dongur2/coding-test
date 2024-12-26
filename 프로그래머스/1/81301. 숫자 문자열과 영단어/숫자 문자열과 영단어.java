11cf259a8adddd4a1122e048845ba22b7f62b771
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-12-26 17:51:02 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 15.25 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,9 +1,30 @@
 class Solution {
+    String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    
     public int solution(String s) {
-        String[] table = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        for(int i=0; i<table.length; i++) {
-            if(s.contains(table[i])) s = s.replaceAll(table[i], String.valueOf(i));
-        }
-        return Integer.parseInt(s);
+        StringBuilder answer = new StringBuilder();
+        StringBuilder res = new StringBuilder();
+        
+        for(char c:s.toCharArray()) {
+            //숫자일 경우 추가
+            if(c <= 97) answer.append(c+"");
+            //글자일 경우 보관 후 변환해서 추가
+            else {
+                res.append(c+"");
+                
+                for(int i=0; i<arr.length; i++) {
+                    if(arr[i].equals(res.toString())) {
+                        answer.append(i+"");
+                        res = new StringBuilder();
+                    }
+                }
+            }
+        }   
+        
+        return Integer.parseInt(answer.toString());
+    }
+    
+    private void checkAndRevertInt(StringBuilder res) {
+        
     }
 }
