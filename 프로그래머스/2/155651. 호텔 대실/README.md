6e5434e768f7713fac965526228f8082968f5597
dongur2 <134156691+dongur2@users.noreply.github.com>
2024-12-27 18:35:47 +0900

    [level 2] Title: 호텔 대실, Time: 12.20 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/README.md
+++ 프로그래머스/2/155651. 호텔 대실/README.md
@@ -1,10 +1,10 @@
 # [level 2] 호텔 대실 - 155651 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651) 
 
 ### 성능 요약
 
-메모리: 90.5 MB, 시간: 15.56 ms
+메모리: 87 MB, 시간: 12.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 29일 15:07:02
+2024년 12월 27일 18:35:44
 
 ### 문제 설명
 
--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,41 +1,41 @@
 import java.util.*;
-
+/*
+Q. 필요한 최소 객실 수
+- 퇴실 -> 10분 -> 사용
+*/
 class Solution {
-    /*
-    필요한 최소 객실 수를 리턴
-    */
     public int solution(String[][] book_time) {
-        //대실 시작이 빠른 순서대로 정렬
+        int answer = 0;
+        
+        //예약시간 빠른 순서대로 정렬
         Arrays.sort(book_time, (book1, book2) -> book1[0].compareTo(book2[0]));
         
-        //예약 대기열(큐)을 대실 종료가 빠른 순서대로 정렬
-        PriorityQueue<int[]> booking = new PriorityQueue<>((book1, book2) -> book1[1] - book2[1]);
+        //퇴실시간 빠른 순서대로 정렬
+        PriorityQueue<int[]> table = new PriorityQueue<>((book1, book2) -> book1[1] - book2[1]);
         
-        //배열을 앞에서부터(대실 시작이 빠른 순서) 순회하며,
-        int rooms = 0;
-        for(String[] book:book_time) {
-            //문자열로 주어진 시간 -> 정수화
-            String[] splited = book[0].split(":");
-            int start = Integer.parseInt(splited[0]) * 60 + Integer.parseInt(splited[1]);
-            splited = book[1].split(":");
-            int end = Integer.parseInt(splited[0]) * 60 + Integer.parseInt(splited[1]) + 10;
+        //문자열 => 숫자화
+        for(String[] book : book_time) {
+            int start = convertToInt(book[0]);
+            int end = convertToInt(book[1]) + 10;
             
-            //대기열이 비었거나, 대기열의 가장 처음 예약건의 종료 시간보다 현재 대실 시작건의 시간이 빠르면: 처음 예약건과 새로운 예약건을 큐에 넣고 객실 추가 카운트
-            if(booking.isEmpty()) {
-                rooms++;
-                booking.offer(new int[]{start, end});
-            } else {
-                int[] fastest = booking.poll();
-                
-                //현재 대실 시작 시간이 대기열 처음 예약건의 종료보다 뒤라면 추가 카운트 없이 현재 대실 예약건을 대기열에 저장
-                if(start >= fastest[1]) booking.offer(new int[]{start, end});
-                else { 
-                    rooms++;
-                    booking.offer(fastest);
-                    booking.offer(new int[]{start, end});
-                }
+            //배정된 방이 없을 경우
+            if(table.isEmpty()) answer++;
+            
+            //배정된 방의 종료 시간이 현재 새로 등록된 방의 예약 시간보다 뒤라면 확인 필요
+            else {
+                if(table.peek()[1] > start) answer++;
+                else table.poll();
             }
+            
+            //방 배정
+            table.offer(new int[]{start, end});
         }
-        return rooms;
+        
+        return answer;
+    }
+    
+    private int convertToInt(String time) {
+        String[] arr = time.split(":");
+        return Integer.parseInt(arr[0]) * 60 + Integer.parseInt(arr[1]);
     }
 }
