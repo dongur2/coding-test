5b884cfd87cadd3104d24eddd537d07cfcf88823
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-01-29 21:58:01 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 568 ms, Memory: 52752 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,42 +1,39 @@
 import java.io.*;
 import java.util.*;
 
+//구간 합 구하기 1
 public class Main {
+    /*
+     * 수 N개가 주어졌을 때, i번째 수부터 j번째 수까지 합을 구하는 프로그램을 작성하시오.
+     * I> 첫째 줄에 수의 개수 N과 합을 구해야 하는 횟수 M, 둘째 줄에는 N개의 수(0< 숫자 <=1000), 셋째 줄부터 M개의 줄에는 합을 구해야 하는 구간 i와 j가 주어진다.
+     * O> 총 M개의 줄에 입력으로 주어진 i번째 수부터 j번째 수까지 합을 출력한다.
+     * */
     public static void main(String[] args) throws IOException {
-        /*
-         * i~j번째 수의 합 출력
-         */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = br.readLine();
-        StringTokenizer st = new StringTokenizer(input);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken()); //숫자 개수
-        int M = Integer.parseInt(st.nextToken()); //연산 횟수
+        st = new StringTokenizer(br.readLine());
 
-        //N개의 수를 입력받아 배열에 저장
-        input = br.readLine();
-        String[] arr = input.split(" ");
-
-        // 누적 합 배열 **
-        int[] preSum = new int[N + 1];
-        for (int i=1; i<=N; i++) {
-            preSum[i] = preSum[i-1] + Integer.parseInt(arr[i-1]);
+        //구간합 계산
+        int[] table = new int[N + 1];
+        for(int i=1; i<=N; i++) {
+            table[i] = table[i-1] + Integer.parseInt(st.nextToken());
         }
 
-        //연산 & 결과값 저장 **
-        int[] res = new int[M];
+        //인덱스를 받아 해당하는 구간합 구하기
+        StringBuilder sb = new StringBuilder();
         for(int i=0; i<M; i++) {
-            st = new StringTokenizer(br.readLine()); // input
+            st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
 
-            res[i] = preSum[end] - preSum[start-1];
+            int sum = table[end] - table[start-1];
+            sb.append(sum).append("\n");
         }
-        
-        //출력
-        for(int r : res) {
-            System.out.println(r);
-        }
+
+        System.out.println(sb);
     }
 }
