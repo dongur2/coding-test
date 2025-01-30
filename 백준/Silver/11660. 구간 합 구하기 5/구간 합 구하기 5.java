2a1db6896c684f3883aaeb3f84124c847379a8a6
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-01-30 18:35:06 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1056 ms, Memory: 119184 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,53 +1,48 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-
+//구간 합 구하기 2
 public class Main {
     /*
-     * 주어진 배열 시작 위치 ~ 끝 위치의 합 출력 - 사각형 영역 내 합
-     */
+    * N×N개의 수가 N×N 크기의 표에 채워져 있다. (x1, y1)부터 (x2, y2)까지 합을 구하는 프로그램을 작성하시오.
+    * I> 첫째 줄에 표의 크기 N과 합을 구해야 하는 횟수 M, 둘째 줄부터 N개의 줄에는 표에 채워져 있는 수가 1행부터 차례대로 주어진다
+    * 다음 M개의 줄에는 네 개의 정수 x1, y1, x2, y2 가 주어지며, (x1, y1)부터 (x2, y2)의 합을 구해 출력
+    * O> 총 M줄에 걸쳐 (x1, y1)부터 (x2, y2)까지 합을 구해 출력한다.
+    * */
     public static void main(String[] args) throws IOException {
-
-        //1. 배열 크기 N, 합 연산 횟수 M 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        //2. N크기의 배열 입력
-        int[][] arr = new int[N+1][N+1];
-        for(int i=1; i<=N; i++) {
+        //구간합 배열 저장
+        long[][] table = new long[N+1][N+1];
+        for(int r=1; r<=N; r++) {
             st = new StringTokenizer(br.readLine());
 
-            for(int j=1; j<=N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            for(int c=1; c<=N; c++) {
+                table[r][c] = table[r][c - 1] + Long.parseLong(st.nextToken());
             }
         }
 
-        //3. 누적합 구하기
-        int[][] sum = new int[N+1][N+1];
-        for(int i=1; i<=N; i++) {
-            for(int j=1; j<=N; j++) {
-                sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + arr[i][j];
-            }
-        }
-
-        //4. 연산
+        //해당하는 구간합 구하기
+        StringBuilder sb = new StringBuilder();
         for(int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
+            int x1 = Integer.parseInt(st.nextToken()), y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken()), y2 = Integer.parseInt(st.nextToken());
 
-            int res = sum[x2][y2] - sum[x1-1][y2] - sum[x2][y1-1] + sum[x1-1][y1-1];
-            System.out.println(res);
+            long sum = 0;
+            for(int row = x1; row<=x2; row++) {
+                sum += table[row][y2] - table[row][y1 - 1];
+            }
+            sb.append(sum).append("\n");
         }
 
-        /*
-        * 사각형에서 맞닿은 모서리는 빼고, 마주보는 모서리는 더함
-        * */
+        System.out.print(sb);
     }
 }
