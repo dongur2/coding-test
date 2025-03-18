f1ea9af57c36026b0bb7cabe65d2c41168bc4a2a
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-03-18 20:58:57 +0900

    [level 3] Title: 단어 변환, Time: 1.30 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
 
 ### 성능 요약
 
-메모리: 88.4 MB, 시간: 1.07 ms
+메모리: 76.7 MB, 시간: 1.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 20일 20:15:01
+2025년 03월 18일 20:58:53
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,87 +1,66 @@
 import java.util.*;
 
 /*
-Q. begin -> target으로 변환할 수 있는 "최소 단계 수"
-- 변환할 수 없으면 0 리턴
-- bfs로 탐색하면서 target으로 변환 완료되면 즉시 그 시점의 단계 수를 리턴
-
-- 다음 단어로 변환하기 전 확인
-(1) 이미 변환한 적 있는 단어는 무시
-(2) 다른 글자 개수가 1글자 초과할 경우 무시
+- 최소 변환 횟수 [bfs]
+- 한번에 글자 하나 
 */
 class Solution {
-    public int solution(String begin, String target, String[] words) {        
-        return getMinimumForTarget(begin, target, words);
+    String[] wordsTable;
+    String targetWord;
+    boolean[] visited;
+    
+    public int solution(String begin, String target, String[] words) {
+        wordsTable = words; targetWord = target;
+        visited = new boolean[words.length];
+        
+        return bfs(begin);
     }
     
-    //최소 단계 구하기
-    private int getMinimumForTarget(String word, String target, String[] words) {
-        //주어진 단어 배열에 타겟 단어가 없으면 이미 불가능하므로 즉시 리턴
-        if(!isTargetExist(target, words)) return 0; 
+    int bfs(String start) {
+        Queue<Node> q = new ArrayDeque<>();
+        q.offer(new Node(start, 0)); //현재 단어, 변환 횟수
         
-        //bfs
-        return bfs(word, target, words);
-    }
-    
-    //주어진 배열의 타겟 단어 포함 여부
-    private boolean isTargetExist(String target, String[] words) {
-        for(String word:words) {
-            if(word.equals(target)) return true;
-        }
-        return false;
-    }
-    
-    private int bfs(String word, String target, String[] words) {
-        //세팅
-        Queue<Word> q = new ArrayDeque<>();
-        q.offer(new Word(0, word));
-        
-        List<String> log = new ArrayList<>();
-        
-        //큐 기반 탐색
         while(!q.isEmpty()) {
-            //now
-            Word cur = q.poll();
-            String curWord = cur.word;
-            int curPhase = cur.phase;
+            Node cur = q.poll();
+            String nowWord = cur.word;
+            int nowCnt = cur.cnt;
             
-            //현재 단어가 target단어로 변환 완료된 상태라면 리턴
-            if(curWord.equals(target)) return curPhase;
+            //단어 변환 완료했을 경우 리턴
+            if(nowWord.equals(targetWord)) return nowCnt;
             
-            //next: 변환한 적 없고 & 다른 글자가 1글자인 단어로 변환 가능
-            for(int i=0; i<words.length; i++) {
-                if(!isValid(log, curWord, words[i], i)) continue;
+            //새로운 단어 중에 글자 1개만 다른 단어로 변환
+            for(int i=0; i<wordsTable.length; i++) {
+                if(visited[i]) continue;
                 
-                q.offer(new Word(curPhase+1, words[i]));
-                log.add(words[i]); //같은 단어로 또 변환하는 것을 방지하기 위한 로그
+                //1개만 다른 글자 찾기
+                int diffCnt = 0, diffIdx = -1;
+                for(int idx=0; idx<start.length(); idx++) {
+                    if(nowWord.charAt(idx) != wordsTable[i].charAt(idx)) {
+                        diffCnt++; 
+                        diffIdx = idx;
+                    }
+                }
+                
+                //변환
+                if(diffCnt == 1) {
+                    StringBuilder tempWord = new StringBuilder(nowWord);
+                    tempWord.setCharAt(diffIdx, wordsTable[i].charAt(diffIdx));
+                    q.offer(new Node(tempWord.toString(), cur.cnt+1));
+                    visited[i] = true;                    
+                }
             }
         }
+        
         return 0;
     }
     
-    //단어 변환 전 유효 조건인지 확인
-    private boolean isValid(List<String> log, String now, String nxt, int idx) {
-        if(log.contains(nxt)) return false; //변환한 적 있는지 확인
-        return isValidSameCnt(now.toCharArray(), nxt.toCharArray());
-    }
-    
-    //다른 단어 1개 초과 확인
-    private boolean isValidSameCnt(char[] now, char[] nxt) {
-        int cnt = 0;
-        for(int i=0; i<now.length; i++) {
-            if(now[i] != nxt[i]) cnt++;
-            if(cnt > 1) return false;
-        }
-        return cnt == 1 ? true : false;
-    }
-    
-    class Word {
-        int phase;
+    class Node {
         String word;
+        int cnt;
         
-        public Word(int phase, String word) {
-            this.phase = phase;
+        public Node(String word, int cnt) {
             this.word = word;
+            this.cnt = cnt;
         }
     }
 }
