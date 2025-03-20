764ee14bac9367a8c63d16c55c08b31619fb53e0
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-03-20 18:35:11 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 46.24 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,29 +1,25 @@
 import java.util.*;
 
 /*
-모든 퍼즐을 해결하기 위한 최소 숙련도
-
-- 제한 시간 내 n개 퍼즐을 순서대로 해결
-- 퍼즐[난이도, 소요시간]
-- 난이도 <= 숙련도면 소요시간만큼 사용
-- 난이도 > 숙련도면 (난이도 - 숙련도)번 틀리고, 틀릴 때마다 소요시간, 그리고 이전 퍼즐 다시 풀고와야함 + 그 후에 소요시간
+제한 시간 내 모든 퍼즐을 해결할 수 있는 '최소 숙련도'
+- 퍼즐 순서 고정
+- 숙련도 >= 난이도: 현재 퍼즐 소요 시간
+- 숙련도 < 난이도: (난이도 - 숙련도)번 틀리고, 틀릴 때마다 +현재 퍼즐 소요 시간 +이전 퍼즐 소요 시간
 */
 class Solution {
-    
     public int solution(int[] diffs, int[] times, long limit) {
-        int[] temp = new int[diffs.length];
-        for(int t=0; t<temp.length; t++) {
-            temp[t] = diffs[t];
-        }
-        Arrays.sort(temp);
+        //최고 숙련도 = 퍼즐 중 최고 난이도
+        int answer = Arrays.stream(diffs).max().getAsInt();
         
+        //최소 숙련도 찾기: 중간값으로 좁혀가기
         int left = 1;
-        int answer = temp[diffs.length-1]; //최대 난이도
         int right = answer;
-        
+      
+        //숙련도를 낮춰가면서, 제한 시간 내 모든 퍼즐 해결이 가능한지 확인
         while(left <= right) {
             int mid = (left + right) / 2;
-            if(canSolve(diffs, times, mid, limit)) {
+            
+            if(canSolve(diffs, times, limit, mid)) {
                 answer = mid;
                 right = mid - 1;
             } else left = mid + 1;
@@ -32,26 +28,16 @@ class Solution {
         return answer;
     }
     
-    public boolean canSolve(int[] diffs, int[] times, int level, long limit) {
-        long time = 0L;
-        
-        //각 숙련도마다 퍼즐 풀어보기
-        for(int idx=0; idx<diffs.length; idx++) {
-            //숙련도 >= 난이도면 소요시간 사용
-            if(level >= diffs[idx]) {
-                time += times[idx];
-
-            } else { //숙련도 < 난이도면
-                //(난이도 - 숙련도)번 틀리고, 이전 퍼즐 풀이
-                int missed = diffs[idx] - level;
-                if(idx > 0) time += (times[idx] + times[idx-1]) * missed;
-                else time += times[idx] * missed;
-
-                //이후 소요시간으로 풀이 가능    
-                time += times[idx];
+    boolean canSolve(int[] diffs, int[] times, long limit, int level) {
+        long time = 0;
+        for(int i=0; i<diffs.length; i++) {
+            if(level >= diffs[i]) time += times[i];
+            else {
+                int miss = diffs[i] - level;
+                time += (times[i-1] + times[i]) * miss + times[i];
             }
             if(time > limit) return false;
-        } 
+        }
         return time <= limit;
     }
 }
