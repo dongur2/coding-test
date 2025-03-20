112391f2302918cca8c5c7b34eb897ce58b7189a
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-03-20 21:22:13 +0900

    [level 2] Title: 과제 진행하기, Time: 16.97 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
@@ -1,15 +1,17 @@
 import java.util.*;
 
 /*
-- 과제 끝낸 순서대로 배열에 담아 리턴!
-- 정해둔 시간대로 과제 시작
-- 하다 말았으면 새로운거 시작 (최우선)- 다 끝내고나서 멈춰둔 과제 시작 (멈춰둔 것중엔 최근 것부터)
+과제 이름을 종료 순서대로 리턴
+- 시작 시간에 시작
+- 다음 과제 시작 시간에도 마치지 못했을 경우 다음 과제 시작
+- 다음 과제 시작 시간 전에 마쳤을 경우, 이전에 멈춰둔 과제 시작
+- 멈춘 과제가 여러 개일 경우 최근 것부터 시작
 */
 class Solution {
+    List<String> answer = new ArrayList<>();
+    
     public String[] solution(String[][] plans) {
-        List<String> list = new ArrayList<>();
-        
-        //과제 수행 큐 - 빠른 시작 시간 순으로 정렬
+        //기존 과제 시작 시간에 따른 수행 큐: 시작 시간이 빠를 수록 우선
         PriorityQueue<String[]> q = new PriorityQueue<>(new Comparator<String[]>(){
             @Override
             public int compare(String[] p1, String[] p2) {
@@ -17,60 +19,66 @@ class Solution {
             }
         });
         
-        //남은 과제 수행 스택
-        Deque<String[]> stack = new ArrayDeque<>();
-        
-        //큐에 과제 저장
-        for(String[] plan : plans) {
-            //시작 시간을 분으로 변환하여 저장
-            String[] times = plan[1].split(":");
-            int hour = Integer.parseInt(times[0]) * 60;
-            int min = Integer.parseInt(times[1]);
-            plan[1] = String.valueOf(hour + min);
-            
+        //시작시간을 분으로 전환한 뒤에 큐에 추가
+        for(String[] plan:plans) {
+            plan[1] = convertMinutes(plan);
             q.offer(plan);
         }
         
+        //남은 과제 스택: 최근 것부터 수행
+        Deque<String[]> st = new ArrayDeque<>();
+        
         while(!q.isEmpty()) {
-           String[] cur = q.poll();
-           String subject = cur[0];
-           int nowTime = Integer.parseInt(cur[1]);
-           int remains = Integer.parseInt(cur[2]);
-
-           //현재 작업 완료시 시각
-           int finishTime = nowTime + remains;
-
-           //다음 작업이 있을 경우
-           if(!q.isEmpty()) {
-               int nxtStart = Integer.parseInt(q.peek()[1]);
-
-               //다음 작업 시각 시간이 현재 작업 완료 시각보다 빠를 경우 
-               if(finishTime > nxtStart) {
-                   //남은 과제를 스택에 추가
-                   stack.push(new String[] {subject, finishTime - nxtStart + ""});
-
-               //다음 작업보다 빠르게/동시에 작업 완료 가능할 경우
-               } else {
-                   list.add(subject); //일단 완료 리스트에 추가하고
-                   
-                   //다음 과제 시작 시각보다 빨리 끝냈는지 확인 후 빠르다면, 남았던 과제를 최우선으로 큐에 추가
-                   if(finishTime < nxtStart && !stack.isEmpty()) {
-                       String[] r = stack.pop();
-                       q.offer(new String[] {r[0], finishTime+"", r[1]});
-                   }
-               }
-           } else list.add(subject);
+            String[] cur = q.poll();
+            String subject = cur[0];
+            int time = Integer.parseInt(cur[1]);
+            int required = Integer.parseInt(cur[2]);
+            
+            int finishTime = time + required; //과제를 모두 완료했을 때 시각
+            
+            //다음 과제가 있을 경우
+            if(!q.isEmpty()) {
+                String[] nxt = q.peek();
+                int nxtTime = Integer.parseInt(nxt[1]);
+                
+                //다음 과제 시작 전에 완료
+                if(finishTime <= nxtTime) {
+                    answer.add(subject);
+                    
+                    //미종료 과제가 있을 경우 그것부터
+                    if(finishTime < nxtTime && !st.isEmpty()) {
+                        String[] remainTask = st.pop();
+                        q.offer(new String[] {remainTask[0], time + required + "", remainTask[1]});
+                    }
+                }
+                
+                //다음 과제 시작 전 미완료
+                else {
+                    int remains = finishTime - nxtTime; //미완료분
+                    st.push(new String[] {subject, remains+""}); //스택에 추가
+                }
+            
+            //다음 과제 없을 경우 완료
+            } else answer.add(subject);
         }
         
-        //남은 과제 순서대로 완료
-        while(!stack.isEmpty()) {
-            list.add(stack.pop()[0]);
+        //남은 과제 완료
+        while(!st.isEmpty()) {
+            answer.add(st.pop()[0]);
         }
         
-        String[] answer = new String[plans.length];
-        for(int i=0; i<plans.length; i++) {
-            answer[i] = list.get(i);
+        String[] arr = new String[answer.size()];
+        for(int i=0; i<answer.size(); i++) {
+            arr[i] = answer.get(i);
         }
-        return answer;
+        return arr;
+    }
+    
+    private String convertMinutes(String[] plan) {
+        String[] time = plan[1].split(":");
+        int hour = Integer.parseInt(time[0]) * 60;
+        int min = Integer.parseInt(time[1]);
+        
+        return String.valueOf(hour+min);
     }
 }
