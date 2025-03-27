b16e6d65a1bdda13bcf07f1b328ec701f39b8487
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-03-27 21:22:29 +0900

    [level 2] Title: 광물 캐기, Time: 11.15 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,13 +1,10 @@
 import java.util.*;
 
-/*
-- 최소 피로도: 최소 거리가 아니라 최소값이니까 dfs
-- 곡괭이 1개로 광물 5개
-- 모든 광물 채광 완료 또는 모든 곡괭이 사용
-*/
+//곡괭이를 다 쓰거나 모든 광물을 캘 때까지 필요한 최소 피로도
+//빨리 캐는게 아니고 최소 피로도니까 dfs
 class Solution {
-    //곡괭이별 피로도
-    int[][] fatigueTable = {
+    //각 곡괭이별 피로도 테이블
+    int[][] fatigues = {
         {1, 1, 1},
         {5, 1, 1},
         {25, 5, 1}
@@ -15,44 +12,48 @@ class Solution {
     
     //최소 피로도
     int minFatigue = Integer.MAX_VALUE;
+    int mineralCnt;
     
     public int solution(int[] picks, String[] minerals) {
-        dfs(picks, minerals, 0, 0); //현재 광물 순서, 누적 피로도
+        mineralCnt = minerals.length;
+        
+        dfs(picks, minerals, 0, 0);
         return minFatigue;
     }
     
-    void dfs(int[] picks, String[] minerals, int m, int fatigue) {
-        //stop: 남은 광물이 없거나 남은 곡괭이가 없을 경우
-        if(minerals.length <= m || (picks[0] == 0 && picks[1] == 0 && picks[2] == 0)) {
+    void dfs(int[] picks, String[] minerals, int idx, int fatigue) {
+        //stop: 남은 광물/곡괭이가 모두 없을 경우 중지
+        if(idx >= mineralCnt || (picks[0] == 0 && picks[1] == 0 && picks[2] == 0)) {
             minFatigue = Math.min(minFatigue, fatigue);
             return;
         }
         
-        //recursive call: 남은 곡괭이 중 선택해 5개 채광
+        //recursive call: 곡괭이 차례대로 선택해서 채광
         for(int i=0; i<3; i++) {
-            //곡괭이 선택
+            //남은 곡괭이 있으면 선택
             if(picks[i] > 0) {
                 picks[i]--;
                 
-                //채광
-                int newFatigue = fatigue;
+                //채광시작 - 연속 5회 
+                int newFatigue = 0, newIdx = idx;
                 for(int cnt=0; cnt<5; cnt++) {
-                    //남은 광물이 없으면 중지
-                    if(m + cnt >= minerals.length) break;
-                    
-                    int mineralType = convertType(minerals[m+cnt]);
-                    newFatigue += fatigueTable[i][mineralType];
+                    //남은 광물이 있을 경우 채광 진행
+                    if(newIdx < mineralCnt) {
+                        int mineral = convertToIdx(minerals[newIdx]);
+                        newFatigue += fatigues[i][mineral];
+                        newIdx++;
+                    }
                 }
                 
-                dfs(picks, minerals, m + 5, newFatigue);
-                picks[i]++; //사용했던 곡괭이 복구 (backtrack)
+                dfs(picks, minerals, idx + 5, fatigue + newFatigue);
+                picks[i]++;
             }
         }
     }
     
-    int convertType(String type) {
-        if(type.equals("diamond")) return 0;
-        else if(type.equals("iron")) return 1;
+    int convertToIdx(String name) {
+        if(name.equals("diamond")) return 0;
+        else if(name.equals("iron")) return 1;
         else return 2;
     }
 }
