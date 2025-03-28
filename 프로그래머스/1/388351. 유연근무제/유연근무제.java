80ea999f56222e24ba3750b40ac353f077036cd3
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-03-29 01:12:27 +0900

    [level 1] Title: 유연근무제, Time: 0.78 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.java
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -1,46 +1,30 @@
 import java.util.*;
 
-/*
-상품을 받을 직원 수
-- 일주일 동안 각자 설정한 출근 시각에 늦지 않고 출근
-- 설정 출근 시각 + 10분까지 출근
-- 토요일, 일요일 상관없음
-- 시각: 10시13분 => 1013 (10 * 100 + 13)
-*/
+//일주일동안 지각안한 직원 수
 class Solution {
-    
     public int solution(int[] schedules, int[][] timelogs, int startday) {
-        int people = schedules.length; //직원 수
-        int answer = people; //최대 수
+        int answer = schedules.length;
         
-        int startIdx = startday - 1; //배열 인덱스와 동기화
-        
-        //각 직원이 요일마다 지각했는지 확인 - 1번이라도 지각하면 무시
-        for(int p=0; p<people; p++) {
-            //일주일 타임로그 확인
-            int today = startday - 1;
+        //직원별로 지각 확인
+        for(int i=0; i<schedules.length; i++) {
+            int deadline = convertToMinute(schedules[i]) + 10;
             
-            int endTime = convertToMinute(schedules[p]) + 10;
-            int[] times = timelogs[p];
-            
-            for(int time : times) {
-                //토일 무시
-                int minuteTime = convertToMinute(time);
-                if(today < 5 && endTime < minuteTime) {
-                    answer--;
-                    break;
+            for(int idx=0; idx<7; idx++) {
+                int timelog = convertToMinute(timelogs[i][idx]);
+                
+                int day = (startday + idx) % 7;
+                if(day != 6 && day != 0 && deadline < timelog) { //토일 제외
+                    answer--; break;
                 }
-                today = (today + 1) % 7;
-            } 
+            }
         }
         
         return answer;
     }
     
-    private int convertToMinute(int time) {
-        int hour = time / 100;
+    int convertToMinute(int time) {
+        int hour = (time / 100) * 60;
         int min = time % 100;
-        
-        return hour * 60 + min;
+        return hour + min;
     }
 }
