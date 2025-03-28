72fedbdf8f97e52a671b255ebae2037fa6abf0e2
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-03-28 15:49:58 +0900

    [level 2] Title: 귤 고르기, Time: 154.94 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,37 +1,39 @@
 import java.util.*;
 
-/*
-- k개를 골랐을 때 최소 종류 개수
-- 개수 많은 종류 선택 최우선
-*/
+//귤 k개를 최대한 다양하게 골랐을 때 종류 개수
 class Solution {
     public int solution(int k, int[] tangerine) {
-        Map<Integer, Integer> map = new HashMap<>(); //크기:개수
+        //종류별 개수 카운트
+        Map<Integer, Integer> map = new HashMap<>();
+        countType(map, tangerine);
         
-        //크기별 개수 카운트
-        for(int t:tangerine) {
-            map.put(t, map.getOrDefault(t, 0) + 1);
-        }
-        
-        //개수 적은 순서대로 정렬
-        int[] table = new int[map.size()];
-        int idx = 0;
-        for(int value : map.values()) {
-            table[idx++] = value;
-        }
-        Arrays.sort(table);
-        
-        //카운트
-        int cnt = 0, type = 0;
-        for(int i=table.length-1; i>=0; i--) {
-            if(cnt >= k) break;
-            else {
-                cnt += table[i];
-                table[i] = 0;
-                type++;
+        //개수 많은 것부터 정렬
+        Queue<int[]> q = new PriorityQueue<>(new Comparator<int[]>(){
+            @Override
+            public int compare(int[] t1, int[] t2) {
+                return t2[1] - t1[1];
             }
+        });
+        
+        for(int type : map.keySet()) {
+            int cnt = map.get(type);
+            q.offer(new int[]{type, cnt});
         }
         
-        return type;
+        //가장 많은 개수가 있는 종류부터 선택
+        int box = 0, answer= 0;
+        while(box < k) {
+            int[] cur = q.poll();
+            answer++; //새로운 종류 카운트
+            box += cur[1];
+        }
+        
+        return answer;
+    }
+    
+    void countType(Map<Integer, Integer> map, int[] tangerine) {
+        for(int t : tangerine) {
+            map.put(t, map.getOrDefault(t, 0)+1);
+        }
     }
 }
