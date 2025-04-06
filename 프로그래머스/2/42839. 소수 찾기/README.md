8a52b551274353a2b5d8a5ad6497285a20d7d54e
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-04-06 23:27:09 +0900

    [level 2] Title: 소수 찾기, Time: 128.18 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
 
 ### 성능 요약
 
-메모리: 86.3 MB, 시간: 99.12 ms
+메모리: 78.9 MB, 시간: 128.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 30일 23:40:49
+2025년 04월 06일 23:27:06
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,55 +1,42 @@
 import java.util.*;
 
+//만들 수 있는 소수 개수
 class Solution {
-    /*
-    Q. 주어진 숫자로 만들 수 있는 '소수의 개수'를 반환
-    - 11 == 011
-    - 중복 사용 불가능
-    - 중복 X + 순서 의미 있음 => 순열 (123, 132)
-    */
-    
-    int cnt = 0;
-    boolean[] visited;
-    Set<Integer> set;
+    static Set<Integer> set = new HashSet<>(); //중복불허
+    static boolean[] visited;
     
     public int solution(String numbers) {
         visited = new boolean[numbers.length()];
-        set = new HashSet<>();
         
-        char[] nums = numbers.toCharArray();
-        
-        for(int i=1; i<=nums.length; i++) {
-            makeNumAndCheckPrime(nums, i, new StringBuilder());
+        for(int len=1; len<=numbers.length(); len++) {
+            make(numbers.toCharArray(), len, new StringBuilder());
         }
         
         return set.size();
     }
     
-    void makeNumAndCheckPrime(char[] nums, int len, StringBuilder num) {
-        if(num.length() == len) {
-            int number = Integer.valueOf(num.toString());
-            if(isPrime(number)) set.add(number);
+    void make(char[] nums, int len, StringBuilder number) {
+        //길이충족시 소수 확인
+        if(number.length() == len) {
+            int num = Integer.parseInt(number.toString());
+            if(isPrime(num)) set.add(num);
+            return;
         }
         
         for(int i=0; i<nums.length; i++) {
             if(visited[i]) continue;
             
-            visited[i] = true;
-            num.append(nums[i]);
-            
-            makeNumAndCheckPrime(nums, len, num);
-            
-            visited[i] = false;
-            num.deleteCharAt(num.length()-1);
+            visited[i] = true; number.append(nums[i]);
+            make(nums, len, number);
+            visited[i] = false; number.deleteCharAt(number.length()-1);
         }
     }
     
-    boolean isPrime(int num) {
-        if(num == 0 || num == 1) return false;
-        else if(num == 2) return true;
+    boolean isPrime(int n) {
+        if(n == 0 || n == 1) return false;
         
-        for(int i=2; i<Math.abs(num); i++) {
-            if(num % i == 0) return false;
+        for(int i=2; i<n; i++) {
+            if(n % i == 0) return false;
         }
         return true;
     }
