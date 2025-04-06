302f857db678fae48bab7c7c7aec458712500e2e
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-04-06 23:08:36 +0900

    [level 1] Title: 소수 찾기, Time: 28.57 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,21 +1,21 @@
-import java.util.stream.*;
+import java.util.*;
 
 class Solution {
+    //10^6
     public int solution(int n) {
-        int[] cnt = new int[n+1]; // 인덱스의 약수 개수
+        int[] arr = new int[n+1];
+        Arrays.fill(arr, 1);
         
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=n/i; j++) {
-                cnt[i*j]++;
+        for(int i=2; i<=(int)Math.sqrt(n); i++) {
+            if(arr[i] == 1) {
+                int j = 2;
+                while(i*j <= n) {
+                    arr[i*j] = 0;
+                    j++;
+                }
             }
         }
         
-        // int answer = 0;
-        // for(int c:cnt) {
-        //     if(c==2) answer++;
-        // }
-        // return answer;
-        return (int)IntStream.of(cnt).filter(c -> c==2).count();
-        
+        return (int)Arrays.stream(arr).filter(a->a==1).count()-2; //0,1 제외
     }
 }
