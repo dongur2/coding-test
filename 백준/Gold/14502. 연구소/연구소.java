2ac629d444d27962e2a2bd9625afa27d0e9eb030
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-04-08 12:43:45 +0900

    [Gold IV] Title: 연구소, Time: 848 ms, Memory: 246340 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,134 +1,119 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int[][] room;
-    static int N, M;
-
-    static int[] rowArr = new int[] {0, 1, 0, -1};
-    static int[] colArr = new int[] {1, 0, -1, 0};
-
-    static int answer = 0;
-
-    public static void main(String[] args) throws IOException {
-        makeRoom();
-        buildWallsAndCountSafeArea(0, 0, 0, new boolean[N][M]);
-        System.out.println(answer);
-    }
-
-    //연구소 생성
-    private static void makeRoom() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        room = new int[N][M];
-        for(int i=0; i<N; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            for(int j=0; j<M; j++) {
-                room[i][j] = Integer.parseInt(st.nextToken());
-            }
+    //바이러는 상하좌우로 인접한 빈 칸으로 전파
+    static int[] dRow = {0, 1, 0, -1};
+    static int[] dCol = {1, 0, -1, 0};
+    
+    static int[][] map;
+    static boolean[][] visited;
+    
+    static int[][] newMap;
+    
+    static int answer = Integer.MIN_VALUE;
+    
+    //안전 영역 크기의 최대값 - dfs로 끝까지 감염
+    public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                  
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int n = Integer.parseInt(st.nextToken());
+      int m = Integer.parseInt(st.nextToken());
+      
+      map = new int[n][m];
+      visited = new boolean[n][m];
+      
+      //지도 생성 0:빈칸, 1:벽, 2:바이러스
+      for(int r=0; r<n; r++) {
+        String line = br.readLine();
+        String[] arr = line.split(" ");
+        
+        for(int c=0; c<m; c++) {
+          map[r][c] = Integer.parseInt(arr[c]);
         }
+      }
+      
+      //벽 3개 세우기 (중복 불허용, 순서 상관없음 -> 조합)
+      //벽을 세울 수 있는 모든 경우의 수에서 감염 후 안전구역 카운트 
+      getMaxSafety(n, m, 0);
+      
+      System.out.println(answer);
     }
-
-    //벽 세우고 안전 지대 개수 카운트
-    private static void buildWallsAndCountSafeArea(int row, int col, int wallCnt, boolean[][] visited) {
-        if(wallCnt == 3) {
-            int[][] copied = copyRoom();
-            spreadVirus(copied, new boolean[N][M]);
-
-            int safety = countSafeArea(copied);
-
-            answer = Math.max(answer, safety);
-
-            return;
+    
+    public static void getMaxSafety(int n, int m, int walls) {
+      //벽 3개 다 세웠으면 
+      if(walls == 3) {
+        //맵 복사 
+        newMap = new int[n][m];
+        for(int i=0; i<n; i++) {
+          // newMap[i] = Arrays.copyOfRange(map[i], 0, m);
+          newMap[i] = Arrays.copyOf(map[i], m);
         }
-
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<M; j++) {
-                if(room[i][j] == 0 && !visited[i][j]) {
-                    room[i][j] = 1; visited[i][j] = true;
-                    buildWallsAndCountSafeArea(row+1, col+1, wallCnt+1, visited);
-                    room[i][j] = 0; visited[i][j] = false;
-                }
-            }
+        
+        //감염
+        visited = new boolean[n][m]; //방문 초기화 
+        infect(n, m);
+        
+        //안전구역 카운트
+        int safety = countSafety(n, m);
+        answer = Math.max(answer, safety);
+        
+        return;
+      }
+      
+      
+      for(int r=0; r<n; r++) {
+        for(int c=0; c<m; c++) {
+          //빈 공간이면 벽 생성
+          if(map[r][c] == 0) {
+            map[r][c] = 1;
+            getMaxSafety(n, m, walls+1);
+            map[r][c] = 0; //복구
+          } 
         }
+      }
     }
-
-    //바이러스 전파
-    static void spreadVirus(int[][] copied, boolean[][] visited) {
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<M; j++) {
-
-                if(copied[i][j] == 2 && !visited[i][j]) {
-                    Queue<int[]> q = new ArrayDeque<>();
-                    q.offer(new int[] {i, j});
-
-                    while(!q.isEmpty()) {
-                        int[] cur = q.poll();
-                        int curR = cur[0];
-                        int curC = cur[1];
-
-                        for(int l=0; l<4; l++) {
-                            int nxtR = curR + rowArr[l];
-                            int nxtC = curC + colArr[l];
-
-                            if(isValid(copied, nxtR, nxtC, visited)) {
-                                copied[nxtR][nxtC] = 2;
-                                q.offer(new int[]{nxtR, nxtC});
-                                visited[nxtR][nxtC] = true;
-                            }
-                        }
-                    }
-                }
-            }
+    
+    public static int countSafety(int n, int m) {
+      //안전 구역 카운트
+      int cnt = 0;
+      for(int r=0; r<n; r++) {
+        for(int c=0; c<m; c++) {
+          if(newMap[r][c] == 0) cnt++;
         }
+      }     
+      return cnt;
     }
-
-    //바이러스 전파 시 상하좌우 유효값 확인
-    static boolean isValid(int[][] copied, int r, int c, boolean[][] visited) {
-        return r >= 0 && r < N && c >= 0 && c < M && !visited[r][c] && copied[r][c] == 0;
-    }
-
-    //연구소 깊은 복사
-    static int[][] copyRoom() {
-        int[][] copied = new int[N][M];
-
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                copied[i][j] = room[i][j];
-            }
+    
+    public static void infect(int n, int m) {
+      for(int r=0; r<n; r++) {
+        for(int c=0; c<m; c++) {
+          //바이러스를 발견하면 감염 시작
+          if(newMap[r][c] == 2 && !visited[r][c]) {
+            visited[r][c] = true;
+            dfs(n, m, r, c);
+          }
         }
-
-        return copied;
+      }
     }
-
-    //안전지대 개수 카운트
-    static int countSafeArea(int[][] copied) {
-        int cnt = 0;
-
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                if(copied[i][j] == 0) cnt++;
-            }
+    
+    public static void dfs(int n, int m, int r, int c) {
+      //인접 지역에 바이러스나 빈 공간이 있을 경우 이동
+      for(int i=0; i<4; i++) {
+        int nxtR = r + dRow[i];
+        int nxtC = c + dCol[i];
+        
+        if(isValid(n, m, nxtR, nxtC)) {
+          visited[nxtR][nxtC] = true;
+          //빈 공간이면 감염 
+          if(newMap[nxtR][nxtC] == 0) newMap[nxtR][nxtC] = 2; 
+          dfs(n, m, nxtR, nxtC); //이동
         }
-
-        return cnt;
+      }
     }
-
-    static void printRoom() {
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                System.out.print(room[i][j] + " ");
-            }
-            System.out.println();
-        }
+    
+    public static boolean isValid(int n, int m, int r, int c) {
+      return r >= 0 && r < n && c >= 0 && c < m && !visited[r][c] && newMap[r][c] != 1;
     }
 }
