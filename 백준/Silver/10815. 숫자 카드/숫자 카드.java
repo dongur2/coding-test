62def25077a222cd986c1d786d4d7258c8be5148
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-04-14 16:19:50 +0900

    [Silver V] Title: 숫자 카드, Time: 856 ms, Memory: 135496 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,38 +1,31 @@
 import java.util.*;
 import java.io.*;
 
-//주어지는 숫자 중 가지고 있는 카드에 있는 숫자 체크
-//인풋 개수 10^5
-class Main {
+//주어진 숫자 M개 중 숫자 카드 N개에서 보유 여부 
+public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        //가진 카드 개수 입력
-        int n = Integer.parseInt(br.readLine());
-        
-        //카드의 숫자 입력
-        Set<Integer> cards = new HashSet<>();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        while(st.hasMoreTokens()) {
-            int num = Integer.parseInt(st.nextToken());
-            cards.add(num);
-        }
-        
-        //주어지는 숫자 개수 입력
-        int m = Integer.parseInt(br.readLine());
-        
-        //주어지는 숫자 입력
-        st = new StringTokenizer(br.readLine());
-        
-        int[] answer = new int[m];
-        for(int i=0; i<m; i++) {
-            int num = Integer.parseInt(st.nextToken());
-
-            //카드 중에 숫자가 존재하는지 확인
-            if(cards.contains(num)) answer[i] = 1;
-        }
-        
-        //결과 리턴
-        Arrays.stream(answer).forEach(a->System.out.print(a + " "));
-    }
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      Set<Integer> set = new HashSet<>();
+      
+      //가지고있는 카드 
+      int n = Integer.parseInt(br.readLine());
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      for(int i=0; i<n; i++) {
+        int card = Integer.parseInt(st.nextToken());
+        set.add(card);
+      }
+      
+      StringBuilder sb = new StringBuilder();
+      
+      //주어진 숫자 
+      int m = Integer.parseInt(br.readLine());
+      st = new StringTokenizer(br.readLine());
+      for(int i=0; i<m; i++) {
+        int num = Integer.parseInt(st.nextToken());
+        int result = set.contains(num) ? 1:0;
+        sb.append(result).append(" ");
+      }
+      
+      System.out.println(sb);
+  }
 }
