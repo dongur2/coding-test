1205b64100de5c7b0575bf879f8921ae2db330ca
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-05-02 00:33:26 +0900

    [level 2] Title: 괄호 회전하기, Time: 21.74 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,29 +1,40 @@
-import java.util.*;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
+//올바른 괄호 문자열이 되게 하는 회전 칸 수 - 불가능하면 0
 class Solution {
+    static int answer = 0;
+    
     public int solution(String s) {
-        int answer = 0;
-        String t = s + s;
-            
-        for(int i=0; i<s.length(); i++) {
-            if(isValid(t.substring(i, i+s.length()))) answer++;
+        int len = s.length();
+        
+        //똑같은 문자열을 뒤에 추가
+        StringBuffer sb = new StringBuffer(s);
+        sb.append(s);
+                
+        //슬라이딩 윈도우로 한칸씩 뒤로 밀면서 확인
+        for(int start=0; start<len; start++) {
+            String sub = sb.substring(start, start+len);
+
+            //스택 활용해서 확인
+            if(isValid(sub)) answer++;
         }
         
         return answer;
     }
     
-    boolean isValid(String word) {
-        Deque<Character> stack = new ArrayDeque<>();
-        for(char w:word.toCharArray()) {
-            if(w == '(' || w == '{' || w == '[') stack.push(w);
+    public static boolean isValid(String sub) {
+        Deque<Character> st = new ArrayDeque<>();
+        
+        for(char c:sub.toCharArray()) {
+            if(c == '(' || c == '{' || c == '[') st.push(c);
             else {
-                if(stack.isEmpty()) return false;
-                else if(stack.peek() == '(' && w == ')' 
-                        || stack.peek() == '{' && w == '}' 
-                        || stack.peek() == '[' && w == ']') stack.pop();
-                else return false;
-            }
+                if(st.isEmpty()) return false;
+                
+                char p = st.peek();
+                if((c == ')' && p == '(') || (c == '}' && p == '{') || (c == ']' && p == '[')) st.pop();
+            }            
         }
-        return stack.isEmpty();
+        return st.isEmpty();
     }
 }
