d884817a91585535b2e0dcdafc4fca0e4e5969f8
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-05-02 18:44:30 +0900

    [level 2] Title: 타겟 넘버, Time: 7.86 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,29 +1,22 @@
-import java.util.*;
-
-/*
-Q. 순서를 유지한 상태로 "더하거나 빼서" 타겟 넘버로 만들 수 있는 "총 방법 수"를 리턴
-
-- 시작 노드는 무조건 "0"
-- 다음 노드는 음수/양수 두가지 경우의 수로
-- 앞 노드는 방문할 필요 없으므로 인덱스를 하나씩 밀기
-- dfs
-*/
+//주어진 배열의 숫자를 순서를 유지한 채로 더하거나 빼서 타겟 숫자로 만드는 방법 수
 class Solution {
-    int cnt = 0;
+    static int answer = 0;
     
     public int solution(int[] numbers, int target) {
-        dfs(numbers, target, 0, 0); 
-        return cnt;    
+        dfs(numbers, target, 0, 0);
+        
+        return answer;
     }
     
-    void dfs(int[] numbers, int target, int now, int idx) {
-        //base case: 끝 노드라면 현재 숫자가 타겟과 같은지 확인 후 종료
+    public void dfs(int[] numbers, int target, int idx, int num) {
+        //마지막 인덱스까지 왔을 경우 값 비교
         if(idx == numbers.length) {
-            if(target == now) cnt++; return;
+            if(num == target) answer++;
+            return;
         }
         
-        //loop: 현재 숫자에 다음 노드 숫자를 빼거나 더함
-        dfs(numbers, target, now + numbers[idx], idx+1);
-        dfs(numbers, target, now - numbers[idx], idx+1);
+        //dfs로 각 원소마다 -, + 진행
+        dfs(numbers, target, idx+1, num+numbers[idx]);
+        dfs(numbers, target, idx+1, num-numbers[idx]);
     }
 }
