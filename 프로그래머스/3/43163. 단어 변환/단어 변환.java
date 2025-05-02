f128c8ba75886edc16e0f8cb0c42b0b8692cbd8d
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-05-02 18:35:04 +0900

    [level 3] Title: 단어 변환, Time: 0.43 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,62 +1,54 @@
-import java.util.*;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
-/*
-- 최소 변환 횟수 [bfs]
-- 한번에 글자 하나 
-*/
+//시작 단어를 타겟으로 바꾸는데 필요한 최소 작업 수 (불가능하면 0)
 class Solution {
-    String[] wordsTable;
-    String targetWord;
-    boolean[] visited;
+    static boolean[] visited;
     
     public int solution(String begin, String target, String[] words) {
-        wordsTable = words; targetWord = target;
         visited = new boolean[words.length];
         
-        return bfs(begin);
+        return bfs(begin, target, words);
     }
     
-    int bfs(String start) {
-        Queue<Node> q = new ArrayDeque<>();
-        q.offer(new Node(start, 0)); //현재 단어, 변환 횟수
+    public int bfs(String begin, String target, String[] words) {
+        Deque<Node> q = new ArrayDeque<>();
+        q.offer(new Node(begin, 0));
         
         while(!q.isEmpty()) {
-            Node cur = q.poll();
-            String nowWord = cur.word;
-            int nowCnt = cur.cnt;
+            Node curr = q.poll();
             
-            //단어 변환 완료했을 경우 리턴
-            if(nowWord.equals(targetWord)) return nowCnt;
+            //타겟 단어를 만들었을 경우 리턴
+            if(curr.word.equals(target)) return curr.cnt;
             
-            //새로운 단어 중에 글자 1개만 다른 단어로 변환
-            for(int i=0; i<wordsTable.length; i++) {
-                if(visited[i]) continue;
+            //방문한 적 없는 단어 중에 한글자만 다른 단어 찾아서 진행
+            for(int i=0; i<words.length; i++) {
+                if(visited[i] || !isValid(curr.word, words[i])) continue;
                 
-                //1개만 다른 글자 찾기
-                int diffCnt = 0, diffIdx = -1;
-                for(int idx=0; idx<start.length(); idx++) {
-                    if(nowWord.charAt(idx) != wordsTable[i].charAt(idx)) {
-                        diffCnt++; 
-                        diffIdx = idx;
-                    }
-                }
-                
-                //변환
-                if(diffCnt == 1) {
-                    StringBuilder tempWord = new StringBuilder(nowWord);
-                    tempWord.setCharAt(diffIdx, wordsTable[i].charAt(diffIdx));
-                    q.offer(new Node(tempWord.toString(), cur.cnt+1));
-                    visited[i] = true;                    
-                }
+                visited[i] = true;
+                q.offer(new Node(words[i], curr.cnt+1));
             }
         }
         
         return 0;
     }
     
-    class Node {
-        String word;
-        int cnt;
+    public boolean isValid(String word, String target) {
+        int cnt = 0;
+        
+        for(int i=0; i<word.length(); i++) {
+            if(word.charAt(i) != target.charAt(i)) cnt++;
+            
+            //두글자 이상 다를 경우 중지
+            if(cnt > 1) return false;
+        }
+        
+        return true;
+    }
+    
+    private class Node {
+        private String word;
+        private int cnt;
         
         public Node(String word, int cnt) {
             this.word = word;
