f37e415b56a782d728d55e598aa5817a2771e04e
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-05-04 03:50:42 +0900

    [level 1] Title: 인기있는 아이스크림, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/133024. 인기있는 아이스크림/인기있는 아이스크림.sql
+++ 프로그래머스/1/133024. 인기있는 아이스크림/인기있는 아이스크림.sql
@@ -1,5 +1,5 @@
--- 아이스크림 맛 조회
--- 상반기 총 주문량 내림차순 & 출하 번호 오름차순
-
-SELECT FLAVOR FROM FIRST_HALF 
+-- 맛
+-- 총주문량 내림차순, 출하번호 오름차순
+SELECT FLAVOR 
+FROM FIRST_HALF
 ORDER BY TOTAL_ORDER DESC, SHIPMENT_ID ASC;
