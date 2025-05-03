efc174a6bbf40093dec4635f80c45df098a97758
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-05-03 13:38:25 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.07 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,62 +1,54 @@
-import java.util.*;
-
-/*
-Q. 각 대기실별 거리두기 준수 여부 확인
-
-- 맨해튼 거리 확인
-1. 사람 주변 상하좌우 확인
-1-1. 확인한 자리에 파티션(X)이 있으면 그 위치에서의 탐색 중지 (준수)
-1-2. 확인한 자리에 사람(P)이 있으면 거리두기 규칙 준수 탐색을 중지하고 false 리턴 (미준수)
-1-3. 확인한 자리에 빈 테이블(O)이 있으면 그 위치에서 탐색 시작 (2차 탐색)
-
-2. 빈 테이블이 있는 위치에서 상하좌우 탐색 -> 사람이 있으면 false 리턴
-*/
 class Solution {
-    int[] rowArr = {0, 1, 0, -1};
-    int[] colArr = {1, 0, -1, 0};
+    static int[] answer = new int[] {1, 1, 1, 1, 1}; //거리두기를 지키지 않으면 0
     
-    int[] answer;
-    
+    static int[] dRow = new int[] {0, 1, 0, -1};
+    static int[] dCol = new int[] {1, 0, -1, 0};
+
     public int[] solution(String[][] places) {
-        answer = new int[places.length];
-        checkPlacesWithRule(places);
+        //대기실 5개
+        for(int i=0; i<5; i++) {
+            check(places, i);
+        }
+        
         return answer;
     }
     
-    void checkPlacesWithRule(String[][] places) {
-        for(int i=0; i<places.length; i++) {
-            answer[i] = doesFllowRule(places[i]) ? 1 : 0;
-        }
-    }
-    
-    boolean doesFllowRule(String[] place) {
-        for(int r=0; r<place.length; r++) {
-            for(int c=0; c<place[0].length(); c++) {
-                if(place[r].charAt(c) == 'P' && !isValidDistance(place, r, c, new boolean[place.length][place[0].length()], 1)) {
-                    return false;
+    public void check(String[][] places, int i) {
+        //대기실의 모든 자리를 탐색
+        for(int r=0; r<5; r++) {
+            if(answer[i] == 0) break;
+
+            for(int c=0; c<5; c++) {
+                if(answer[i] == 0) break;
+                
+                //사람이 앉아있으면 주변 맨해튼 거리 탐색 - 규칙을 지키지 않았을 경우에 0으로 대체
+                if(places[i][r].charAt(c) == 'P' && !isGood(places[i], new boolean[places.length][places[i].length], r, c, 1)) {
+                    answer[i] = 0;
                 }
             }
         }
+    }
+    
+    //P - human, O - table, X - Partition
+    public boolean isGood(String[] place, boolean[][] visited, int r, int c, int cnt) {
+        if(cnt == 3) return true;
+        
+        visited[r][c] = true;
+        for(int i=0; i<4; i++) {
+            int nxtR = r + dRow[i];
+            int nxtC = c + dCol[i];
+            
+            //영역 내이면서 파티션이 아닌 자리라면 진행
+            if(isValid(place, visited, nxtR, nxtC)) {
+                if(place[nxtR].charAt(nxtC) == 'P') return false;
+                if(!isGood(place, visited, nxtR, nxtC, cnt+1)) return false;
+            }
+        }
+        
         return true;
     }
     
-    //dfs
-    boolean isValidDistance(String[] place, int row, int col, boolean[][] visited, int phase) {
-        visited[row][col] = true;
-        
-        //base case: 3번째 루프면 반복 중지
-        if(phase == 3) return true;
-        
-        //loop: 빈테이블일 경우 재탐색
-        for(int i=0; i<4; i++) {
-            int r = row + rowArr[i];
-            int c = col + colArr[i];
-            
-            if(r < 0 || r >= 5 || c < 0 || c >= 5 || visited[r][c] || place[r].charAt(c) == 'X') continue;
-            
-            if(place[r].charAt(c) == 'P') return false;
-            if(!isValidDistance(place, r, c, visited, phase+1)) return false;
-        }
-        return true;
+    public boolean isValid(String[] place, boolean[][] visited, int r, int c) {
+        return r>=0 && r<5 && c>=0 && c<5 && place[r].charAt(c) != 'X' && !visited[r][c];
     }
 }
