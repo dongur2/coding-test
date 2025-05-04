ae1b8eb6c84e7c69b301fd811e891d1f31f1ae66
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-05-04 14:52:26 +0900

    [Gold I] Title: 구슬 탈출, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Gold/13459. 구슬 탈출/구슬 탈출.java
+++ 백준/Gold/13459. 구슬 탈출/구슬 탈출.java
@@ -1,136 +1,128 @@
-import java.util.*;
+import java.util.Deque;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
 import java.io.*;
+/*
+구멍이 하나 있는 보드에서 "10번 이하"로 보드를 움직여서
+"빨간 구슬만 구멍으로 빼낼 수 있는지" 여부 
 
-//10번 이하로 빨간 구슬을 움직여 빼낼 수 있는지 여부 (가능:1, 불가능:0)
+- 상하좌우로 이동 가능 
+- 빨간 구슬과 파란 구슬은 <동시에 한 방향>으로 장애물에 부딪혀 멈출 때까지 이동
+- 오직 빨간 구슬만 탈출 
+- 두 구슬은 동시에 같은 위치에 있을 수 없음 
+*/
 public class Main {
-    /*
-      파란 구슬을 구멍으로 빠뜨리지 않고 빨간 구슬을 빼내기
-      - 구슬은 상하좌우로 끝까지 이동 
-      - 두 공은 동시에 이동
-      - 두 공은 같은 칸에 동시에 위치할 수 없음
-      - . 빈 칸, # 장애물, O hole, R red ball, B blue ball
-    */
+    static int[] dRow = {0, 1, 0, -1};
+    static int[] dCol = {1, 0, -1, 0};
     
-    static int[] dRow = new int[] {0, 1, 0, -1};
-    static int[] dCol = new int[] {1, 0, -1, 0};
-    
+    static int n = 0, m = 0; //height n, width m
     static char[][] map;
     static boolean[][][][] visited;
-    static int n = 0, m = 0; // height:n, width:m
     
-    static int[] hole = new int[2];
-    
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    n = Integer.parseInt(st.nextToken());
-    m = Integer.parseInt(st.nextToken());
-    
-    map = new char[n][m];
-    visited = new boolean[n][m][n][m];
-
-    
-    int[] red = new int[2];
-    int[] blue = new int[2];
-    
-    //make map
-    for(int r=0; r<n; r++) {
-      String line = br.readLine();
-      for(int c=0; c<m; c++) {
-        map[r][c] = line.charAt(c); 
-        
-        //find locations of balls and hole
-        if(line.charAt(c) == 'B') {
-          blue[0] = r; blue[1] = c;
-        }
-        if(line.charAt(c) == 'R') {
-          red[0] = r; red[1] = c;
-        }
-        if(line.charAt(c) == 'O') {
-          hole[0] = r; hole[1] = c;
-        }
-      }
-    }
-    
-    //bfs
-    System.out.print(canEscape(red, blue));
-  }
-  
-  public static int canEscape(int[] startR, int[] startB) {
-    Deque<Balls> q = new ArrayDeque<>();
-    q.offer(new Balls(startR[0], startR[1], startB[0], startB[1], 1));
-    visited[startR[0]][startR[1]][startB[0]][startB[1]] = true;
-    
-    while(!q.isEmpty()) {
-      Balls curr = q.poll();
+    public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       
-      //10번 초과
-      if(curr.moves > 10) break;
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      n = Integer.parseInt(st.nextToken());
+      m = Integer.parseInt(st.nextToken());
       
-      for(int i=0; i<4; i++) {
-        int[] red = move(curr.rx, curr.ry, dRow[i], dCol[i]);
-        int[] blue = move(curr.bx, curr.by, dRow[i], dCol[i]);
-        
-        //파란 구슬이 탈출했을 경우 다른 방법으로 (파란공이 빠지면 무조건 불가)
-        if(blue[0] == hole[0] && blue[1] == hole[1]) continue;
-        
-        //빨간 구슬이 탈출하고
-        if(red[0] == hole[0] && red[1] == hole[1]) {
-          //파란 공도 빠졌으면 실패 (동시에 빠지는 경우)
-          if(blue[0] == hole[0] && blue[1] == hole[1]) continue;
-          //파란공은 안빠졌으면 성공
-          else return 1;
-        }
-
-        //최종 위치가 같을 경우 더 적게 움직인 구슬을 앞으로 (먼저 도착함)
-        if(red[0] == blue[0] && red[1] == blue[1]) {
-          if(red[2] < blue[2]) {
-            blue[0] -= dRow[i]; blue[1] -= dCol[i];
-          } else {
-            red[0] -= dRow[i]; red[1] -= dCol[i];
+      //맵 생성 
+      map = new char[n][m];
+      visited = new boolean[n][m][n][m]; // 두 공의 각 위치 방문 여부 
+      
+      int rx = 0, ry = 0, bx = 0, by = 0; //두 공의 위치 
+      for(int r=0; r<n; r++) {
+        char[] line = br.readLine().toCharArray();
+        for(int c=0; c<m; c++) {
+          map[r][c] = line[c];
+          
+          if(map[r][c] == 'R') {
+            rx = r; ry = c;
+          }
+          
+          if(map[r][c] == 'B') {
+            bx = r; by = c;
           }
         }
+      }
+      
+      System.out.print(bfs(rx, ry, bx, by, 1));
+    }
+    
+    public static int bfs(int rx, int ry, int bx, int by, int moves) {
+      Deque<Balls> q = new ArrayDeque<>();
+      q.offer(new Balls(rx, ry, bx, by, moves));
+      visited[rx][ry][bx][by] = true;
+      
+      while(!q.isEmpty()) {
+        Balls curr = q.poll();
         
-        //방문 체크 후 예약
-        if(!visited[red[0]][red[1]][blue[0]][blue[1]]) {
-          visited[red[0]][red[1]][blue[0]][blue[1]] = true;
-          q.offer(new Balls(red[0], red[1], blue[0], blue[1], curr.moves+1));
+        //10번 초과했을 경우 중지
+        if(curr.moves > 10) return 0;
+        
+        //상하좌우 탐색 - 공 각각 동시 이동 고려 
+        for(int i=0; i<4; i++) {
+          //공이 한 방향으로 굴러가 멈춘 최종 위치와 움직인 칸 개수 
+          int[] red = move(curr.rx, curr.ry, dRow[i], dCol[i]);
+          int[] blue = move(curr.bx, curr.by, dRow[i], dCol[i]);
+          
+          //만약 파란 공이 탈출했으면 다른 방향을 확인 (이번 방향은 실패)
+          if(map[blue[0]][blue[1]] == 'O') continue;
+          
+          //만약 빨간 공이 탈출했을 경우 
+          if(map[red[0]][red[1]] == 'O') {
+            //파란 공도 탈출했으면 다른 방향을 확인 
+            if(map[blue[0]][blue[1]] == 'O') continue;
+            return 1; //파란 공은 탈출못했으면 성공 
+          }
+          
+          //둘의 위치가 같으면 더 늦게 온 공을 뒤로 
+          if(red[0] == blue[0] && red[1] == blue[1]) {
+            if(red[2] < blue[2]) {
+              blue[0] -= dRow[i]; blue[1] -= dCol[i];
+            } else {
+              red[0] -= dRow[i]; red[1] -= dCol[i];
+            }
+          }
+          
+          //방문한 적 없으면 이동 확정 후 예약 
+          if(!visited[red[0]][red[1]][blue[0]][blue[1]]) {
+            visited[red[0]][red[1]][blue[0]][blue[1]] = true;
+            q.offer(new Balls(red[0], red[1], blue[0], blue[1], curr.moves+1));
+          }
         }
       }
+      
+      //반복문이 끝날 때까지 탈출 못했으면 실패
+      return 0;
     }
     
-    return 0;
-  }
-  
-  public static int[] move(int r, int c, int dr, int dc) {
-    int steps = 0;
-    
-    //영역 내 + 빈칸일 경우 이동
-    while(isValid(r+dr,c+dc) && map[r+dr][c+dc] != '#') {
-      r += dr;
-      c += dc;
-      steps++;
-      if(map[r][c] == 'O') break; //구멍에 도착했을 경우 중지
+    public static int[] move(int r, int c, int dr, int dc) {
+      int steps = 0; //공이 움직인 칸 개수 
+      
+      while(isValid(r+dr, c+dc)) {
+        r += dr;
+        c += dc;
+        steps++;
+        //만약 움직인 위치에 구멍이 있다면 탈출 
+        if(map[r][c] == 'O') break;
+      }
+      
+      return new int[] {r, c, steps};
     }
     
-    return new int[]{r, c, steps}; //최종 위치와 움직인 횟수 리턴
-  }
-  
-  //in map area, not hurdle
-  public static boolean isValid(int r, int c) {
-    return r>=0 && r<n && c>=0 && c<m;
-  }
-  
-  private static class Balls {
-    private int rx, ry, bx, by, moves;
-    
-    public Balls(int rx, int ry, int bx, int by, int moves) {
-      this.rx=rx;
-      this.ry=ry;
-      this.bx=bx;
-      this.by=by;
-      this.moves=moves;
+    public static boolean isValid(int r, int c) {
+      return r>=0 && r<n && c>=0 && c<m && map[r][c] != '#';
     }
-  }
+    
+    //두 공의 현재 위치와 움직임 횟수 
+    private static class Balls {
+      int rx, ry, bx, by, moves;
+      
+      public Balls(int rx, int ry, int bx, int by, int moves) {
+        this.rx = rx; this.ry = ry;
+        this.bx = bx; this.by = by;
+        this.moves = moves;
+      }
+    }
 }
