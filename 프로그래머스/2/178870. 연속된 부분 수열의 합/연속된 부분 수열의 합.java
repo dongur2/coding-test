e1e9e2eee789fbf92c6de7c2ded6daa8d943d64e
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-05-07 17:28:23 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 12.73 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,32 +1,29 @@
+//오름차순으로 정렬된 수열에서 원소의 합이 k가 되는 부분 수열의 시작-끝 인덱스
 class Solution {
     public int[] solution(int[] sequence, int k) {
-        int end = 0;
-        int sum = 0;
-        int len = -1;
-
-        int[] res = new int[2];
-
-        //1. 시작인덱스++
-        for(int start = 0; start<sequence.length; start++) {
-            //2. 끝인덱스가 배열 길이를 넘지 않고 부분 수열의 합이 k보다 작으면, 끝인덱스++ & 합에 추가
-            while (end<sequence.length && sum < k) {
-                sum += sequence[end++];
-            }
-
-            //3. 합이 k와 같을 경우, 이전 sum==k인 배열의 길이와 현재 배열 길이를 비교한 후,
-            //현재 배열 길이가 더 짧다면, 리턴할 인덱스와 전역 변수로 저장해 둔 배열 길이를 교체
-            if (sum == k) {
-                if(len == -1 || end - start < len) {
-                    res[0] = start;
-                    res[1] = end - 1;
-                    len = end - start;
+        int len = Integer.MAX_VALUE;
+        int[] answer = new int[2];
+        
+        //앞에서부터 시작
+        int left = 0, sum = 0;
+        
+        for(int right=0; right<sequence.length; right++) {
+            sum += sequence[right]; // 1, 3, 6, 10 (9, 7)
+            
+            //k보다 합이 크면 작거나 같아질 때까지 왼쪽 인덱스 땡기기
+            while(sum > k) sum -= sequence[left++];
+            
+            //현재 부분 수열의 합 = k: 이미 저장된 수열의 길이보다 "짧으면" 업데이트
+            //앞에서부터 탐색을 진행했으므로, <길이가 같으면 앞이 우선> 조건은 자동 충족 
+            if(sum == k) {
+                if(right - left < len) {
+                    len = right - left;
+                    answer[0] = left; answer[1] = right;
                 }
+                //더 짧은 길이가 가능한지 확인하기 위해 왼쪽 인덱스 땡기기
+                sum -= sequence[left++];
             }
-
-            //4. 시작인덱스가 오른쪽으로 이동할 것이므로, 배열에서 벗어나게 되는 원소를 합에서 빼기
-            sum -= sequence[start];
         }
-
-        return res;
+        return answer;
     }
 }
