259b98d7cc76464850dfcd03da7e4629ba1014ac
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-05-07 18:41:44 +0900

    [level 3] Title: 양과 늑대, Time: 4.90 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,47 +1,44 @@
 import java.util.*;
 
-//모을 수 있는 최대 양 마리수
+//모을 수 있는 최대 양 수
 class Solution {
     static int answer = 0;
-    static Map<Integer, List<Integer>> map = new HashMap<>();
     
     public int solution(int[] info, int[][] edges) {
-        makeGraph(edges); //연결관계    
-        dfs(info, new ArrayList<>(map.get(0)), 1, 0); //루트 노드에서 시작
+        //부모 노드:[자식 노드]
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for(int[] edge:edges) {
+            graph.computeIfAbsent(edge[0], k->new ArrayList<>()).add(edge[1]);
+        }
+        
+        //탐색
+        dfs(info, graph, 0, 1, 0, graph.get(0));
+        
         return answer;
     }
     
-    public void makeGraph(int[][] edges) {
-        for(int i=0; i<edges.length; i++) {
-            int[] edge = edges[i];
-            map.computeIfAbsent(edge[0], k->new ArrayList<>()).add(edge[1]);
-        }
-    }
-    
-    public void dfs(int[] info, List<Integer> nxtNodes, int s, int w) {
-        //최대 양 수 
-        answer= Math.max(answer, s);
+    public void dfs(int[] info, Map<Integer, List<Integer>> graph, int node, int sheep, int wolf, List<Integer> nxtNodes) {
+        //현재 가지고 있는 양의 수와 최대 수 비교 후 업데이트
+        answer = Math.max(sheep, answer);
         
         //자식 노드 탐색
-        for(int i=0; i<nxtNodes.size(); i++) {
-            int newSheep = s, newWolf = w;
+        for(int nxt:nxtNodes) {
+            int newSheep = sheep, newWolf = wolf;
             
-            //다음 노드
-            int nxt = nxtNodes.get(i);
-            
-            //양과 늑대 카운트
+            //다음 노드가 양/늑대
             if(info[nxt] == 0) newSheep++;
             else newWolf++;
             
-            //양 <= 늑대일 경우 이동하지 않음
-            if(newWolf >= newSheep) continue;
+            //양 <= 늑대면 방문하지 않음
+            if(newSheep <= newWolf) continue;
             
-            //양 > 늑대일 경우 이동
-            //다음 노드의 자식 노드 추가
-            List<Integer> newNxtNodes = new ArrayList<>(nxtNodes);
-            if(map.containsKey(nxt)) newNxtNodes.addAll(map.get(nxt));
-            newNxtNodes.remove(i); //현재 노드 삭제
-            dfs(info, newNxtNodes, newSheep, newWolf);
+            //양 > 늑대면 방문
+            else {
+                List<Integer> newNxtNodes = new ArrayList<>(nxtNodes);
+                newNxtNodes.remove(Integer.valueOf(nxt));
+                if(graph.containsKey(nxt)) newNxtNodes.addAll(graph.get(nxt));
+                dfs(info, graph, nxt, newSheep, newWolf, newNxtNodes);
+            }
         }
     }
 }
