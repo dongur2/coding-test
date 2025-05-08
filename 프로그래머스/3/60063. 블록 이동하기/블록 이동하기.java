cd1c46da223b9937cd96ccbe73d43529197bd5e1
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-05-08 12:12:16 +0900

    [level 3] Title: 블록 이동하기, Time: 84.18 ms, Memory: 221 MB -BaekjoonHub

--- 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.java
+++ 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.java
@@ -1,108 +1,156 @@
-import java.util.Deque;
-import java.util.ArrayDeque;
+import java.util.*;
 
+//0,0 -> n-1,n-1에 도착하는 최소 시간
 class Solution {
-    static int[] dRow = {0, 1, 0, -1};
-    static int[] dCol = {1, 0, -1, 0};
+    static int[] dRow = new int[] {0, 1, 0, -1};
+    static int[] dCol = new int[] {1, 0, -1, 0};
     
-    static int n;
-    static boolean[][][][] visited;
-
+    static int n = 0;
+    
     public int solution(int[][] board) {
         n = board.length;
-        visited = new boolean[n][n][n][n];
         return bfs(board);
     }
-
+    
     public static int bfs(int[][] board) {
-        Deque<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{0, 0, 0, 1, 0});
+        boolean[][][][] visited = new boolean[n][n][n][n];
+        Deque<Position> q = new ArrayDeque<>();
+        q.offer(new Position(0, 0, 0, 1, 0));        
         visited[0][0][0][1] = true;
-
-        while (!q.isEmpty()) {
-            int[] curr = q.poll();
-            int sr = curr[0], sc = curr[1], er = curr[2], ec = curr[3], moves = curr[4];
-
+        
+        while(!q.isEmpty()) {
+            Position curr = q.poll();
+            int lx = curr.lx;
+            int ly = curr.ly;
+            int rx = curr.rx;
+            int ry = curr.ry;
+            
             //도착
-            if ((sr == n - 1 && sc == n - 1) || (er == n - 1 && ec == n - 1)) return moves;
-
-            // 상하좌우 이동
-            for (int i = 0; i < 4; i++) {
-                int nSr = sr + dRow[i];
-                int nSc = sc + dCol[i];
-                int nEr = er + dRow[i];
-                int nEc = ec + dCol[i];
-
-                if (isValid(board, nSr, nSc, nEr, nEc)) {
-                    int[] pos = sortPosition(nSr, nSc, nEr, nEc);
-                    if (!visited[pos[0]][pos[1]][pos[2]][pos[3]]) {
-                        visited[pos[0]][pos[1]][pos[2]][pos[3]] = true;
-                        q.offer(new int[]{pos[0], pos[1], pos[2], pos[3], moves + 1});
-                    }
+            if((curr.lx == n-1 && curr.ly == n-1) || (curr.rx == n-1 && curr.ry == n-1)) return curr.moves;
+            
+            //로봇은 두칸 차지 - 가로/세로/회전 이동 
+            //상하좌우 이동
+            for(int i=0; i<4; i++) {
+                int nlx = lx + dRow[i]; int nly = ly + dCol[i];
+                int nrx = rx + dRow[i]; int nry = ry + dCol[i];
+                
+                //좌우 정렬
+                Position next = sortPosition(nlx, nly, nrx, nry, curr.moves+1);
+                if(isValid(board, visited, next)) {
+                    q.offer(next); 
+                    visited[next.lx][next.ly][next.rx][next.ry] = true;
                 }
             }
-
-            // 가로로 놓여있을 경우 회전 (22 '23' -> 22 '12', 22 '32'), (22 '23' -> 13 '23', 33 '23')
-            if (sr == er) {
-                for (int dir : new int[]{-1, 1}) {
-                    int nsr1 = sr + dir, nsc1 = sc;
-                    int nsr2 = er + dir, nsc2 = ec;
-
-                    if (isInBoard(board, nsr1, nsc1) && isInBoard(board, nsr2, nsc2)) {
-                        // 왼쪽 기준 회전 (왼쪽 고정)
-                        int[] pos1 = sortPosition(sr, sc, nsr1, nsc1);
-                        if (!visited[pos1[0]][pos1[1]][pos1[2]][pos1[3]]) {
-                            visited[pos1[0]][pos1[1]][pos1[2]][pos1[3]] = true;
-                            q.offer(new int[]{pos1[0], pos1[1], pos1[2], pos1[3], moves + 1});
-                        }
-                        // 오른쪽 기준 회전 (오른쪽 고정)
-                        int[] pos2 = sortPosition(er, ec, nsr2, nsc2);
-                        if (!visited[pos2[0]][pos2[1]][pos2[2]][pos2[3]]) {
-                            visited[pos2[0]][pos2[1]][pos2[2]][pos2[3]] = true;
-                            q.offer(new int[]{pos2[0], pos2[1], pos2[2], pos2[3], moves + 1});
+            
+            //회전
+            //가로로 놓여있을 경우: 행이 같음  (2,2)(2,3) -> 왼쪽 고정/오른쪽 고정 (-1,-1)(+1,-1)/(-1,+1)(+1,+1)
+            if(lx == rx) {
+                //왼쪽 고정하고 오른쪽을 위/아래로 회전
+                for(int i:new int[] {1, -1}) { //(2,2)(2,3)
+                    int nx1 = lx - i; 
+                    int ny1 = ly; //(1,2) //회전해서 도착하는 지점 
+                    int nx2 = rx - i; 
+                    int ny2 = ry; //(1,3) //회전하면서 거쳐가는 지점
+                    
+                    if(isInBoard(nx1, ny1) && isInBoard(nx2, ny2) && board[nx2][ny2] == 0 && board[nx1][ny1] == 0) {
+                        Position p = sortPosition(lx, ly, nx1, ny1, curr.moves+1);
+                    
+                        if(isValid(board, visited, p)) {
+                            q.offer(p);
+                            visited[p.lx][p.ly][p.rx][p.ry] = true;
                         }
                     }
                 }
-            }
-
-            // 세로로 놓여있을 경우 회전 ((22 '32' -> 22 '21', 22 '23'), ('22' 32 -> '31' 32, '33' 32))
-            if (sc == ec) {
-                for (int dir : new int[]{-1, 1}) {
-                    int nsr1 = sr, nsc1 = sc + dir;
-                    int nsr2 = er, nsc2 = ec + dir;
-
-                    if (isInBoard(board, nsr1, nsc1) && isInBoard(board, nsr2, nsc2)) {
-                        // 위쪽 기준 회전 (위쪽 고정)
-                        int[] pos1 = sortPosition(sr, sc, nsr1, nsc1);
-                        if (!visited[pos1[0]][pos1[1]][pos1[2]][pos1[3]]) {
-                            visited[pos1[0]][pos1[1]][pos1[2]][pos1[3]] = true;
-                            q.offer(new int[]{pos1[0], pos1[1], pos1[2], pos1[3], moves + 1});
+                
+                //오른쪽 고정하고 완쪽을 위/아래로 회전
+                for(int i:new int[] {1, -1}) { //(2,2)(2,3)
+                    int nx1 = rx - i; 
+                    int ny1 = ry; //(1,3) //회전해서 도착하는 지점 
+                    int nx2 = lx - i; 
+                    int ny2 = ly; //(1,2) //회전하면서 거쳐가는 지점
+                    
+                    if(isInBoard(nx1, ny1) && isInBoard(nx2, ny2) && board[nx2][ny2] == 0 && board[nx1][ny1] == 0) {
+                        Position p = sortPosition(rx, ry, nx1, ny1, curr.moves+1);
+                    
+                        if(isValid(board, visited, p)) {
+                            q.offer(p);
+                            visited[p.lx][p.ly][p.rx][p.ry] = true;
                         }
-                        // 아래쪽 기준 회전 (아래쪽 고정)
-                        int[] pos2 = sortPosition(er, ec, nsr2, nsc2);
-                        if (!visited[pos2[0]][pos2[1]][pos2[2]][pos2[3]]) {
-                            visited[pos2[0]][pos2[1]][pos2[2]][pos2[3]] = true;
-                            q.offer(new int[]{pos2[0], pos2[1], pos2[2], pos2[3], moves + 1});
+                    }
+                }
+            }
+            
+            //세로로 놓여있을 경우: 열이 같음
+            if(ly == ry) {
+                //위쪽 고정하고 아래쪽을 왼쪽/오른쪽으로 회전
+                for(int i:new int[] {1, -1}) { //(2,2)(3,2)
+                    int nx1 = lx;
+                    int ny1 = ly - i; //(2,1) //회전해서 도착하는 지점 
+                    int nx2 = rx;
+                    int ny2 = ry - i; //(3,1) //회전하면서 거쳐가는 지점
+                    
+                    if(isInBoard(nx1, ny1) && isInBoard(nx2, ny2) && board[nx2][ny2] == 0 && board[nx1][ny1] == 0) {
+                        Position p = sortPosition(lx, ly, nx1, ny1, curr.moves+1);
+                    
+                        if(isValid(board, visited, p)) {
+                            q.offer(p);
+                            visited[p.lx][p.ly][p.rx][p.ry] = true;
+                        }
+                    }
+                }
+                
+                //아래쪽 고정하고 위쪽을 왼쪽/오른쪽으로 회전
+                for(int i:new int[] {1, -1}) { //(2,2)(3,2)
+                    int nx1 = rx;
+                    int ny1 = ry - i; //(3,1) //회전해서 도착하는 지점 
+                    int nx2 = lx;
+                    int ny2 = ly - i; //(2,1) //회전하면서 거쳐가는 지점
+                    
+                    if(isInBoard(nx1, ny1) && isInBoard(nx2, ny2) && board[nx2][ny2] == 0 && board[nx1][ny1] == 0) {
+                        Position p = sortPosition(rx, ry, nx1, ny1, curr.moves+1);
+                    
+                        if(isValid(board, visited, p)) {
+                            q.offer(p);
+                            visited[p.lx][p.ly][p.rx][p.ry] = true;
                         }
                     }
                 }
             }
         }
-        return 0; // 큐 다 돌고도 도착 못한 경우
+        return -1;
     }
-
-    public static boolean isValid(int[][] board, int sr, int sc, int er, int ec) {
-        return isInBoard(board, sr, sc) && isInBoard(board, er, ec)
-                && board[sr][sc] == 0 && board[er][ec] == 0;
+    
+    //항상 왼쪽이 더 작게 
+    public static Position sortPosition(int lx, int ly, int rx, int ry, int moves) {
+        if (lx == rx) { // 가로 방향이면 y 오름차순 기준
+            if (ly > ry) {
+                int tmpx = lx, tmpy = ly;
+                lx = rx; ly = ry;
+                rx = tmpx; ry = tmpy;
+            }
+        } else { // 세로 방향이면 x 오름차순 기준
+            if (lx > rx) {
+                int tmpx = lx, tmpy = ly;
+                lx = rx; ly = ry;
+                rx = tmpx; ry = tmpy;
+            }
+        }
+        return new Position(lx, ly, rx, ry, moves);
     }
-
-    public static boolean isInBoard(int[][] board, int r, int c) {
-        return r >= 0 && r < n && c >= 0 && c < n && board[r][c] == 0;
+    
+    public static boolean isValid(int[][] board, boolean[][][][] visited, Position p) {
+        return p.lx >= 0 && p.lx < n && p.rx >= 0 && p.rx < n && p.ly >= 0 && p.ly < n && p.ry >= 0 && p.ry < n && !visited[p.lx][p.ly][p.rx][p.ry] && board[p.lx][p.ly] == 0 && board[p.rx][p.ry] == 0;
     }
-
-    //정렬 (앞뒤가 뒤바뀔 수 있음)
-    public static int[] sortPosition(int sr, int sc, int er, int ec) {
-        if (sr < er || (sr == er && sc < ec)) return new int[]{sr, sc, er, ec};
-        else return new int[]{er, ec, sr, sc};
+    
+    public static boolean isInBoard(int x, int y) {
+        return x>=0 && x<n && y>=0 && y<n;
+    }
+    
+    private static class Position {
+        int lx, ly, rx, ry, moves;
+        
+        public Position(int lx, int ly, int rx, int ry, int m) {
+            this.lx=lx; this.ly=ly; this.rx=rx; this.ry=ry; this.moves=m;
+        }
     }
 }
