9d01859ed9baf9f49a6288d0975842c6be4fbf77
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-05-08 12:37:00 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 12.04 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,30 +1,35 @@
+import java.util.Map;
+import java.util.HashMap;
+//문자열을 숫자로 변환
 class Solution {
-    String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-    
     public int solution(String s) {
         StringBuilder answer = new StringBuilder();
-        StringBuilder res = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         
-        for(char c:s.toCharArray()) {
-            //숫자일 경우 추가
-            if(c <= 97) answer.append(c+"");
-            //글자일 경우 보관 후 변환해서 추가
-            else {
-                res.append(c+"");
-                
-                for(int i=0; i<arr.length; i++) {
-                    if(arr[i].equals(res.toString())) {
-                        answer.append(i+"");
-                        res = new StringBuilder();
-                    }
+        Map<String, Integer> map = new HashMap<>();
+        map.put("one", 1); map.put("1", 1);
+        map.put("two", 2); map.put("2", 2);
+        map.put("three", 3); map.put("3", 3);
+        map.put("four", 4); map.put("4", 4);
+        map.put("five", 5); map.put("5", 5);
+        map.put("six", 6); map.put("6", 6);
+        map.put("seven", 7); map.put("7", 7);
+        map.put("eight", 8); map.put("8", 8);
+        map.put("nine", 9); map.put("9", 9);
+        map.put("zero", 0); map.put("0", 0);
+        
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if(!map.containsKey(c+"")) {
+                sb.append(c+"");
+                if(map.containsKey(sb.toString())) {
+                    answer.append(map.get(sb.toString())+"");
+                    sb = new StringBuilder();
                 }
-            }
-        }   
+            } else answer.append(map.get(c+""));
+        }
         
         return Integer.parseInt(answer.toString());
-    }
-    
-    private void checkAndRevertInt(StringBuilder res) {
-        
     }
 }
