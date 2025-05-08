871af2d9a80f4c6f8357723eea4cf1eb91f8062e
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-05-08 16:34:39 +0900

    Time: 185 ms (32.82%), Space: 41.5 MB (78.44%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,56 +1,49 @@
-import java.util.*;
-
+//보드에 단어가 있는지 여부 리턴 
 class Solution {
-    /* 주어진 단어(word)가 주어진 그리드(board)에 존재하는지 여부를 리턴 - 상하좌우로 이동 가능 & 중복 방문 불가능 
-    1. 전체 노드를 차례로 순회
-    2. 단어 첫 글자를 만나면 탐색 시작
-    3. 상하좌우를 탐색 - 다음 글자일 경우에 큐에 추가하고 그 노드로 이동하여 재탐색
-    4. 다음 노드가 끝 글자일 경우 즉시 true 리턴 
-    5. 모든 반복이 끝났음에도 단어를 찾지 못했다면 false 리턴
-    */
-    
-    int rowLen, colLen;
-    char[][] copiedBoard; String copiedWord;
-    boolean isContained = false; //단어 존재 여부
-    int[] rowArr = {0, 1, 0, -1}, colArr = {1, 0, -1, 0};
-    
+    //상하좌우 인접
+    static int[] dRow = new int[] {0, 1, 0, -1};
+    static int[] dCol = new int[] {1, 0, -1, 0};
+
+    static int m = 0, n = 0;
+    static boolean[][] visited;
+
     public boolean exist(char[][] board, String word) {
-        rowLen = board.length; colLen = board[0].length;
-        copiedBoard = board; copiedWord = word;
-        
-        //전체 노드를 차례로 순회하며 첫글자를 만나면 dfs 순회 시작
-        for(int r=0; r<rowLen; r++) {
-            for(int c=0; c<colLen; c++) {
-                if(board[r][c] == word.charAt(0)) dfs(new boolean[rowLen][colLen], r, c, 0); //visited, row, col, charAt(idx)
+        m = board.length; n = board[0].length;
+        visited = new boolean[m][n];
+
+        //모든 칸을 차례로 탐색하면서 첫 글자를 발견하면 그 자리에서 dfs로 다음 글자 탐색
+        for(int r=0; r<m; r++) {
+            for(int c=0; c<n; c++) {
+                if(board[r][c] == word.charAt(0)) {
+                    visited[r][c] = true;
+                    if(dfs(board, word, r, c, 1)) return true;
+                    visited[r][c] = false;
+                }
             }
-            if(isContained) return true;
         }
+        //탐색이 끝날 때까지 단어를 찾지 못했으면 실패
         return false;
     }
-    
-    void dfs(boolean[][] visited, int row, int col, int idx) {
-        visited[row][col] = true; // 0, 0, 0 (A)
-        
-        if(idx == copiedWord.length() - 1) {
-            isContained = true; 
-            return;
-        }
-        
-        //상하좌우 탐색
+
+    public static boolean dfs(char[][] board, String word, int row, int col, int idx) {
+        //모든 문자를 다 찾았으면 종료
+        if(idx == word.length()) return true;
+
         for(int i=0; i<4; i++) {
-            int nxtR = row + rowArr[i];
-            int nxtC = col + colArr[i];
-            
-            //B
-            if(isValid(nxtR, nxtC, visited) && copiedWord.charAt(idx+1) == copiedBoard[nxtR][nxtC]) {
-                dfs(visited, nxtR, nxtC, idx+1);
+            int nr = row + dRow[i];
+            int nc = col + dCol[i];
+
+            if(isValid(nr, nc) && board[nr][nc] == word.charAt(idx)) {
+                visited[nr][nc] = true;
+                if(dfs(board, word, nr, nc, idx+1)) return true;
+                visited[nr][nc] = false;
             }
         }
-        
-        visited[row][col] = false;
+
+        return false;
     }
-    
-    boolean isValid(int r, int c, boolean[][] visited) {
-        return r >= 0 && r < rowLen && c >= 0 && c < colLen && !visited[r][c];
+
+    public static boolean isValid(int r, int c) {
+        return r>=0 && r<m && c>=0 && c<n && !visited[r][c];
     }
 }
