be8d75742020393223dba1368f0cd0eb2a405266
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-05-10 19:19:52 +0900

    [level 2] Title: 귤 고르기, Time: 92.67 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,39 +1,27 @@
 import java.util.*;
-
-//귤 k개를 최대한 다양하게 골랐을 때 종류 개수
+/*
+귤 k개를 골랐을 때 최소 종류 개수 
+*/
 class Solution {
+    //귤 개수 10^5, 귤 종류 10^7
     public int solution(int k, int[] tangerine) {
-        //종류별 개수 카운트
+        //종류별로 개수 세서 저장 - {종류:개수}
         Map<Integer, Integer> map = new HashMap<>();
-        countType(map, tangerine);
-        
-        //개수 많은 것부터 정렬
-        Queue<int[]> q = new PriorityQueue<>(new Comparator<int[]>(){
-            @Override
-            public int compare(int[] t1, int[] t2) {
-                return t2[1] - t1[1];
-            }
-        });
-        
-        for(int type : map.keySet()) {
-            int cnt = map.get(type);
-            q.offer(new int[]{type, cnt});
-        }
-        
-        //가장 많은 개수가 있는 종류부터 선택
-        int box = 0, answer= 0;
-        while(box < k) {
-            int[] cur = q.poll();
-            answer++; //새로운 종류 카운트
-            box += cur[1];
-        }
-        
-        return answer;
-    }
-    
-    void countType(Map<Integer, Integer> map, int[] tangerine) {
-        for(int t : tangerine) {
+        for(int t:tangerine) {
             map.put(t, map.getOrDefault(t, 0)+1);
         }
+        
+        //종류별 개수만 추출
+        List<Integer> list = new ArrayList<>(map.values());
+        
+        //정렬
+        list.sort(Collections.reverseOrder());
+        
+        int sum = 0, answer = 0;
+        for(int l:list) {
+            sum += l; answer++;
+            if(sum >= k) return answer;
+        }
+        return answer;
     }
 }
