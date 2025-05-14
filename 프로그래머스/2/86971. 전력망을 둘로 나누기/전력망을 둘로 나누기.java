d26e65204cbfe2a958c0c70c090be11b51f170df
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-05-14 21:08:09 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 11.70 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,73 +1,49 @@
 import java.util.*;
-
+//n개 노드가 하나로 연결
+//간선 하나를 없애서 그래프 2개로 분할 - 각 그래프의 개수 최대한 비슷하게 
+//두 그래프의 노드 개수 차이 리턴
 class Solution {
-    int answer = Integer.MAX_VALUE;
+    static int answer = Integer.MAX_VALUE;
     
-    public int solution(int n, int[][] wires) {
-        //배열의 연결 관계를 처음부터 끝까지 차례대로 하나씩 끊어보기 위한 순회
-        for(int i=0; i<wires.length; i++) {
-            countDividedGraph(wires, n, i);
-        }
+    public int solution(int n, int[][] wires) {        
+        boolean[] visited;
         
+        //간선 하나씩 없앤 모든 경우의 수
+        for(int i=0; i<wires.length; i++) {
+            Map<Integer, List<Integer>> graph = new HashMap<>();
+            makeGraph(wires, graph, i);
+            
+            //각 그래프의 노드 개수를 dfs로 탐색
+            visited = new boolean[n+1];
+            for(int node=1; node<=n; node++) {
+                // if(visited[node]) continue; 
+                int cnt = dfs(graph, visited, node, 1); //현재 노드가 속한 그래프의 노드 개수 카운트
+                int other = Math.abs(n-cnt); //다른 그래프의 노드 개수
+                answer = Math.min(answer, Math.abs(other-cnt)); //현재 노드 차이와 기존 노드 차이 중 작은 값 저장
+                break;
+            }
+        }
         return answer;
     }
     
-    private void countDividedGraph(int[][] wires, int n, int unconnect) {
-        //그래프화:해시테이블 {노드번호: {연결노드번호목록}}
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        makeGraph(wires, graph, unconnect);
-        
-        //만든 그래프를 순회
-        Queue<Integer> waiting = new ArrayDeque<>();
-        boolean[] visited = new boolean[n];
-        
-        waiting.offer(1); //첫번째 노드
-        visited[0] = true;
-        
-        while(!waiting.isEmpty()) {
-            int cur = waiting.poll();
-            if(graph.get(cur) == null) break;
-            
-            for(int connection : graph.get(cur)) {
-                if(visited[connection-1]) continue;
-                
-                visited[connection-1] = true;
-                waiting.offer(connection);
-            }
-        }
-        
-        countNodes(visited);
-    }
-    
-    private void makeGraph(int[][] wires, Map<Integer, List<Integer>> graph, int unconnect) {
+    public static void makeGraph(int[][] wires, Map<Integer, List<Integer>> g, int idx) {
         for(int i=0; i<wires.length; i++) {
-            //현재 끊으려고 하는 연결관계라면 추가하지 않음
-            if(i == unconnect) continue;
-            
-            int baseNode = wires[i][0];
-            int connectedNode = wires[i][1];
-            
-            //연결된 노드관계를 추가
-            addConnection(graph, baseNode, connectedNode); //현재 노드에 추가
-            addConnection(graph, connectedNode, baseNode); //연결된 상대 노드에도 현재 노드를 추가
+            if(i == idx) continue; //간선 제거
+            g.computeIfAbsent(wires[i][0], k->new ArrayList<>()).add(wires[i][1]);
+            g.computeIfAbsent(wires[i][1], k->new ArrayList<>()).add(wires[i][0]);
         }
     }
     
-    private void addConnection(Map<Integer, List<Integer>> graph, int from, int to) {
-        List<Integer> connections = graph.getOrDefault(from, new ArrayList<>());
-        connections.add(to);
-        graph.put(from, connections);
-    }
-    
-    private void countNodes(boolean[] visited) {
-        int graph1 = 0, graph2 = 0;
+    public static int dfs(Map<Integer, List<Integer>> graph, boolean[] visited, int node, int cnt) {
+        int count = 1;
+        visited[node] = true;
         
-        //방문한 노드 개수 카운트
-        for(boolean v:visited) {
-            if(v) graph1++; else graph2++;
+        if(graph.get(node) == null) return cnt;
+        
+        for(int next:graph.get(node)) {
+            if(visited[next]) continue;
+            count += dfs(graph, visited, next, cnt+1);
         }
-        
-        //노드 개수 차이 최소값 저장
-        answer = Math.min(answer, Math.abs(graph1 - graph2));
+        return count;
     }
 }
