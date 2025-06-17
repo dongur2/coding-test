235edb0ed90b6bc6a2c72ff0a042913f391d3cc7
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-06-17 22:11:56 +0900

    Time: 16 ms (58.13%), Space: 44 MB (95.16%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,54 +1,22 @@
-import java.util.*;
-
-/*
-Q. 주어진 동전으로 목표 금액을 만들 수 있을 때, 필요한 동전의 "최소 개수"를 리턴
-- 만들 수 없으면 -1 리턴
-
-- 최소 개수니까 bfs 적용
-*/
+//주어진 가격을 만드는 데 필요한 동전 개수 (불가능하면 -1)
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        return bfs(coins, amount);        
-    }
-    
-    int bfs(int[] coins, int amount) {
-        //setting
-        Queue<Node> q = new ArrayDeque<>();
-        q.offer(new Node(amount, 0));
-        
-        Set<Integer> log = new HashSet<>();
-        
-        //queue
-        while(!q.isEmpty()) {
-            //now
-            Node cur = q.poll();
-            int curAmt = cur.now;
-            int curCnt = cur.cnt;
-            
-            if(curAmt == 0) return curCnt;
-            
-            //next
+        //table[i] = 금액 i를 만드는 데 필요한 최소 동전 개수
+        int[] table = new int[amount + 1]; 
+        Arrays.fill(table, Integer.MAX_VALUE); //기본값 INF
+        table[0] = 0; //출발 
+
+        //1원부터 amount까지 계산 
+        for(int i = 1; i <= amount; i++) {
+            //각 동전 시도 
             for(int coin:coins) {
-                int nxtAmt = curAmt - coin;
-                
-                if(nxtAmt >= 0 && !log.contains(nxtAmt)) {
-                    if(nxtAmt == 0) return curCnt+1;
-                    
-                    q.offer(new Node(nxtAmt, curCnt+1));
-                    log.add(nxtAmt);   
+                //i-coin이 음수가 아니고, 그 금액을 만들 수 있으면 시도('동전을 더하기 전 금액'을 만들 수 있었는지 확인)
+                if(i - coin >= 0 && table[i - coin] != Integer.MAX_VALUE) {
+                    //'동전을 더하기 전 금액을 만든 방법 개수 + 1'이 총 방법 개수: 기존 값보다 작을 경우에만 업데이트
+                    table[i] = Math.min(table[i], 1 + table[i - coin]);
                 }
             }
         }
-        return -1;
-    }
-    
-    class Node {
-        int now;
-        int cnt;
-        
-        public Node(int now, int cnt) {
-            this.now = now;
-            this.cnt = cnt;
-        }
+        return (table[amount] == Integer.MAX_VALUE) ?  -1 : table[amount];
     }
 }
