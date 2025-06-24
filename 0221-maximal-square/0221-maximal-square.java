5d8819f0e668dd2e9aee712784137bae9c4a1aa1
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-06-24 15:49:32 +0900

    Time: 7 ms (88.87%), Space: 60.3 MB (57.91%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,24 +1,24 @@
-//1로만 이루어진 가장 넓은 '정사각형' 영역의 넓이 리턴 
+//주어진 맵에서 만들 수 있는 가장 큰 정사각형의 넓이
 class Solution {
-    static int m, n;
-
     public int maximalSquare(char[][] matrix) {
-        m = matrix.length; n = matrix[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-        //가능한 한 변의 최대 길이 저장 
-        int[][] dp = new int[m+1][n+1];
+        //가장 긴 한 변의 길이 
+        int max = 0;
 
-        int max = 0; //최대값
+        //해당 칸을 포함하는 가장 큰 정사각형의 한 변 길이 
+        int[][] w = new int [m+1][n+1];
 
-        //1인 칸에서만 확인 - (2*2 구역 내 가장 작은값 + 1)이 가능한 한 변의 최대 길이가 된다. 
-        for(int i=1; i<=m; i++) {
-            for(int j=1; j<=n; j++) {
-                if(matrix[i-1][j-1] == '1') {
-                    dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + 1;
-                    max = Math.max(max, dp[i][j]);
-                }
+        //위, 왼쪽, 왼쪽위대각선 확인 
+        //matrix[r-1][c-1] 위치의 '1'을 오른쪽 아래 끝점으로 삼는 가장 큰 정사각형의 한 변의 길이를 저장
+        for(int r=1; r<=m; r++) {
+            for(int c=1; c<=n; c++) {
+                if(matrix[r-1][c-1] == '1') w[r][c] = Math.min(Math.min(w[r-1][c-1], w[r-1][c]), w[r][c-1]) + 1;
+                max = Math.max(max, w[r][c]);
             }
         }
+
         return max * max;
     }
 }
