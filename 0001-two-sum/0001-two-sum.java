7c10a96116115b0fd9e3657547703e6dab875337
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-06-25 17:06:22 +0900

    Time: 2 ms (98.94%), Space: 45.2 MB (31.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,3 +1,6 @@
+import java.util.Map;
+import java.util.HashMap;
+
 //주어진 배열에서 더해서 target을 만들 수 있는 두 숫자의 '인덱스' 리턴 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
@@ -5,14 +8,36 @@ class Solution {
             1. 한가지 방법만 가능하도록 주어지며,
             2. 같은 위치 숫자를 중복해서 사용할 수 없음
          */
-         
-        //O(N^2 -- 10^8)
+        // return sol1(nums, target);
+        return sol2(nums, target);
+    }
+
+    //O(N^2 -- 10^8)
+    private int[] sol1(int[] nums, int target) {
         for(int i=0; i<nums.length-1; i++) {
             for(int j=i+1; j<nums.length; j++) {
-                if(nums[i] + nums[j] == target) return new int[] {i, j};
+                if(nums[i] + nums[j] == target) return new int[] {i, j}; //합이 target이 되면 즉시 리턴 
             }
+        }
+        return new int[] {};
+    }
+
+    //O(N)
+    private int[] sol2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(); //값:인덱스
+
+        //O(N)
+        for (int i=0; i<nums.length; i++) {
+            int need = target - nums[i]; //target을 만들기 위해 필요한 숫자 
+
+            //필요한 숫자가 이미 맵에 저장돼있으면 리턴 O(1)
+            if (map.containsKey(need)) return new int[] { map.get(need), i };
+            
+            //맵에 없으면 현재 숫자를 맵에 추가
+            map.put(nums[i], i); 
         }
 
         return new int[] {};
     }
+
 }
