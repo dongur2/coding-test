de273e029faf5f1ef0a32762f465bef03fbce39c
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-07-19 01:30:57 +0900

    Time: 1 ms (70.55%), Space: 42.3 MB (63.58%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -1,25 +1,20 @@
-import java.util.*;
-
-//두 문자열이 어떤 문자열 x를 여러 번 이어붙여 만든 것이라면, 가장 긴 x
+//두 문자열이 어떤 문자열 x를 여러 번 붙여 만든 것이라면 -> x 구하기 
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        //str1과 str2는 어떤 문자열 x의 반복으로 이루어져있다.
-        //str1과 str2에 공통된 문자열 x가 없는 경우도 있다.
+        //두 문자열이 같은 패턴으로 생성되었는지 확인
+        //서로 순서를 바꿔 이어 붙였을 때 두 경우의 문자열이 같으면 같은 패턴
+        //ABABAB, AB -> ABABABAB == ABABABAB
+        if(!(str1+str2).equals(str2+str1)) return ""; //같은 패턴이 아니면 답 없음 
 
-        //모든 경우로 조각낸 문자열 저장
-        List<String> list = new ArrayList<>();
-        for(int i=1; i<=str1.length(); i++) {
-            list.add(str1.substring(0, i));
-        }
+        //공통 패턴의 길이는 최대공약수
+        //str1.length() = x.length의 배수 = str2.length() - x를 여러 번 이어붙였으므로 
+        int gcdLen = gcd(str1.length(), str2.length()); 
+        
+        return str1.substring(0, gcdLen);
+    }
 
-        //리스트 마지막부터 순회 - 긴 문자열부터 확인
-        //해당 문자열로 기존 문자열을 나눴을 때 배열이 비었으면 정답 (즉시 리턴)
-        for(int i=list.size()-1; i>=0; i--) {
-            String[] a1 = str1.split(list.get(i));
-            String[] a2 = str2.split(list.get(i));            
-            if(a1.length == 0 && a2.length == 0) return list.get(i);
-        }
-
-        return "";
+    //최대공약수 
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
     }
 }
