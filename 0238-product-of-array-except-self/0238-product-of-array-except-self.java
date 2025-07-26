1190f2d966349bc6e0e8f89574118906d0e70e48
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-07-27 01:16:18 +0900

    Time: 3 ms (19.77%), Space: 55.6 MB (49.95%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,59 +1,30 @@
-//answer[i] = nums[i]를 제외한 nums의 모든 원소의 곱 
+/*
+    숫자 배열 nums가 주어질 때, nums[i]를 제외한 모든 nums의 원소들의 곱이 되는 answer[i]
+    O(N)
+ */
 class Solution {
-    public int[] productExceptSelf(int[] nums) { //10^5
-        // return sol1(nums);
-        return sol2(nums);
-    }
-
-    private int[] sol2 (int[] nums) {
+    public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] answer = new int[n];
 
-        Arrays.fill(answer, 1);
+        int[] res = new int[n];
+        Arrays.fill(res, 1);
 
         int curr = 1;
 
-        //앞에서부터 곱
+        //앞에서부터 곱하기
         for(int i=0; i<n; i++) {
-            answer[i] *= curr;  
+            res[i] *= curr;
             curr *= nums[i];
         }
 
         curr = 1; //초기화
 
-        //뒤에서부터 곱
+        //뒤에서부터 곱하기
         for(int i=n-1; i>=0; i--) {
-            answer[i] *= curr;
+            res[i] *= curr;
             curr *= nums[i];
         }
 
-        return answer;
-    }
-    
-    //O(N)
-    private int[] sol1 (int[] nums) {
-        int n = nums.length;
-        int[] answer = new int[n];
-        
-        int[] pre = new int[n]; //앞
-        int[] suff = new int[n]; //뒤
-
-        //각 배열의 시작점 1
-        pre[0] = 1;
-        suff[n-1] = 1;
-
-        for(int i=1; i<n; i++) {
-            pre[i] = pre[i-1] * nums[i-1];
-        }
-
-        for(int i=n-2; i>=0; i--) {
-            suff[i] = suff[i+1] * nums[i+1];
-        }
-
-        for(int i=0; i<n; i++) {
-            answer[i] = pre[i] * suff[i];
-        }
-
-        return answer; 
+        return res;
     }
 }
