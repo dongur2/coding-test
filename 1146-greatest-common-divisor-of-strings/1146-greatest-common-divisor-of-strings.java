85ca08f775190a1985a7610ac44cd2d8ffa46922
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-07-26 23:35:20 +0900

    Time: 0 ms (100%), Space: 42.5 MB (24.79%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -1,20 +1,19 @@
-//두 문자열이 어떤 문자열 x를 여러 번 붙여 만든 것이라면 -> x 구하기 
+//주어진 두 문자열에서 반복되는 공통 문자열 중 가장 긴 문자열 리턴 
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        //두 문자열이 같은 패턴으로 생성되었는지 확인
-        //서로 순서를 바꿔 이어 붙였을 때 두 경우의 문자열이 같으면 같은 패턴
-        //ABABAB, AB -> ABABABAB == ABABABAB
-        if(!(str1+str2).equals(str2+str1)) return ""; //같은 패턴이 아니면 답 없음 
+        //두 문자열이 같은 문자열의 반복으로 이루어졌을 경우, 순서바꿔서 붙여도 같은 문자열
+        if(!(str1+str2).equals(str2+str1)) return "";
 
-        //공통 패턴의 길이는 최대공약수
-        //str1.length() = x.length의 배수 = str2.length() - x를 여러 번 이어붙였으므로 
-        int gcdLen = gcd(str1.length(), str2.length()); 
-        
-        return str1.substring(0, gcdLen);
+        /*
+            <같은 문자열의 반복>으로 이루어진 두 문자열은 <"몫"만 다르다>는 말과 같음
+            그럼 최대공약수를 구해서 확인
+         */
+        int len = gcd(str1.length(), str2.length());
+        return str1.substring(0, len);
     }
 
-    //최대공약수 
     private int gcd(int a, int b) {
-        return b == 0 ? a : gcd(b, a % b);
+        if(b == 0) return a;
+        return gcd(b, a % b);
     }
 }
