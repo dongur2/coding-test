f735cb828314ad86717f7e3221c2828fb2ac0a9b
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-07-29 01:17:03 +0900

    Time: 0 ms (100%), Space: 41.5 MB (32.6%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.java
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.java
@@ -7,19 +7,13 @@
  */
 class Solution {
     public int largestAltitude(int[] gain) {
-        int[] res = new int[gain.length+1];
-
-        //값 복사 
-        for(int i=0; i<gain.length; i++) {
-            res[i+1] = gain[i];
-        }
-
-        int max = res[0]; //0
+        int max = 0; //0에서 시작 
+        int res = 0;
 
         //포인트 차례로 이동 
-        for(int i=1; i<=gain.length; i++) {
-            res[i] += res[i-1];
-            max = Math.max(max, res[i]); //최대 이득 업데이트
+        for(int i=0; i<gain.length; i++) {
+            res += gain[i]; //누적합
+            max = Math.max(max, res); //최대 이득 업데이트
         }
 
         return max;
