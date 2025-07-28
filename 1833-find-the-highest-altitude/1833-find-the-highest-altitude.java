9bb34a114106eee10e412e79542f78a2aac73d29
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-07-29 01:15:26 +0900

    Time: 0 ms (100%), Space: 41.4 MB (44.07%) - LeetHub

---
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.java
@@ -0,0 +1,27 @@
+/*
+    여행 루트는 서로 다른 고도의 (n+1)개 지점으로 이루어져 있음
+    바이커는 지점 0(고도가 0)에서 출발한다.
+
+    길이가 n인 숫자 배열 gain이 주어진다. gain[i]는 i지점과 i+1지점 사이의 순수익이다. 
+    지점 중 가장 높은 이익을 리턴한다.
+ */
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int[] res = new int[gain.length+1];
+
+        //값 복사 
+        for(int i=0; i<gain.length; i++) {
+            res[i+1] = gain[i];
+        }
+
+        int max = res[0]; //0
+
+        //포인트 차례로 이동 
+        for(int i=1; i<=gain.length; i++) {
+            res[i] += res[i-1];
+            max = Math.max(max, res[i]); //최대 이득 업데이트
+        }
+
+        return max;
+    }
+}
