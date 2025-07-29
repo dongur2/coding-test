e283be266501ae46fee3a16a20c3a24804de9bb2
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-07-29 23:27:51 +0900

    Time: 57 ms (20.06%), Space: 47.4 MB (96.95%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
@@ -1,10 +1,42 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 /*
     서로 같은 (i번째 행, j번째 열)의 개수 
  */
 class Solution {
     public int equalPairs(int[][] grid) {
+        int n = grid.length; //n*n
+        int cnt = 0;
+        
+        //행:등장 횟수 
+        Map<String, Integer> map = new HashMap<>();
+        for(int r=0; r<n; r++) {
+            StringBuilder row = new StringBuilder();
+
+            for(int c=0; c<n; c++) {
+                row.append(grid[r][c]+",");
+            }
+
+            map.put(row.toString(), map.getOrDefault(row.toString(), 0)+1);
+        }
+
+        //열: 맵에 저장된 행과 일치하는 값이 있다면 - 값을 카운트에 더함
+        for(int c=0; c<n; c++) {
+            StringBuilder col = new StringBuilder();
+
+            for(int r=0; r<n; r++) {
+                col.append(grid[r][c]+",");
+            }
+
+            if(map.containsKey(col.toString())) cnt += map.get(col.toString());
+        }
+ 
+        return cnt;
+    }
+
+    private int sol1(int[][] grid) {
         int n = grid.length; //n*n
 
         int cnt = 0;
