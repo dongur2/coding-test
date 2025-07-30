17a05dbcaa74b7f3fac324d3116f50f3862436ae
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-07-31 01:11:52 +0900

    Time: 1 ms (92.82%), Space: 44.5 MB (81.62%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,42 +1,36 @@
-import java.util.*;
+import java.util.List; import java.util.ArrayList;
 
+//주어진 숫자 배열로 만들 수 있는 모든 순열 (중복X, 순서의미있음)
 class Solution {
-    /*
-    Q. 주어진 숫자 배열로 만들 수 있는 모든 순열을 반환
-    - 순열 크기는 주어진 숫자 배열의 크기와 같음
-    - 순열: 중복 불허 & 순서에 의미
-    */
-    
-    boolean[] visited; //방문 체크
-    List<List<Integer>> result; //반환할 배열
-    
+    List<List<Integer>> answer = new ArrayList<>();
+    boolean[] visited;
+    int n;
+
     public List<List<Integer>> permute(int[] nums) {
-        visited = new boolean[nums.length];
-        result = new ArrayList<>();
-        
-        //배열을 순서대로 순회하면서 순열 생성
-        makePermutation(nums, new ArrayList<>());
-        
-        return result;
+        n = nums.length;
+        visited = new boolean[n];
+
+        backtrack(nums, new ArrayList<>());
+
+        return answer;
     }
-    
-    private void makePermutation(int[] nums, List<Integer> list) {
-        //만든 순열의 크기가 주어진 배열의 크기와 같으면 반환할 배열에 추가 후 종료
-        if(list.size() == nums.length) {
-            result.add(new ArrayList<>(list)); 
+
+    private void backtrack(int[] nums, List<Integer> list) {
+        if(list.size() == n) {
+            answer.add(new ArrayList<>(list));
             return;
         }
-        
-        //다시 배열을 순서대로 순회
-        for(int i=0; i<nums.length; i++) {
-            //이미 방문한 숫자면 무시
+
+        for(int i=0; i<n; i++) {
             if(visited[i]) continue;
-            
-            //방문하지 않은 숫자면 방문 체크 후 순열에 추가
-            visited[i] = true; list.add(nums[i]);
-            makePermutation(nums, list);
-            //가능한 순열 모두 생성 후에 다시 방문 체크 무효, 순열에서 제거
-            visited[i] = false; list.remove(Integer.valueOf(nums[i]));
+
+            visited[i] = true;
+            list.add(nums[i]);
+
+            backtrack(nums, list);
+
+            visited[i] = false;
+            list.remove(Integer.valueOf(nums[i]));
         }
     }
 }
