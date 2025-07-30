a83f0a0712383e60fb4449ca695e85faa8846ca2
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-07-31 01:19:04 +0900

    Time: 40 ms (6.93%), Space: 94.5 MB (86.58%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,24 +1,22 @@
-import java.util.*;
+import java.util.List; import java.util.ArrayList;
 
+//1~n 숫자 범위로 만들 수 있는 k 길이의 모든 조합 (중복X, 순서의미X)
 class Solution {
-    List<List<Integer>> answer;
-    
+    List<List<Integer>> answer = new ArrayList<>();
+
     public List<List<Integer>> combine(int n, int k) {
-        answer = new ArrayList<>();
-        
-        backtrack(n, 1, k, new ArrayList<>());
-        
+        backtrack(n, k, new ArrayList<>(), 1);
         return answer;
     }
-    
-    void backtrack(int n, int start, int k, List<Integer> list) {
+
+    private void backtrack(int n, int k, List<Integer> list, int idx) {
         if(list.size() == k) {
             answer.add(new ArrayList<>(list)); return;
         }
-        
-        for(int i=start; i<=n; i++) {
+
+        for(int i=idx; i<=n; i++) {
             list.add(i);
-            backtrack(n, i+1, k, list);
+            backtrack(n, k, list, i+1);
             list.remove(Integer.valueOf(i));
         }
     }
