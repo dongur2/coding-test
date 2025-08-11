12e18d75054c93b0c45a49baca160fce18b7cc09
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-08-11 22:25:03 +0900

    [level 2] Title: 문자열 압축, Time: 15.89 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,61 +1,69 @@
-import java.util.*;
-import java.util.stream.*;
-
+/*
+    연속 중복 값 => 중복 횟수 + 값 (1은 생략)
+    가장 짧게 압축한 문자열의 길이 리턴 
+*/
+import java.util.Deque; import java.util.ArrayDeque;
 class Solution {
-    // 스택에 저장할 클래스: 문자열과 카운트로 구성
-    private class Entry {
-        private String word;
-        private int cnt;
+    public int solution(String s) {
+        if(s.length() == 1) return 1;
         
-        public Entry(String word, int cnt) {
-            this.word = word;
-            this.cnt = cnt;
+        int answer = Integer.MAX_VALUE;
+                
+        //중복 확인할 문자열 길이를 1~s.length-1 마다 확인해서 최소 길이 탐색
+        for(int len=1; len<s.length(); len++) {
+            answer = Math.min (compress(s, len), answer);
         }
         
-        // toString을 오버라이딩해서 스택에서 꺼내어 압축된 문자열로 변환할 때 사용
-        @Override
-        public String toString() {
-            return (cnt > 1) ? word+cnt : word; 
-        }
+        return answer;
     }
     
-    public int solution(String s) {
-        //1. 리턴할 길이 변수 생성: 길이 최대값은 원래 문자열의 길이
-        int answer = s.length();
+    int compress(String s, int l) {
+        Deque<Entry> stack = new ArrayDeque<>();
         
-        //2. 문자열을 압축할 범위를 최소 1개부터 원래 문자열 반까지 하나씩 늘려가며 순회 & 압축
-        for(int i=1; i<=s.length()/2; i++) {
-            //3. 문자열을 범위에 따라 나눠 리스트에 저장
-            List<String> words = new ArrayList<>();
-            for(int j=0; j<s.length(); j+=i) {
-                words.add(s.substring(j, Math.min(j+i, s.length())));  // * 끝 인덱스가 문자열 길이를 넘지 않도록 최소 연산
-            }
+        StringBuilder sb = new StringBuilder();
+        
+        int idx=0;
+        
+        while(idx+l <= s.length()) {
+            String curr = s.substring(idx, idx+l);
             
-            //4. 스택을 생성하고, 맨 첫 번째 문자열 블록을 저장 
-            Deque<Entry> stack = new ArrayDeque<>();
-            stack.push(new Entry(words.get(0), 1));
-            
-            //5. 저장한 문자열 블록 다음부터 끝까지 자른 문자열을 스택에 저장한 최신 문자열값과 비교
-            for(String word:words.subList(1, words.size())) {
-                
-                //6. 스택에 저장한 앞 글자와 현재 글자가 같으면: 스택에서 값을 꺼내어 카운트를 추가한 다음 다시 저장
-                if(stack.peek().word.equals(word)) {
-                    Entry preWord = stack.pop();
-                    preWord.cnt++;                
-                    stack.push(preWord);
+            if(stack.isEmpty()) stack.push(new Entry(curr, 1));
+            else {
+                //들어있는 값과 비교
+                Entry top = stack.pop();
+                //같으면 카운트 추가
+                if(top.word.equals(curr)) {
+                    top.cnt++;
+                    stack.push(top);
                 }
-                
-                //7. 앞 글자와 현재 글자가 다르면: 스택에 Entry 새 객체를 생성해 저장
-                else stack.push(new Entry(word, 1));
+                //다르면 꺼낸 값은 문자열에 추가하고 들고있던 값은 스택에 저장
+                else {
+                    if(top.cnt != 1) sb.append(top.cnt);
+                    sb.append(top.word);
+                    stack.push(new Entry(curr, 1));
+                }
             }
-            //8. 스택에 저장된 값을 압축된 문자열로 변환
-            String compressed = stack.stream()
-                .map(Entry::toString).collect(Collectors.joining());
             
-            //9. 압축 문자열의 길이와 전역 길이 변수의 최솟값을 비교해 최솟값을 저장
-            answer = Math.min(answer, compressed.length());
+            idx += l;
         }
-        //10. 길이 리턴
-        return answer;
+        
+        //스택에 남아있는 값 붙이기
+        while(!stack.isEmpty()) {
+            Entry top = stack.pop();
+            if(top.cnt != 1) sb.append(top.cnt);
+            sb.append(top.word);
+        }
+        
+        //자르는 문자열 길이를 충족하지 못하고 남겨진 꼬리 붙이기 
+        while(idx < s.length()) {
+            sb.append(s.charAt(idx++));
+        }
+        
+        return sb.length();
+    }
+    
+    class Entry {
+        String word; int cnt;
+        public Entry(String word, int cnt) { this.word = word; this.cnt = cnt; }
     }
 }
