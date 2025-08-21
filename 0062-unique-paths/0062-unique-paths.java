384b8a93a6e9c3905eeb8f7261b976af461a726a
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-08-21 23:47:57 +0900

    Time: 115 ms (1.4%), Space: 40.5 MB (47.13%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,35 +1,28 @@
-//왼쪽 위 -> 오른쪽 아래까지 갈 수 있는 경로 개수  
-//아래, 오른쪽으로만 이동 가능
+/*
+    m x n 보드
+    로봇은 아래쪽, 오른쪽 1칸씩 이동 
+    >>> [0][0] -> [m-1][n-1]까지의 경로 수 
+ */
 class Solution {
-    static int[][] map;
-
     public int uniquePaths(int m, int n) {
-        map = new int[m][n];
-        return countWays(m, n);        
-    }
+        int[][] dp = new int[m][n];
 
-    //노드 도달 방법 개수 = 위 노드 + 왼쪽 노드 
-    public static int countWays(int m, int n) {
-        //각 노드에 도착할 수 있는 방법 개수 저장
-        //0번째 행과 열은 1 고정 
-        for(int r=0; r<m; r++) {
-            //첫행일 경우 오른쪽으로만 이동 
-            if(r == 0) {
-                for(int c=0; c<n; c++) {
-                    map[r][c] = 1;
-                }
-            }
+        //row[0],col[0]은 무조건 1
+        for(int i=0; i<n; i++) {
+            dp[0][i] = 1;
+        }
 
-            else {
-                for(int c=0; c<n; c++) {
-                    //첫열일 경우 아래로만 이동 
-                    if(c == 0) map[r][c] = 1; 
-                    //첫열이 아닐 경우: 위 노드 + 왼쪽 노드 
-                    else map[r][c] = map[r-1][c] + map[r][c-1];
-                }
+        for(int i=0; i<m; i++) {
+            dp[i][0] = 1;
+        }
+
+        for(int r=1; r<m; r++) {
+            for(int c=1; c<n; c++) {
+                //왼쪽+위쪽 (맨 첫번째 열과 행을 제외하고는 왼쪽에서 오는 경로, 윗쪽에서 오는 경로를 합하면 그 칸까지 오는 총 경로 수)
+                dp[r][c] = dp[r-1][c] + dp[r][c-1];
             }
         }
 
-        return map[m-1][n-1];
+        return dp[m-1][n-1];
     }
 }
