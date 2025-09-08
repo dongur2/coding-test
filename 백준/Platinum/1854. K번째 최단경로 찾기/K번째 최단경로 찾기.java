b3eeee4a448563f5a5b31dc7dedf0f179626f540
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-09-09 00:12:35 +0900

    [Platinum IV] Title: K번째 최단경로 찾기, Time: 1420 ms, Memory: 230016 KB -BaekjoonHub

--- 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.java
+++ 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.java
@@ -2,81 +2,89 @@ import java.util.*;
 import java.io.*;
 
 /*
-  >>> 1번 도시->i번 도시로 가는 k번째 최단경로 (존재하지 않으면 -1)
-  - 1번 도시에서 시작 
-  - 같은 도시 중복 방문 가능 
+  >>> k번째 최단경로 
 */
 public class Main {
   static class Edge {
     int to, dist;
-    public Edge(int to, int dist) { this.to=to; this.dist=dist; }
+    public Edge(int to, int dist) {
+      this.to=to; this.dist=dist;
+    }
   }
-  static class City implements Comparable<City> {
+  
+  static class Node implements Comparable<Node>{
     int num, dist;
-    public City(int num, int dist) { this.num=num; this.dist=dist; }
-    @Override public int compareTo(City c) { return this.dist - c.dist; }
+    public Node(int num, int dist) {
+      this.num=num; this.dist=dist;
+    }
+    @Override
+    public int compareTo(Node node) {
+      return this.dist - node.dist;
+    }
   }
   
   public static void main(String[] args) throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     
     StringTokenizer st = new StringTokenizer(br.readLine());
-    int n = Integer.parseInt(st.nextToken()); //도시 개수 
-    int m = Integer.parseInt(st.nextToken()); //도로 개수 
-    int k = Integer.parseInt(st.nextToken()); 
+    int n = Integer.parseInt(st.nextToken()); //도시 
+    int m = Integer.parseInt(st.nextToken()); //도로 
+    int k = Integer.parseInt(st.nextToken()); //최단경로 순번 
     
     Map<Integer, List<Edge>> map = new HashMap<>();
+    
+    //a -> b 도로 길이 
     for(int i=0; i<m; i++) {
       st = new StringTokenizer(br.readLine());
-      int a = Integer.parseInt(st.nextToken()); //출발 
-      int b = Integer.parseInt(st.nextToken()); //도착 
-      int c = Integer.parseInt(st.nextToken()); //거리 
+      int a = Integer.parseInt(st.nextToken()); 
+      int b = Integer.parseInt(st.nextToken());  
+      int c = Integer.parseInt(st.nextToken()); 
+      
       map.computeIfAbsent(a, key -> new ArrayList<>()).add(new Edge(b, c));
     }
     
-    //각 도시별로 최대 k개 최단 거리 후보 저장 - 내림차순 
-    Map<Integer, Queue<Integer>> dist = new HashMap<>();
-    for(int i=1; i<=n; i++) {
-      dist.put(i, new PriorityQueue<>((o1, o2) -> o2 - o1));
+    //1번에서 시작, 1->n번까지 연결되어있음 
+    Map<Integer, PriorityQueue<Integer>> kthShorts = dijkstra(map, n, k);
+    
+    //i번째 줄: 1 -> i번 도시로 가는 k번째 최단경로(존재하지 않으면 -1)
+    for(int node=1; node<=n; node++) {
+      System.out.println(kthShorts.get(node) == null || kthShorts.get(node).size() < k ? -1 : kthShorts.get(node).peek());
     }
-    
-    //각 도시별 최단 거리 후보 저장 
-    dijkstra(map, dist, k);
-    
-    //k개 후보를 채우지 못했으면 k번쨰 최단 거리가 없음 => -1 / 아니라면 가장 앞 원소 출력 
-    for(int i=1; i<=n; i++) {
-      System.out.println(dist.get(i).size() < k ? -1 : dist.get(i).peek());
-    }
-    
   }
   
-  static void dijkstra(Map<Integer, List<Edge>> map, Map<Integer, Queue<Integer>> dist, int k) {
-    Queue<City> q = new PriorityQueue<>();
-    dist.get(1).offer(0); //시작 도시인 1번은 첫번째 최단 거리가 0
-    q.offer(new City(1, 0));
+  static Map<Integer, PriorityQueue<Integer>> dijkstra(Map<Integer, List<Edge>> map, int n, int k) {
+    Map<Integer, PriorityQueue<Integer>> q = new HashMap<>();
+    for(int i=1; i<=n; i++) {
+      q.put(i, new PriorityQueue<>((o1, o2) -> o2-o1)); //내림차순 -> k번째 최단 경로를 구해야 하므로 긴게 앞으로
+    }
     
-    while(!q.isEmpty()) {
-      City curr = q.poll();
+    Queue<Node> pq = new PriorityQueue<>();
+    q.get(1).offer(0); //1번 도시에서 시작 
+    pq.offer(new Node(1, 0));
+    
+    while(!pq.isEmpty()) {
+      Node curr = pq.poll();
       
       if(!map.containsKey(curr.num)) continue;
       
       for(Edge next : map.get(curr.num)) {
-        int newDist = curr.dist + next.dist;
+        int newDist = curr.dist + next.dist; //새로 계산한 최단 거리 
         
-        //다음 도시의 최단 거리 후보 개수가 k개 미만이면 추가 
-        if(dist.get(next.to).size() < k) {
-          dist.get(next.to).offer(newDist);
-          q.offer(new City(next.to, newDist));
+        //k개보다 적을 때만 큐에 추가 -> k번째 최단 경로를 바로 꺼낼 수 있도록
+        PriorityQueue<Integer> distQ = q.get(next.to);
+        if(distQ.size() < k) {
+          distQ.offer(newDist);
+          pq.offer(new Node(next.to, newDist));
         
-        //k개 채웠지만 저장된 k번째 최단 거리가 새로 계산한 거리보다 길다면 대체 
-        } else if(dist.get(next.to).peek() > newDist) {
-          dist.get(next.to).poll();
-          dist.get(next.to).offer(newDist);
-          q.offer(new City(next.to, newDist));
+        //저장했던 k번째 최단 거리보다 짧은 거리가 있으면 큐에서 꺼내고 다시 저장 
+        } else if (distQ.peek() > newDist) {
+          distQ.poll();
+          distQ.offer(newDist);
+          pq.offer(new Node(next.to, newDist));
         }
       }
     }
-  }
-
     
+    return q;
+  }
 }
