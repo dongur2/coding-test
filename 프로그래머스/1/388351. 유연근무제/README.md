c0591172bb9cc2850a414ac71918f880576a4c98
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-09-13 21:08:16 +0900

    [level 1] Title: 유연근무제, Time: 0.74 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/README.md
+++ 프로그래머스/1/388351. 유연근무제/README.md
@@ -1,10 +1,10 @@
 # [level 1] 유연근무제 - 388351 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351?language=java) 
 
 ### 성능 요약
 
-메모리: 87 MB, 시간: 0.78 ms
+메모리: 94.1 MB, 시간: 0.74 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 29일 01:12:23
+2025년 09월 13일 21:08:14
 
 ### 문제 설명
 
--- 프로그래머스/1/388351. 유연근무제/유연근무제.java
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -1,19 +1,37 @@
-import java.util.*;
-
-//일주일동안 지각안한 직원 수
+/*
+    상품을 받을 직원의 수?
+    - 오늘부터 일주일 동안 각자 설정한 출근 희망 시각에 늦지 않으면 상품
+    - 출근 희망 시각 + 10분까지 출근
+    - 토요일, 일요일은 영향 없음
+    - 시간 = 시*100 + 분
+*/
 class Solution {
     public int solution(int[] schedules, int[][] timelogs, int startday) {
-        int answer = schedules.length;
+        int n = schedules.length; //직원 수
         
-        //직원별로 지각 확인
-        for(int i=0; i<schedules.length; i++) {
-            int deadline = convertToMinute(schedules[i]) + 10;
+        int answer = n;
+        
+        int[] deadline = new int[n]; //희망 시각 분으로 변환
+        for(int i=0; i<n; i++) {
+            deadline[i] = convertToMin(schedules[i]) + 10;
+        }
+        
+        //startday - 1-5:월-금, 6,7:토일
+        
+        //일주일 간 지각 확인 
+        for(int i=0; i<n; i++) {
+            int today = startday;
+            int cut = deadline[i];
             
-            for(int idx=0; idx<7; idx++) {
-                int timelog = convertToMinute(timelogs[i][idx]);
+            for(int t : timelogs[i]) {
+                //주말은 건너뛰기
+                if(today++ >= 6) {
+                    if(today == 8) today = 1;
+                    continue;
+                }
                 
-                int day = (startday + idx) % 7;
-                if(day != 6 && day != 0 && deadline < timelog) { //토일 제외
+                int min = convertToMin(t);
+                if(min > cut) {
                     answer--; break;
                 }
             }
@@ -22,9 +40,9 @@ class Solution {
         return answer;
     }
     
-    int convertToMinute(int time) {
-        int hour = (time / 100) * 60;
+    private int convertToMin(int time) {
+        int hour = time / 100;
         int min = time % 100;
-        return hour + min;
+        return (hour * 60) + min;
     }
 }
