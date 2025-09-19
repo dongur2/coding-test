d3f1588ec3c8ac7da77029b4427986caa58adbb1
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-09-19 23:05:09 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 57.97 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,42 +1,43 @@
-import java.util.*;
-
-//제한 시간 내 퍼즐 해결 위한 최소 숙련도
+import java.util.Arrays;
+//제한 시간 내 퍼즐 모두 해결 가능한 최소 숙련도 
 class Solution {
-    public int solution(int[] diffs, int[] times, long limit) {
-        //최대 숙련도: 주어진 난이도 중 최소 난이도 <= 최대 숙련도 <= 최고 난이도
-        int answer = Arrays.stream(diffs).max().getAsInt();
-
-        int left = 1;
-        int right = answer;
+    static int answer = Integer.MAX_VALUE; 
+    static int n;
+    
+    public int solution(int[] diffs, int[] times, long limit) {    
+        n = diffs.length; //퍼즐 개수
         
-        while(left <= right) {
-            int mid = (left+right)/2;
-            
-            if(isSolved(diffs, times, limit, mid)) {
-                answer = mid;
-                right = mid - 1;
-            }
-            else left = mid + 1;
+        //내 숙련도: diffs목록의 최대 난이도와 최저 난이도(1) 사이 값을 확인 
+        int right = Arrays.stream(diffs).max().getAsInt();
+        int left = 1;
+        
+        while(right >= left) {
+            int mid = (right + left) / 2;
+            if(isValid(diffs, times, limit, mid)) right = mid-1;
+            else left = mid+1;
         }
         
         return answer;
     }
     
-   boolean isSolved(int[] diffs, int[] times, long limit, int level) {
-        long time = 0;
+    boolean isValid(int[] diffs, int[] times, long limit, int level) {
+        long time = 0L; //소요 시간
 
-        for(int i=0; i<diffs.length; i++) {
-            //난이도 <= 숙련도: 현재 퍼즐 소요 시간
-            if(diffs[i] <= level) time += times[i];
-            
-            //난이도 > 숙련도: ((난이도 - 숙련도)번 실패 * (현재 퍼즐 소요 시간 + 이전 퍼즐 소요 시간)) + 현재 퍼즐 소요 시간
+        //모든 퍼즐 풀이
+        for(int i=0; i<n; i++) {
+            if(level >= diffs[i]) time += times[i];
             else {
-                int miss = diffs[i] - level;
-                time += ((times[i-1]+times[i]) * miss) + times[i];
+                time += ((diffs[i]-level) * (times[i]+times[i-1]));
+                time += times[i];
             }
-            if(time > limit) return false;
         }
 
-        return time <= limit;
+        //다 풀었을 경우 확인 후 업데이트
+        if(time <= limit) {
+            answer = Math.min(answer, level);
+            return true;
+        }
+        
+        return false;
     }
 }
