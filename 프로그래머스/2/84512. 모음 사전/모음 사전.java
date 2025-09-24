e45d54cbc6b951cd0de28bb37fd64dbff0f2191d
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-09-24 18:13:31 +0900

    [level 2] Title: 모음 사전, Time: 5.72 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,38 +1,32 @@
-import java.util.*;
-
+/*
+    주어진 단어가 사전에서 몇 번째 단어인지?
+    
+    [사전]
+    - 길이 5 이하
+    - AEIOU만 사용
+    - 짧은 길이 우선, 내림차순
+*/
 class Solution {
-    /*
-    문제: 주어진 단어 word가 사전에서 몇 번째 단어인지 리턴
-    - 사전에는 A, E, I, O, U 만 사용하여 만들 수 있는 길이 5 이하 모든 단어 수록
-    - 첫번째 단어: A, 두 번째: AA, 마지막: UUUUU
-    */
-    String[] arr;
-    int order = 0, answer = 0;
+    int answer = 0;
+    
+    String[] arr = new String[]{"A", "E", "I", "O", "U"};
     
     public int solution(String word) {
-        arr = new String[]{"A", "E", "I", "O", "U"};
-        
-        countOrder(word, 1, new StringBuilder());
-        
+        make(word, "");
         return answer;
     }
     
-    private void countOrder(String word, int len, StringBuilder w) {
-        //현재 길이가 6이상이면 재탐색 중지하고 리턴
-        if(len > 5) return;
+    boolean make(String word, String str) {
+        if(word.equals(str)) return true;
         
-        //모음 배열 원소 처음부터 끝까지 순회:
-        for(String a:arr) {
-            //현재 글자 추가 - 순서 카운트
-            w.append(a); //A1 AA2 AAA3 AAAA4 AAAAA5 6AAAA AAAAE5 AAAAI5 AAAAO5 AAAAU5 
-            order++;
+        if(str.length() == 5) return false;
             
-            if(word.equals(w.toString())) answer = order;
-            
-            //재탐색 - 길이 + 1 매개변수로 전달
-            countOrder(word, len+1, w);
-	        //현재 글자 제거
-            w.deleteCharAt(w.length()-1);
+        for(int i=0; i<5; i++) {
+            answer++;
+            boolean isDone = make(word, str+arr[i]);
+            if(isDone) return true;
         }
+        
+        return false;
     }
 }
