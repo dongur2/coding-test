f38eb787cfe41e0790716cca2aa53b7c280cf360
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-10-01 18:48:11 +0900

    [level 2] Title: 피로도, Time: 3.55 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,39 +1,23 @@
-//탐험 가능한 최대 던전 개수
-/*
-던전 순서를 바꿔서 탐험 [필요, 소모]
-- 유저 >= 최소 필요 피로도 -> 탐험 가능 -> 탐험 후 소모 피로도만큼 유저의 피로도 감소
-- 순서 의미 있는 -- "순열"
-*/
 class Solution {
-    static boolean[] visited;
-    static int answer = 0, n = 0;
-    public int solution(int k, int[][] dungeons) {
-        n = dungeons.length;
-        visited = new boolean[n];
-        
-        explore(k, dungeons, 0);
-        
+    int answer = 0;
+    boolean[] visited;
+    
+    public int solution(int k, int[][] dungeons) {    
+        visited = new boolean[dungeons.length];
+        dfs(k, dungeons, 0);
         return answer;
     }
     
-    public static void explore(int k, int[][] dungeons, int cnt) {
-        for(int i=0; i<n; i++) {
-            if(visited[i]) continue; //이미 탐험한 던전은 무시
+    void dfs(int k, int[][] dungeons, int cnt) {
+        for(int curr=0; curr<dungeons.length; curr++) {
+            if(visited[curr]) continue;
+            if(dungeons[curr][0] > k) continue;
             
-            int[] d = dungeons[i]; //현재 던전
-            
-            //최소 필요 피로도 > 유저 피로도: 불가
-            if(d[0] > k) continue;
-            
-            //최소 필요 피로도 <= 유저 피로도: 탐험 진행
-            else if(d[0] <= k) {
-                visited[i] = true;
-                explore(k-d[1], dungeons, cnt+1);
-                visited[i] = false;
-            }
+            visited[curr] = true;
+            dfs(k-dungeons[curr][1], dungeons, cnt+1);
+            visited[curr] = false;
         }
         
-        //최대 탐험 개수 업데이트
-        answer = Math.max(answer, cnt);
+        answer= Math.max(answer, cnt);
     }
 }
