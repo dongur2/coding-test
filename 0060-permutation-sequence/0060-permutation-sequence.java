d0b44f98df68c2bf03cf44a471cb5dea8728ca05
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-10-01 19:55:49 +0900

    Time: 180 ms (12.07%), Space: 41.3 MB (42.22%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -2,27 +2,25 @@ class Solution {
     int cnt = 0;
     boolean[] visited;
 
-    String answer = "";
     public String getPermutation(int n, int k) {
         visited = new boolean[n+1];
-        dfs(n, k, "");        
-        return answer;
+        char[] answer = new char[n];
+        dfs(n, k, 0, answer);        
+        return new String(answer);
     }
 
-    boolean dfs(int n, int k, String num) {
+    boolean dfs(int n, int k, int idx, char[] num) {
         //길이 만족
-        if(num.length() == n) {
-            if(++cnt == k) {
-                answer = num; return true;
-            }
+        if(idx == n) {
+            if(++cnt == k) return true;
             return false;
         }
 
         for(int curr=1; curr<=n; curr++) {
             if(visited[curr]) continue;
-            visited[curr] = true;
-            if(dfs(n, k, num+curr)) return true;
-            visited[curr] = false;
+            visited[curr] = true; num[idx] = (char)(curr+'0');
+            if(dfs(n, k, idx+1, num)) return true;
+            visited[curr] = false; num[idx] = '0';
         }
 
         return false;
