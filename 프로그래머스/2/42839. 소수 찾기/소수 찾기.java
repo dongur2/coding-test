438a1bab87df6976007041a7047ab09115c0f7f2
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-10-01 19:11:27 +0900

    [level 2] Title: 소수 찾기, Time: 12.55 ms, Memory: 97.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,45 +1,36 @@
-import java.util.Set;
-import java.util.HashSet;
-
-//모든 길이의 부분수열 구하기 -> 소수 판별
+import java.util.Set; import java.util.HashSet;
 class Solution {
-    static Set<Integer> set = new HashSet<>();
-    static int n = 0;
+    Set<Integer> set = new HashSet<>();
+    String[] arr;
+    boolean[] visited;
     
     public int solution(String numbers) {
-        n = numbers.length();
-        
-        //모든 길이로 만들 수 있는 숫자 생성
-        for(int len=1; len<=n; len++) {
-            makeSub(numbers.toCharArray(), len, new boolean[n], new StringBuffer());
-        }
-        
-        //소수 판별
-        return (int) set.stream().filter(s -> isPrime(s)).count();
+        arr = numbers.split("");
+        visited = new boolean[numbers.length()];
+        getPrimes("");
+        return set.size();
     }
     
-    public static void makeSub(char[] chars, int len, boolean[] visited, StringBuffer sb) {
-        //중복을 필터링하기 위해 세트에 추가
-        if(sb.length() == len) {
-            set.add(Integer.parseInt(sb.toString()));
-            return;
-        }
+    void getPrimes(String num) {
+        //소수 확인
+        if(num.length()>0 && isPrime(Integer.valueOf(num))) set.add(Integer.valueOf(num));
         
-        //조합하기 (중복 비허용, 순서 의미 있음)
-        for(int i=0; i<n; i++) {
-            if(visited[i]) continue;
+        //최대 길이면 재귀 중지
+        if(num.length() == arr.length) return;
+        
+        for(int idx=0; idx<arr.length; idx++) {
+            if(visited[idx]) continue;
             
-            sb.append(chars[i]); visited[i] = true;
-            makeSub(chars, len, visited, sb);
-            sb.deleteCharAt(sb.length()-1); visited[i] = false;
+            visited[idx] = true;
+            getPrimes(num+arr[idx]);
+            visited[idx] = false;           
         }
     }
     
-    public static boolean isPrime(int num) {
+    boolean isPrime(int num) {
         if(num == 0 || num == 1) return false;
-        
         for(int i=2; i<=Math.sqrt(num); i++) {
-            if(num % i == 0) return false;
+            if(num%i==0) return false;
         }
         return true;
     }
