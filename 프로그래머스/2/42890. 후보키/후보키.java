6e198678226399c95bd89baf2b073cf291c34758
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-10-01 19:37:24 +0900

    [level 2] Title: 후보키, Time: 9.79 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -1,76 +1,46 @@
-/*
-    후보키 (1)모든 튜플에 대해 유일하게 식별되는 속성 (2)유일하게 식별하는 데 꼭 필요한 속성으로만 구성
-    > 후보 키의 개수 리턴 
-    
-    *1 <= relation[0].length <= 8 튜플 개수 
-    *1 <= relation.length <= 20 속성 개수 
-*/
+import java.util.Set; import java.util.HashSet;
 import java.util.List; import java.util.ArrayList;
-import java.util.Set; import java.util.HashSet; import java.util.stream.Collectors;
 class Solution {
-    List<List<Integer>> comb = new ArrayList<>();
-    
+    Set<List<Integer>> set = new HashSet<>(); 
     public int solution(String[][] relation) {
-        
-        //모든 속성 인덱스 조합 
-        makeAllCombinations(relation, 0, new ArrayList<>()); 
-        
-        //유일성 확인
-        filterUniqueAttributeCombinations(relation);
-        
-        //조합 크기 오름차순 정렬
-        comb.sort((a,b) -> a.size() - b.size());
-        
-        //최소성 확인
-        filterMinimumCombinations();
-        
-        return comb.size();
+        for(int l=1; l<=relation[0].length; l++) {
+            getCandidate(relation, l, new ArrayList<>(), 0);
+        }
+        return set.size();
     }
     
-
-    void makeAllCombinations(String[][] relation, int idx, List<Integer> list) {
-        //인덱스가 범위를 넘어서면 중지 
-        if(idx == relation[0].length) return;
+    void getCandidate(String[][] relation, int l, List<Integer> comb, int curr) {
+        //길이만족 - 유일성,최소성확인
+        if(comb.size() == l) {
+            if(isUnique(relation, comb) && isMinimum(comb)) set.add(new ArrayList<>(comb));
+            return;
+        }
         
-        for(int i=idx; i<relation[0].length; i++) {
-            list.add(i);
-            comb.add(new ArrayList<>(list));
-            makeAllCombinations(relation, i+1, list);
-            list.remove(list.size()-1); //복구
+        for(int i=curr; i<relation[0].length; i++) {
+            comb.add(i);
+            getCandidate(relation, l, comb, i+1);
+            comb.remove(Integer.valueOf(i));
         }
     }
     
-    void filterUniqueAttributeCombinations(String[][] relation) {
-        comb = comb.stream().filter(c -> isUnique(relation, c)).collect(Collectors.toList());
-    }
-
-    //유일성 만족 확인
-    boolean isUnique(String[][] relation, List<Integer> list) {
+    boolean isUnique(String[][] relation, List<Integer> comb) {
         Set<String> tuples = new HashSet<>();
         
         for(String[] r:relation) {
-            StringBuilder data = new StringBuilder();
-            for(int idx:list) {
-                data.append(r[idx]);
-            }     
-            
-            if(tuples.contains(data.toString())) return false;
-            else tuples.add(data.toString());
+            StringBuilder t = new StringBuilder();
+            comb.forEach(c -> t.append(r[c]));
+            if(tuples.contains(t.toString())) return false;
+            else tuples.add(t.toString());
         }
         
         return tuples.size() == relation.length;
     }
     
-    void filterMinimumCombinations() {
-        comb = comb.stream().filter(c -> isMinimum(c)).collect(Collectors.toList());
-    }
-    
-    //최소성 유지 
-    boolean isMinimum(List<Integer> list) {
-        for (List<Integer> c : comb) {
-            if (list.containsAll(c) && !list.equals(c)) return false;
+    boolean isMinimum(List<Integer> comb) {
+        Set<Integer> t = new HashSet<>(comb);
+        for(List<Integer> s:set) {
+            if(t.containsAll(new HashSet<>(s))) return false;
         }
         return true;
     }
-
 }
