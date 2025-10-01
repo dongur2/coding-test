b2b0111adcb314c966abaf25e443d553fed2aca3
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-10-01 16:11:19 +0900

    Time: 1 ms (92.77%), Space: 44.7 MB (46.07%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,6 +1,5 @@
 import java.util.List; import java.util.ArrayList;
 
-//주어진 숫자 배열로 만들 수 있는 모든 순열 (중복X, 순서의미있음)
 class Solution {
     List<List<Integer>> answer = new ArrayList<>();
     boolean[] visited;
@@ -9,28 +8,29 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         n = nums.length;
         visited = new boolean[n];
-
-        backtrack(nums, new ArrayList<>());
-
+        dfs(nums, new ArrayList<>());
         return answer;
     }
 
-    private void backtrack(int[] nums, List<Integer> list) {
+    void dfs(int[] nums, List<Integer> list) {
+        //basecase: 크기 같으면 중지
         if(list.size() == n) {
-            answer.add(new ArrayList<>(list));
+            answer.add(new ArrayList<>(list)); 
             return;
         }
 
-        for(int i=0; i<n; i++) {
-            if(visited[i]) continue;
+        for(int curr=0; curr<n; curr++) {
+            if(visited[curr]) continue;
 
-            visited[i] = true;
-            list.add(nums[i]);
-
-            backtrack(nums, list);
-
-            visited[i] = false;
-            list.remove(Integer.valueOf(nums[i]));
+            //현재 원소 추가
+            list.add(nums[curr]);
+            visited[curr] = true;
+            //다음 원소 탐색
+            dfs(nums, list);
+            //복구
+            list.remove(Integer.valueOf(nums[curr]));
+            visited[curr] = false;
         }
+        
     }
 }
