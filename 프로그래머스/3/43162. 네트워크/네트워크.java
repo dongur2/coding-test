17dd199cbf680408d29f50ac088231fc93181732
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-10-03 02:11:30 +0900

    [level 3] Title: 네트워크, Time: 2.02 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,48 +1,42 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-
-//네트워크 개수
+import java.util.Map; import java.util.HashMap;
+import java.util.List; import java.util.ArrayList;
+import java.util.Deque; import java.util.ArrayDeque;
 class Solution {
     public int solution(int n, int[][] computers) {
-        //연결된 그래프 개수 - dfs 활용
-        int answer = 0;
-
-        //{노드:[인접노드]} 맵 만들기
+        //그래프 연결리스트화
         Map<Integer, List<Integer>> map = new HashMap<>();
-        makeGraph(map, n, computers);
-        
-        //dfs로 연결된 영역 탐색하면서 네트워크 개수 세기
-        boolean[] visited = new boolean[n];
-        for(int i=0; i<n; i++) {
-            if(visited[i]) continue;
-            dfs(map, visited, i);
-            answer++;
+        for(int i=0; i<computers.length; i++) {
+            for(int j=0; j<computers.length; j++) {
+                if(i==j) continue;
+                if(computers[i][j] == 1) map.computeIfAbsent(i, k->new ArrayList<>()).add(j);
+            }
         }
         
+        //bfs로 한 네트워크에 포함된 모든 장치 방문
+        int answer = 0;
+        boolean[] visited = new boolean[computers.length];
+        for(int i=0; i<computers.length; i++) {
+            if(!visited[i]) {
+                bfs(map, visited, i);
+                answer++;
+            }
+        }
         
         return answer;
     }
     
-    public void makeGraph(Map<Integer, List<Integer>> map, int n, int[][] computers) {
-        for(int i=0; i<n; i++) {
-            int[] arr = computers[i];
-            
-            for(int j=0; j<n; j++) {
-                if(arr[j] == 1) map.computeIfAbsent(i, k->new ArrayList<>()).add(j);
-            }
-        }
-    }
-    
-    public void dfs(Map<Integer, List<Integer>> map, boolean[] visited, int node) {
-        visited[node] = true;
+    void bfs(Map<Integer, List<Integer>> map, boolean[] visited, int start) {
+        Deque<Integer> q = new ArrayDeque<>();
+        q.offer(start); visited[start] = true;
         
-        if(map.get(node).isEmpty()) return;
-        //인접 노드 이동
-        for(int n:map.get(node)) {
-            if(visited[n]) continue;
-            dfs(map, visited, n);
+        while(!q.isEmpty()) {
+            int curr = q.poll();
+            
+            if(!map.containsKey(curr)) continue;
+            for(int next : map.get(curr)) {
+                if(visited[next]) continue;
+                q.offer(next); visited[next] = true;
+            }
         }
     }
 }
