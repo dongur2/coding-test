89a7ee341d914b37ffca560acb5e2690e1c0245c
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-10-03 01:55:18 +0900

    Time: 193 ms (5.03%), Space: 45.5 MB (18.76%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,27 +1,28 @@
-//amount원을 만드는 데 필요한 최소 동전 개수 (불가능하면 -1)
+import java.util.Map; import java.util.HashMap;
+
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        final int INF = Integer.MAX_VALUE;
+        if(amount == 0) return 0; 
 
-        /*
-            인덱스 == 가격
-            counts[i] = i원을 만들기 위해 필요한 동전 '최소' 개수 
-         */
-        int[] counts = new int[amount+1];
-        Arrays.fill(counts, INF); //기본값 INF 설정 (주어진 동전으로 i원을 만들 수 없으면 INF 유지)
-        counts[0] = 0; //0원은 0
+        Map<Integer, Integer> map = new HashMap<>(); //가격:필요동전개수 - 최소값으로 업데이트 
+        //0원은 0개
+        map.put(0, 0);
 
-        //1원부터 amount원까지 1원씩 올려가면서 주어진 동전으로 만들 수 있는지 확인
-        for(int price=1; price<=amount; price++) {
-            //각각 동전마다 시도
-            for(int coin:coins) {
-                //동전을 추가했을 때, 만들어야하는 남은 금액이 음수가 아닐 때 (음수면 이미 동전 금액이 초과) 
-                //+ 남은 금액을 주어진 동전으로 만들 수 있을 때 (counts[남은 금액]에 저장된 값은: 해당 금액을 만드는 데 필요한 최소 동전 개수이므로 +1만 하면 됨)
-                int remains = price - coin;
-                if(remains >= 0 && counts[remains] != INF) counts[price] = Math.min(counts[price], counts[remains]+1); //이미 price를 만들었던 적이 있을 수도 있으니까 최소값으로 바인딩
-            }
+        for(int coin : coins) {
+            for(int price=coin; price<=amount; price++) {
+                //현재 동전을 포함해서 가격을 만들기 위해 (가격-동전)원을 만드는 최소 동전 개수를 가져옴
+                int prev = map.getOrDefault(price-coin, -1);
+
+                //(가격-동전)원을 만드는 동전 개수값이 없으면 현재 동전을 포함해서 지금 가격 만들기 불가능
+                if(prev == -1) continue;
+
+                //(가격-동전)원을 만드는 동전 개수값은 있는데 현재 가격에 대한 저장된 값이 없으면 - 아직 한 번도 만든 적 없으므로 바로 개수를 최소로 저장
+                if(!map.containsKey(price)) map.put(price, prev+1);
+                //저장된 값이 있으면 최소인지 비교해서 업데이트
+                else map.put(price, Math.min(map.get(price), prev+1));
+            }            
         }
 
-        return counts[amount] == INF ? -1 : counts[amount];
+        return map.containsKey(amount) ? map.get(amount) : -1;
     }
 }
