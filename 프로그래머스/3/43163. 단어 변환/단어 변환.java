3e42480a23ddbada20bc20e4e43a94e519cf4bce
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-10-03 02:24:28 +0900

    [level 3] Title: 단어 변환, Time: 0.42 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,58 +1,48 @@
-import java.util.Deque;
-import java.util.ArrayDeque;
-
-//시작 단어를 타겟으로 바꾸는데 필요한 최소 작업 수 (불가능하면 0)
+import java.util.Deque; import java.util.ArrayDeque;
 class Solution {
-    static boolean[] visited;
-    
     public int solution(String begin, String target, String[] words) {
-        visited = new boolean[words.length];
-        
         return bfs(begin, target, words);
     }
     
-    public int bfs(String begin, String target, String[] words) {
-        Deque<Node> q = new ArrayDeque<>();
-        q.offer(new Node(begin, 0));
+    class Entry {
+        String str; int cnt;
+        public Entry(String str, int cnt) { this.str=str; this.cnt=cnt; }
+    }
+    
+    int bfs(String begin, String target, String[] words) {
+        boolean[] visited = new boolean[words.length];
+        
+        Deque<Entry> q = new ArrayDeque<>();
+        q.offer(new Entry(begin, 0));
         
         while(!q.isEmpty()) {
-            Node curr = q.poll();
+            Entry curr = q.poll();
+            String currStr = curr.str;
+            int currCnt = curr.cnt;
             
-            //타겟 단어를 만들었을 경우 리턴
-            if(curr.word.equals(target)) return curr.cnt;
+            //타겟단어 완성 - 리턴
+            if(currStr.equals(target)) return currCnt;
             
-            //방문한 적 없는 단어 중에 한글자만 다른 단어 찾아서 진행
             for(int i=0; i<words.length; i++) {
-                if(visited[i] || !isValid(curr.word, words[i])) continue;
+                if(visited[i]) continue;
                 
-                visited[i] = true;
-                q.offer(new Node(words[i], curr.cnt+1));
+                //한글자만 다른 글자만 탐색
+                if(isValid(words[i], currStr)) {
+                    q.offer(new Entry(words[i], currCnt+1)); visited[i] = true; 
+                }
             }
         }
         
+        //타겟 단어 실패
         return 0;
     }
     
-    public boolean isValid(String word, String target) {
+    boolean isValid(String word, String curr) {
         int cnt = 0;
-        
         for(int i=0; i<word.length(); i++) {
-            if(word.charAt(i) != target.charAt(i)) cnt++;
-            
-            //두글자 이상 다를 경우 중지
+            if(word.charAt(i) != curr.charAt(i)) cnt++;
             if(cnt > 1) return false;
         }
-        
         return true;
-    }
-    
-    private class Node {
-        private String word;
-        private int cnt;
-        
-        public Node(String word, int cnt) {
-            this.word = word;
-            this.cnt = cnt;
-        }
     }
 }
