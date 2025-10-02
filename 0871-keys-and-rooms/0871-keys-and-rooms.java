22cff911571cdb51b90bf00bf59cfcb96bf7c002
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-10-02 20:30:13 +0900

    Time: 2 ms (49.88%), Space: 45 MB (5.1%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.java
+++ 0871-keys-and-rooms/0871-keys-and-rooms.java
@@ -1,33 +1,30 @@
 import java.util.List; import java.util.ArrayList;
+import java.util.Deque; import java.util.ArrayDeque;
 
-//0번 방만 잠겨있지 않은 n개 방이 있고, 방문한 방에는 n번 방의 열쇠가 있음 -> 열쇠 번호에 해당하는 방 방문 가능
-//모든 방에 방문할 수 있는지 여부. 
 class Solution {
-    int n;
-    boolean[] visited;
-
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        n = rooms.size();
-        visited = new boolean[n]; //방문 여부 
-
-        dfs(rooms, 0); //0번 방부터 방문 
-
-        //방문한 적 없는 방이 있는지 확인
-        for(boolean v:visited) {
-            if(!v) return false;
-        }
-        return true;
+        int cnt = bfs(rooms);    
+        return cnt == rooms.size();
     }
 
-    private void dfs(List<List<Integer>> rooms, int curr) {
-        visited[curr] = true;
+    int bfs(List<List<Integer>> rooms) {
+        boolean[] visited = new boolean[rooms.size()];
+        int cnt = 0; //방문한 방 개수 
 
-        //방에 열쇠 없으면 리턴 
-        if(rooms.get(curr) == null) return;
-        //방에 있는 열쇠에 해당하는 방에 방문
-        for(int key:rooms.get(curr)) {
-            if(visited[key]) continue;
-            dfs(rooms, key);
+        Deque<Integer> q = new ArrayDeque<>();
+        q.offer(0); visited[0] = true;
+
+        while(!q.isEmpty()) {
+            int curr = q.poll();
+            cnt++; //방문 카운트 
+
+            if(rooms.get(curr) == null) continue; //방에 열쇠 없으면 넘어감
+            for(int key : rooms.get(curr)) {
+                if(visited[key]) continue; //이미 방문한 적 있는 방이면 무시 
+                q.offer(key); visited[key] = true;
+            }
         }
+
+        return cnt;
     }
 }
