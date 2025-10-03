6dfc91871ff25370d91369c473f8364d1c1de725
dongur2 <134156691+dongur2@users.noreply.github.com>
2025-10-04 02:40:50 +0900

    [Gold IV] Title: N-Queen, Time: 7700 ms, Memory: 20380 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,54 +1,52 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
+class Main {
+    static int answer = 0;
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-//크기가 n*n인 체스판에서 퀸 n개가 서로 공격할 수 없는 배치 개수 
-public class Main {
-  static int answer = 0;
-  
-  public static void main(String[] args) throws IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
-    int n = Integer.parseInt(br.readLine());
-    int[][] board = new int[n][n];
+        int[][] board = new int[n][n];
+        
+        dfs(n, board, 0);
+        
+        System.out.println(answer);    
+    }
 
-    place(board, n, 0);
-    
-    System.out.println(answer);
-  }
-  
-  public static void place(int[][] board, int n, int row) {
-    //모든 퀸 배치 완료 
-    if(row == n) {
-      answer++; return;
+    static void dfs(int n, int[][] board, int row) {
+        //퀸 모두 배치 완료
+        if(row == n) {
+            answer++; return;
+        }
+
+        for(int col=0; col<n; col++) {
+            //열마다 퀸 배치해보기
+            //위, 왼쪽 대각선, 오른쪽 대각선에 배치된 퀸이 없으면 배치
+            if(!isValid(board, row, col)) continue;
+            
+            board[row][col] = 1;
+            dfs(n, board, row+1);
+            board[row][col] = 0;
+        }
     }
-    
-    for(int c=0; c<n; c++) {
-      //빈 칸일 경우 유효한 자리인지 확인 후 배치 
-      if(board[row][c] == 0 && isValid(board, n, row, c)) {
-        board[row][c] = 1;
-        place(board, n, row+1);
-        board[row][c] = 0; //복구
-      }
+
+    static boolean isValid(int[][] board, int row, int col) {
+        //위
+        for(int x=row-1; x>=0; x--) {
+            if(board[x][col] == 1) return false;
+        }
+
+        //왼쪽 대각선 위
+        for(int x=row-1, y=col-1; x>=0 && y>=0; x--, y--) {
+            if(board[x][y] == 1) return false;
+        }
+
+        //오른쪽 대각선 위
+        for(int x=row-1, y=col+1; x>=0 && y<board.length; x--, y++) {
+            if(board[x][y] == 1) return false;
+        }
+
+        return true;
     }
-  }
-  
-  public static boolean isValid(int[][] board, int n, int r, int c) {
-    //윗 열에 퀸이 있는지 확인 
-    for(int i=r; i>=0; i--) {
-      if(board[i][c] == 1) return false;
-    }
-    
-    //왼쪽 대각선 위에 퀸 있는지 확인 
-    for(int i=1; r-i>=0 && c-i>=0; i++) {
-      if(board[r-i][c-i] == 1) return false;
-    }
-    
-    //오른쪽 대각선 위에 퀸 있는지 확인 
-    for(int i=1; r-i>=0 && c+i<n; i++) {
-      if(board[r-i][c+i] == 1) return false;
-    }
-    
-    return true;
-  }
 }
